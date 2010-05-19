b596920feea6b11bdfc2658bc390a1c96a6a239c
Rémy Coutable <remy.coutable@gmail.com>
2010-05-19 09:37:36 +0200

    Pending helper spec examples instead of commenting them Setted thumbnail path same as video path /v/<token>

--- app/uploaders/thumbnail_uploader.rb
+++ app/uploaders/thumbnail_uploader.rb
@@ -5,7 +5,7 @@ class ThumbnailUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "t/#{model.token}"
+    "v/#{model.token}"
   end
   
   # Override the filename of the uploaded files
