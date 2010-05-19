ae8c5cce14e6febc24e4125b85fd8e86a2335c7c
Rémy Coutable <remy.coutable@gmail.com>
2010-05-20 01:52:30 +0200

    Don't forget to rake db:populate:videos to see the sort in actions ;) SortHelper improvements

--- app/helpers/sort_helper.rb
+++ app/helpers/sort_helper.rb
@@ -1,23 +1,23 @@
 module SortHelper
   
-  def sort_hash(sort_by, *args)
+  def sort_hash(field, *args)
     options = args.extract_options!
     options.reverse_merge!(:reverse => false)
     
+    sort_by = :"by_#{field}"
     way = ((params[sort_by].nil? && options[:reverse]) || (params[sort_by].present? && params[sort_by] == 'asc')) ? 'desc' : 'asc'
-    
-    { sort_by.to_sym => way, :page => params[:page] }
+    { sort_by => way, :page => params[:page] }
   end
   
-  def sort_classes(sort_by, *args)
+  def sort_classes(field, *args)
     options = args.extract_options!
     options.reverse_merge!(:default => false, :reverse => false)
     
-    sort_name = sort_by.to_s.sub('by_', '')
-    active = " active" if params[sort_by].present? || (options[:default] && params.keys.select { |k| k =~ %r(^by_\w+$) }.all? { |s| s == sort_by })
-    way = " up" if (params[sort_by] == (options[:reverse] ? 'asc' : 'desc'))
+    sort_by = :"by_#{field}"
+    active = " active" if params[sort_by].present? || (options[:default] && params.keys.select { |k| k =~ %r(^by_\w+$) }.all? { |s| s.to_sym == sort_by })
+    way = " up" if params[sort_by] == (options[:reverse] ? 'asc' : 'desc')
     
-    "sort #{sort_name}#{active}#{way}"
+    "sort #{field}#{active}#{way}"
   end
   
 end
