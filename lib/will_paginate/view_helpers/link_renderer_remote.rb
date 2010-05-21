368c749e95d42a61ce7c8d6309bf567f0c1ac66f
Octave Zangs <octavez@octavez.com>
2010-05-21 11:59:13 +0200

    Started implementation of paginate

--- lib/will_paginate/view_helpers/link_renderer_remote.rb
+++ lib/will_paginate/view_helpers/link_renderer_remote.rb
@@ -2,6 +2,16 @@ module WillPaginate
   module ViewHelpers
     class LinkRendererRemote < LinkRenderer
     
+    protected
+    
+      def page_number(page)
+        unless page == current_page
+          link(page, page, :rel => rel_value(page), :class => "page_link")
+        else
+          tag(:em, page, :class => "current_page")
+        end
+      end
+    
     private
     
       def link(text, target, attributes = {})
