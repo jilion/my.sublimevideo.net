697dd8080935c686e1f8e77e63a8dc1c69ce723a
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-05-26 10:37:07 +0200

    Updated & tested log_analyzer parse method

--- lib/log_analyzer.rb
+++ lib/log_analyzer.rb
@@ -1,26 +1,34 @@
 module LogAnalyzer
   class << self
     
-    def parse
-      ral = RequestLogAnalyzer::Controller.build(
+    # Return just RequestLogAnalyzer trackers
+    def parse(logs_file)
+      @controller = RequestLogAnalyzer::Controller.build(
         :format => LogsFileFormat::Voxcast,
-        # :output => LogAnalyzer::Output,
         :boring => true,
         :silent => true,
-        :source_files => Rails.root.join('spec/fixtures/*.gz').to_s
+        :source_files => logs_file.path
       )
-      ral.aggregators.each { |agg| agg.prepare }
-      ral.source.each_request do |request|
-        ral.aggregate_request(ral.filter_request(request))
+      run!
+      @controller.aggregators.first.trackers
+    end
+    
+  private
+    
+    def run!
+      @controller.aggregators.each { |agg| agg.prepare }
+      @controller.source.each_request do |request|
+        @controller.aggregate_request(@controller.filter_request(request))
       end
-      ral.aggregators.each { |agg| agg.finalize }
-      ral.source.finalize
+      @controller.aggregators.each { |agg| agg.finalize }
+      @controller.source.finalize
       
-      p ral.source.parsed_lines
-      p ral.source.skipped_lines
-      ral.aggregators.first.trackers.each do |tracker|
-        p tracker.options
-        p tracker.categories
+      notify_skipped_lines
+    end
+    
+    def notify_skipped_lines
+      if @controller.source.skipped_lines > 0
+        # TODO Notify via Exceptional
       end
     end
     
