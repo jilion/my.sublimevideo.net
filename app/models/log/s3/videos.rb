321fb3fe99dadfea65c9d017784c87e7b9506184
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-06-23 16:58:10 +0200

    Added S3Video log file format

---
+++ app/models/log/s3/videos.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: logs
+#
+#  id         :integer         not null, primary key
+#  type       :string(255)
+#  name       :string(255)
+#  hostname   :string(255)
+#  state      :string(255)
+#  file       :string(255)
+#  started_at :datetime
+#  ended_at   :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Log::S3::Videos < Log::S3
+  
+end
