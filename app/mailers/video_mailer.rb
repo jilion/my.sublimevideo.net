54a4e6674b3eb88418327e3e75a9d64b1eee42f3
Rémy Coutable <remy.coutable@gmail.com>
2010-06-25 00:34:41 +0200

    Not escaping quotes in the "video active" mail subject

--- app/mailers/video_mailer.rb
+++ app/mailers/video_mailer.rb
@@ -2,7 +2,7 @@ class VideoMailer < SublimeVideoMailer
   
   def video_active(video)
     @video = video
-    mail(:to => "#{video.user.full_name} <#{video.user.email}>", :subject => "Your video &ldquo;#{video.title}&rdquo; is now ready!")
+    mail(:to => "#{video.user.full_name} <#{video.user.email}>", :subject => "Your video “#{video.title}” is now ready!")
   end
   
 end
