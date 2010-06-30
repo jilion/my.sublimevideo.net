532f72b916fff1776affbba47deb7f431a7b0a62
Rémy Coutable <remy.coutable@gmail.com>
2010-06-30 15:53:23 +0200

    Prepared dev buckets in case of...

--- app/uploaders/posterframe_uploader.rb
+++ app/uploaders/posterframe_uploader.rb
@@ -6,7 +6,7 @@ class PosterframeUploader < CarrierWave::Uploader::Base
   end
   
   def s3_bucket
-    Rails.env.production? ? "sublimevideo.videos" : "sublimevideo.videos" # TODO Change for dev
+    Rails.env.production? ? "sublimevideo.videos" : "dev.sublimevideo.videos"
   end
   
   # Override the directory where uploaded files will be stored
