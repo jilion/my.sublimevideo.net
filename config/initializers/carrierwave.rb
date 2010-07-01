2a69d285f449e603ec3d0f6f004d595478272272
Rémy Coutable <remy.coutable@gmail.com>
2010-07-01 23:45:41 +0200

    Use S3 for CarrierWave in dev (allowing to try the move-froms3-to-s3 directly that is not testable since test does not use S3)

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 CarrierWave.configure do |config|
   case Rails.env
-  when 'production'
+  when 'production', 'development'
     config.cache_dir            = Rails.root.join('tmp/uploads')
     config.storage              = :s3
     config.s3_cnamed            = true if S3.cnamed
     config.s3_access_key_id     = S3.access_key_id
     config.s3_secret_access_key = S3.secret_access_key
     config.s3_access            = :public_read
-  when 'development'
-    config.storage              = :file
+  # when 'development'
+  #   config.storage              = :file
   when 'test'
     config.storage              = :file
     config.enable_processing    = false
