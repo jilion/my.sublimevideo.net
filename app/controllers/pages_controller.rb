680b03891becdde99acd9bd4b71ca733848800ea
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-07-06 12:06:59 +0200

    Added suspended page and protected sites & videos controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
- 
+  before_filter :authenticate_suspended_user!
+  
   def show
     render params[:page]
+  end
+  
+protected
+  
+  def authenticate_suspended_user!
+    if params[:page] == 'suspended'
+      authenticate_user!
+      redirect_to root_path if user_signed_in? && !current_user.suspended?
+    end
   end
   
 end
