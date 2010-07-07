79dda1916b1b23c976296d6784ddc878bae3a134
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-07-07 14:41:43 +0200

    Reorganized routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,21 +4,18 @@ MySublimeVideo::Application.routes.draw do |map|
   
   match 'login',    :to => 'devise/sessions#new',      :as => "new_user_session"
   match 'logout',   :to => 'devise/sessions#destroy',  :as => "destroy_user_session"
-  match 'register', :to => 'registrations#new', :as => "new_user_registration"
+  match 'register', :to => 'registrations#new',        :as => "new_user_registration"
   
   %w[sign_up signup users/register].each              { |action| match action => redirect('/register'), :via => :get }
   %w[log_in sign_in signin users/login].each          { |action| match action => redirect('/login'),    :via => :get }
   %w[log_out sign_out signout exit users/logout].each { |action| match action => redirect('/logout'),   :via => :get }
   
   devise_for :users,
-  :controllers => { :registrations => "registrations", :invitations => "admin/invitations" },
-  :path_names => { :sign_up => 'register', :sign_in => 'login', :sign_out => 'logout' }
-  
-  match 'admin/users/invitation/new', :to => 'admin/invitations#new', :as => "new_user_invitation"
-  match 'admin/users/invitation', :via => :post, :to => 'admin/invitations#create', :as => "user_invitation"
-  match 'admin/users/invitation', :via => :put, :to => 'admin/invitations#update', :as => "user_invitation"
+    :controllers => { :registrations => "registrations", :invitations => "admin/invitations" },
+    :path_names  => { :sign_up => 'register', :sign_in => 'login', :sign_out => 'logout' }
   
   resources :users, :only => :update
+  
   resources :sites do
     get :state, :on => :member
   end
@@ -28,18 +25,33 @@ MySublimeVideo::Application.routes.draw do |map|
   end
   
   resources :invoices, :only => [:index, :show]
+  
   resource :card, :controller => "credit_cards", :as => :credit_card, :only => [:edit, :update]
   
-  %w[login log_in sign_in signin].each          { |action| match "admin/#{action}" => redirect('/admin/admins/login'), :via => :get  }
+  match ':page', :to => 'pages#show', :via => :get, :as => :page, :page => /terms|docs|support|suspended/
+  
+  root :to => redirect('/sites')
+  
+  # =========
+  # = Admin =
+  # =========
+  
+  match 'admin', :to => redirect('/admin/djs'), :as => "admin"
+  
+  match 'admin/users/invitation/new', :to => 'admin/invitations#new',    :as => "new_user_invitation"
+  match 'admin/users/invitation',     :to => 'admin/invitations#create', :as => "user_invitation", :via => :post
+  match 'admin/users/invitation',     :to => 'admin/invitations#update', :as => "user_invitation", :via => :put
+  
+  %w[login log_in sign_in signin].each          { |action| match "admin/#{action}" => redirect('/admin/admins/login'),  :via => :get }
   %w[logout log_out sign_out signout exit].each { |action| match "admin/#{action}" => redirect('/admin/admins/logout'), :via => :get }
   
-  match 'admin', :to => redirect('/admin/profiles'), :as => "admin"
-  
   devise_for :admins, :path_prefix => "/admin",
-  :controllers => { :registrations => "admin/registrations", :invitations => "admin/invitations" },
-  :path_names => { :sign_in => 'login', :sign_out => 'logout' }
+    :controllers => { :registrations => "admin/registrations", :invitations => "admin/invitations" },
+    :path_names  => { :sign_in => 'login', :sign_out => 'logout' }
   
   namespace "admin" do
+    resources :admins, :only => [:index, :destroy]
+    
     resources :users, :only => [:index]
     
     resources :sites, :only => [:index]
@@ -50,45 +62,7 @@ MySublimeVideo::Application.routes.draw do |map|
       resources :video_profile_versions, :only => [:show, :new, :create, :update], :as => :versions, :path => "versions"
     end
     
-    resources :admins, :only => [:index, :destroy]
-    
     resources :delayed_jobs, :only => [:index, :show, :update, :destroy], :path => "djs"
   end
   
-  match ':page', :to => 'pages#show', :via => :get, :as => :page, :page => /terms|docs|support|suspended/
-  
-  root :to => redirect('/sites')
-  
-  # Sample resource route with options:
-  #   resources :products do
-  #     member do
-  #       get :short
-  #       post :toggle
-  #     end
-  #
-  #     collection do
-  #       get :sold
-  #     end
-  #   end
-  
-  # Sample resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-  
-  # Sample resource route with more complex sub-resources
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get :recent, :on => :collection
-  #     end
-  #   end
-  
-  # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
