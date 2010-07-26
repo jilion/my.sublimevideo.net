80ba6180633867c2ca139c574f71ef4e98490b8f
Rémy Coutable <remy.coutable@gmail.com>
2010-07-26 17:05:03 +0200

    Optimized calculation of amounts and approach real case (not 1cent / hit or similar)
    Replace 60**2 by 1.hour

--- app/models/invoice/videos.rb
+++ app/models/invoice/videos.rb
@@ -1,17 +1,12 @@
 class Invoice::Videos < Array
   attr_reader :amount, :traffic_amount, :requests_amount,
-              :traffic_upload, :traffic_s3, :traffic_us, :traffic_eu, :traffic_as, :traffic_jp, :traffic_unknown,
-              :traffic_upload_amount, :traffic_s3_amount, :traffic_us_amount, :traffic_eu_amount, :traffic_as_amount, :traffic_jp_amount, :traffic_unknown_amount,
-              :requests_s3, :requests_us, :requests_eu, :requests_as, :requests_jp, :requests_unknown,
-              :requests_s3_amount, :requests_us_amount, :requests_eu_amount, :requests_as_amount, :requests_jp_amount, :requests_unknown_amount,
-              :storage_bytehrs, :encoding_time,
               :storage_amount, :encoding_amount,
               :hits # not billed, just for info.
   
   def initialize(invoice, options = {})
     @videos = collect_videos(invoice, options)
     
-    calculate_and_set_values
+    @hits = sum_for(:hits)
     calculate_and_set_amounts
     
     super(@videos)
@@ -24,145 +19,112 @@ private
     if options[:from_cache]
       videos.collect do |video|
         { 
-          :id               => video.id,
-          :video_title      => video.title,
-          :archived_at      => video.archived_at,
-          :traffic_upload   => calculate_upload_traffic(video, invoice),
-          :traffic_s3       => video.traffic_s3_cache,
-          :traffic_us       => video.traffic_us_cache,
-          :traffic_eu       => video.traffic_eu_cache,
-          :traffic_as       => video.traffic_as_cache,
-          :traffic_jp       => video.traffic_jp_cache,
-          :traffic_unknown  => video.traffic_unknown_cache,
-          :requests_s3      => video.requests_s3_cache,
-          :requests_us      => video.requests_us_cache,
-          :requests_eu      => video.requests_eu_cache,
-          :requests_as      => video.requests_as_cache,
-          :requests_jp      => video.requests_jp_cache,
-          :requests_unknown => video.requests_unknown_cache,
-          :storage_bytehrs  => calculate_storage_bytes_per_hours(video, invoice),
-          :encoding_time    => calculate_encoding_time(video, invoice),
-          :hits             => video.hits_cache
+          :id                 => video.id,
+          :video_title        => video.title,
+          :archived_at        => video.archived_at,
+          :traffic_upload     => calculate_upload_traffic(video, invoice),
+          :traffic_s3         => video.traffic_s3_cache,
+          :traffic_us         => video.traffic_us_cache,
+          :traffic_eu         => video.traffic_eu_cache,
+          :traffic_as         => video.traffic_as_cache,
+          :traffic_jp         => video.traffic_jp_cache,
+          :traffic_unknown    => video.traffic_unknown_cache,
+          :requests_s3        => video.requests_s3_cache,
+          :requests_us        => video.requests_us_cache,
+          :requests_eu        => video.requests_eu_cache,
+          :requests_as        => video.requests_as_cache,
+          :requests_jp        => video.requests_jp_cache,
+          :requests_unknown   => video.requests_unknown_cache,
+          :storage_bytes_hour => calculate_storage_bytes_hour(video, invoice),
+          :encoding_time      => calculate_encoding_time(video, invoice),
+          :hits               => video.hits_cache
         }
       end
     else
       # Warning big requests here if video_usages not compacted
       videos.collect do |video|
         { 
-          :id               => video.id,
-          :video_title      => video.title,
-          :archived_at      => video.archived_at,
-          :traffic_upload   => calculate_upload_traffic(video, invoice),
-          :traffic_s3       => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_s3),
-          :traffic_us       => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_us),
-          :traffic_eu       => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_eu),
-          :traffic_as       => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_as),
-          :traffic_jp       => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_jp),
-          :traffic_unknown  => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_unknown),
-          :requests_s3      => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_s3),
-          :requests_us      => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_us),
-          :requests_eu      => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_eu),
-          :requests_as      => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_as),
-          :requests_jp      => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_jp),
-          :requests_unknown => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_unknown),
-          :storage_bytehrs  => calculate_storage_bytes_per_hours(video, invoice),
-          :encoding_time    => calculate_encoding_time(video, invoice),
-          :hits             => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:hits)
+          :id                 => video.id,
+          :video_title        => video.title,
+          :archived_at        => video.archived_at,
+          :traffic_upload     => calculate_upload_traffic(video, invoice),
+          :traffic_s3         => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_s3),
+          :traffic_us         => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_us),
+          :traffic_eu         => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_eu),
+          :traffic_as         => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_as),
+          :traffic_jp         => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_jp),
+          :traffic_unknown    => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:traffic_unknown),
+          :requests_s3        => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_s3),
+          :requests_us        => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_us),
+          :requests_eu        => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_eu),
+          :requests_as        => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_as),
+          :requests_jp        => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_jp),
+          :requests_unknown   => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:requests_unknown),
+          :storage_bytes_hour => calculate_storage_bytes_hour(video, invoice),
+          :encoding_time      => calculate_encoding_time(video, invoice),
+          :hits               => video.usages.started_after(invoice.started_on).ended_before(invoice.ended_on).sum(:hits)
         }
       end
     end
   end
   
-  def calculate_and_set_values
-    @traffic_upload  = @videos.sum { |video| video[:traffic_upload].to_i }
-    @traffic_s3      = @videos.sum { |video| video[:traffic_s3].to_i }
-    @traffic_us      = @videos.sum { |video| video[:traffic_us].to_i }
-    @traffic_eu      = @videos.sum { |video| video[:traffic_eu].to_i }
-    @traffic_as      = @videos.sum { |video| video[:traffic_as].to_i }
-    @traffic_jp      = @videos.sum { |video| video[:traffic_jp].to_i }
-    @traffic_unknown = @videos.sum { |video| video[:traffic_unknown].to_i }
-    @requests_s3       = @videos.sum { |video| video[:requests_s3].to_i }
-    @requests_us       = @videos.sum { |video| video[:requests_us].to_i }
-    @requests_eu       = @videos.sum { |video| video[:requests_eu].to_i }
-    @requests_as       = @videos.sum { |video| video[:requests_as].to_i }
-    @requests_jp       = @videos.sum { |video| video[:requests_jp].to_i }
-    @requests_unknown  = @videos.sum { |video| video[:requests_unknown].to_i }
-    @storage_bytehrs   = @videos.sum { |video| video[:storage_bytehrs].to_i }
-    @encoding_time     = @videos.sum { |video| video[:encoding_time].to_i }
-    @hits              = @videos.sum { |video| video[:hits].to_i }
+  def sum_for(field)
+    @videos.sum { |video| video.key?(field) ? video[field] : 0 }
+  end
+  
+  def traffic_sum
+    %w[upload s3 us eu as jp unknown].sum { |traffic_field| sum_for(:"traffic_#{traffic_field}") }
+  end
+  
+  def requests_sum
+    %w[s3 us eu as jp unknown].sum { |requests_field| sum_for(:"requests_#{requests_field}") }
   end
   
   def calculate_and_set_amounts
-    @videos.each do |video|
-      # TODO, now 100'000 bytes = 1 cent
-      video[:traffic_upload_amount]  = video[:traffic_upload].to_i / 100000.0
-      video[:traffic_s3_amount]      = video[:traffic_s3].to_i / 100000.0
-      video[:traffic_us_amount]      = video[:traffic_us].to_i / 100000.0
-      video[:traffic_eu_amount]      = video[:traffic_eu].to_i / 100000.0
-      video[:traffic_as_amount]      = video[:traffic_as].to_i / 100000.0
-      video[:traffic_jp_amount]      = video[:traffic_jp].to_i / 100000.0
-      video[:traffic_unknown_amount] = video[:traffic_unknown].to_i / 100000.0
-      # TODO, now 10'000 request = 1 cent
-      video[:requests_s3_amount]       = video[:requests_s3].to_i / 10000.0
-      video[:requests_us_amount]       = video[:requests_us].to_i / 10000.0
-      video[:requests_eu_amount]       = video[:requests_eu].to_i / 10000.0
-      video[:requests_as_amount]       = video[:requests_as].to_i / 10000.0
-      video[:requests_jp_amount]       = video[:requests_jp].to_i / 10000.0
-      video[:requests_unknown_amount]  = video[:requests_unknown].to_i / 10000.0
-      # TODO, now 1000'000 bytehrs = 1 cent
-      video[:storage_amount]           = video[:storage_bytehrs].to_i / 1000000.0
-      # TODO, now 1 second = 1 cent
-      video[:encoding_amount]          = video[:encoding_time].to_i
-    end
+    # TODO: Move these conf to config/prices.yml or similar
+    price_in_cents_for_1GB_traffic    = 50 # TODO, now 1GB = 50 cent
+    price_in_cents_for_10000_requests = 1  # TODO, now 10'000 request = 1 cent
+    price_in_cents_for_1GB_per_hour   = 30 # TODO, now 1GB / hour = 30 cent
+    price_in_cents_for_1s_of_encoding = 1  # TODO, now 1 second = 1 cent
     
-    @traffic_upload_amount  = @videos.sum { |video| video[:traffic_upload_amount] }.round
-    @traffic_s3_amount      = @videos.sum { |video| video[:traffic_s3_amount] }.round
-    @traffic_us_amount      = @videos.sum { |video| video[:traffic_us_amount] }.round
-    @traffic_eu_amount      = @videos.sum { |video| video[:traffic_eu_amount] }.round
-    @traffic_as_amount      = @videos.sum { |video| video[:traffic_as_amount] }.round
-    @traffic_jp_amount      = @videos.sum { |video| video[:traffic_jp_amount] }.round
-    @traffic_unknown_amount = @videos.sum { |video| video[:traffic_unknown_amount] }.round
-    @traffic_amount         = @traffic_upload_amount + @traffic_s3_amount + @traffic_us_amount + @traffic_eu_amount + @traffic_as_amount + @traffic_jp_amount + @traffic_unknown_amount
-    @requests_s3_amount       = @videos.sum { |video| video[:requests_s3_amount] }.round
-    @requests_us_amount       = @videos.sum { |video| video[:requests_us_amount] }.round
-    @requests_eu_amount       = @videos.sum { |video| video[:requests_eu_amount] }.round
-    @requests_as_amount       = @videos.sum { |video| video[:requests_as_amount] }.round
-    @requests_jp_amount       = @videos.sum { |video| video[:requests_jp_amount] }.round
-    @requests_unknown_amount  = @videos.sum { |video| video[:requests_unknown] }.round
-    @requests_amount          = @requests_s3_amount + @requests_us_amount + @requests_eu_amount + @requests_as_amount + @requests_jp_amount + @requests_unknown_amount
-    @storage_amount           = @videos.sum { |video| video[:storage_amount] }.round
-    @encoding_amount          = @videos.sum { |video| video[:encoding_amount] }.round
+    @traffic_amount  = (traffic_sum.to_f / 1.gigabyte) * price_in_cents_for_1GB_traffic
     
-    @amount = @traffic_amount + @requests_amount + @storage_amount + @encoding_amount
+    @requests_amount = (requests_sum.to_f / 10000) * price_in_cents_for_10000_requests
+    
+    @storage_amount = (sum_for(:storage_bytes_hour).to_f / 1.gigabyte) * price_in_cents_for_1GB_per_hour
+    
+    @encoding_amount = sum_for(:encoding_time) * price_in_cents_for_1s_of_encoding
+    
+    @amount = [@traffic_amount, @requests_amount, @storage_amount, @encoding_amount].sum.round
   end
   
   def calculate_upload_traffic(video, invoice)
     invoice.include_date?(video.created_at) ? video.file_size : 0
   end
   
-  def calculate_storage_bytes_per_hours(video, invoice)
-    bytes_hours = presence_hours(invoice, video.created_at, video.archived_at) * video.file_size.to_i
-    bytes_hours += video.encodings.all.sum do |encoding|
+  def calculate_storage_bytes_hour(video, invoice)
+    bytes_hour = presence_hours(invoice, video.created_at, video.archived_at) * video.file_size.to_i
+    bytes_hour += video.encodings.all.sum do |encoding|
       presence_hours(invoice, encoding.file_added_at, encoding.file_removed_at) * encoding.file_size.to_i
     end
-    bytes_hours
+    bytes_hour
   end
   
   def presence_hours(invoice, added_at, removed_at)
     if added_at.present?
       if added_at <= invoice.started_on
         if removed_at.nil? || removed_at > invoice.ended_on
-          ((invoice.ended_on.to_time - invoice.started_on.to_time) / 60**2).round
+          ((invoice.ended_on.to_time - invoice.started_on.to_time) / 1.hour).round
         elsif removed_at > invoice.started_on
-          ((removed_at - invoice.started_on.to_time) / 60**2).round
+          ((removed_at - invoice.started_on.to_time) / 1.hour).round
         else
           0 # removed_at < invoice.started_on
         end
       elsif added_at < invoice.ended_on
         if removed_at.nil? || removed_at > invoice.ended_on
-          ((invoice.ended_on.to_time - added_at) / 60**2).round
+          ((invoice.ended_on.to_time - added_at) / 1.hour).round
         elsif removed_at > invoice.started_on
-          ((removed_at - added_at) / 60**2).round
+          ((removed_at - added_at) / 1.hour).round
         else
           0 # removed_at < invoice.started_on
         end
--- spec/models/invoice/videos_spec.rb
+++ spec/models/invoice/videos_spec.rb
@@ -4,59 +4,36 @@ describe Invoice::Videos do
   
   context "from cache" do
     before(:each) do
-      @user   = Factory(:user,
+      @user = Factory(:user,
         :created_at => (1.months + 20.days).ago,
         :last_invoiced_on => 20.days.ago,
         :next_invoiced_on => 10.days.from_now
       )
       create_videos_with_encoding_for(@user)
+      
     end
     
+    let(:total_traffic)  { 200000001 + 400000001 + 300000001 + 100000001 + 60000001 + 20000001 + 111111 }
+    let(:total_requests) { 201 + 401 + 301 + 101 + 61 + 21 }
+    let(:total_storage)  do
+      (((invoice.ended_on.to_time - 10.days.ago) / 1.hour).round * 111111) +
+      (((3.days.ago - 10.day.ago) / 1.hour).round * 1111) +
+      (((invoice.ended_on.to_time - 3.days.ago) / 1.hour).round * 1112) +
+      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 1.hour).round * 222222) +
+      (((5.days.ago - invoice.started_on.to_time) / 1.hour).round * 2223) +
+      (((invoice.ended_on.to_time - 5.days.ago) / 1.hour).round * 2224) +
+      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 1.hour).round * 2225)
+    end
+    let(:total_encoding_time) { 100 + 101 + 303 }
     let(:invoice) { Invoice.current(@user) }
     subject { invoice.videos }
     
-    its(:traffic_upload)   { should == 111111 }
-    its(:traffic_s3)       { should == 200000001 }
-    its(:traffic_us)       { should == 400000001 }
-    its(:traffic_eu)       { should == 300000001 }
-    its(:traffic_as)       { should == 100000001 }
-    its(:traffic_jp)       { should == 60000001 }
-    its(:traffic_unknown)  { should == 20000001 }
-    its(:requests_s3)      { should == 201 }
-    its(:requests_us)      { should == 401 }
-    its(:requests_eu)      { should == 301 }
-    its(:requests_as)      { should == 101 }
-    its(:requests_jp)      { should == 61 }
-    its(:requests_unknown) { should == 21 }
-    its(:encoding_time)    { should == 101 + 100 + 303 }
-    its(:hits)             { should == 2001 }
-    its(:storage_bytehrs)  { should ==
-      (((invoice.ended_on.to_time - 10.days.ago) / 60**2).round * 111111) +
-      (((3.days.ago - 10.day.ago) / 60**2).round * 1111) +
-      (((invoice.ended_on.to_time - 3.days.ago) / 60**2).round * 1112) +
-      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 60**2).round * 222222) +
-      (((5.days.ago - invoice.started_on.to_time) / 60**2).round * 2223) +
-      (((invoice.ended_on.to_time - 5.days.ago) / 60**2).round * 2224) +
-      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 60**2).round * 2225)
-    }
-    its(:traffic_upload_amount)   { should == 1 }
-    its(:traffic_s3_amount)       { should == 2000 }
-    its(:traffic_us_amount)       { should == 4000 }
-    its(:traffic_eu_amount)       { should == 3000 }
-    its(:traffic_as_amount)       { should == 1000 }
-    its(:traffic_jp_amount)       { should == 600 }
-    its(:traffic_unknown_amount)  { should == 200 }
-    its(:traffic_amount)          { should == 10801 }
-    its(:requests_s3_amount)      { should == 0 }
-    its(:requests_us_amount)      { should == 0 }
-    its(:requests_eu_amount)      { should == 0 }
-    its(:requests_as_amount)      { should == 0 }
-    its(:requests_jp_amount)      { should == 0 }
-    its(:requests_unknown_amount) { should == 21 }
-    its(:requests_amount)         { should == 21 }
-    its(:storage_amount)          { should satisfy { |a| [215,216].include?(a) } } # depends on time
-    its(:encoding_amount)         { should == 504 }
-    its(:amount)                  { should satisfy { |a| [11541,11542].include?(a) } } # depends on time
+    its(:hits)            { should == 2001 }
+    its(:traffic_amount)  { should == (total_traffic.to_f / 1.gigabyte) * 50 }
+    its(:requests_amount) { should == (total_requests.to_f / 10000) * 1 }
+    its(:storage_amount)  { should == (total_storage.to_f / 1.gigabyte) * 30 }
+    its(:encoding_amount) { should == total_encoding_time * 1 }
+    its(:amount)          { should == (((total_traffic.to_f / 1.gigabyte) * 50) + ((total_storage.to_f / 1.gigabyte) * 30) + ((total_requests.to_f / 10000) * 1) + (total_encoding_time * 1)).round }
     
     it "should return video array" do
       videos_ids = subject.collect { |video| video[:id] }
@@ -67,7 +44,7 @@ describe Invoice::Videos do
   
   context "from logs" do
     before(:each) do
-      @user   = Factory(:user,
+      @user = Factory(:user,
         :created_at => (2.month + 1.day).ago,
         :invoices_count => 1,
         :last_invoiced_on => (1.month + 1.day).ago,
@@ -124,52 +101,38 @@ describe Invoice::Videos do
       @invoice = Factory(:invoice, :user => @user)
     end
     
+    let(:total_traffic)  do
+      100000000 + 200000000 + 150000000 + 50000000 + 30000000 + 10000000 +
+      10000000 + 20000000 + 15000000 + 5000000 + 3000000 + 1000000 +
+      100000001 + 200000001 + 150000001 + 50000001 + 30000001 + 10000001 +
+      111111
+    end
+    let(:total_requests) do
+      100 + 200 + 150 + 50 + 30 + 10 +
+      10 + 20 + 15 + 5 + 3 + 1 +
+      101 + 201 + 151 + 51 + 31 + 11
+    end
+    let(:total_storage)  do
+      (((invoice.ended_on.to_time - 10.days.ago) / 1.hour).round * 111111) +
+      (((3.days.ago - 10.day.ago) / 1.hour).round * 1111) +
+      (((invoice.ended_on.to_time - 3.days.ago) / 1.hour).round * 1112) +
+      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 1.hour).round * 222222) +
+      (((30.days.ago - invoice.started_on.to_time) / 1.hour).round * 2222) +
+      (((5.days.ago - 30.days.ago) / 1.hour).round * 2223) +
+      (((invoice.ended_on.to_time - 5.days.ago) / 1.hour).round * 2224) +
+      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 1.hour).round * 2225)
+    end
+    let(:total_encoding_time) { 100 + 101 + 301 + 303 }
+    
     let(:invoice) { Invoice.find(@invoice) } # must be deep reloaded!
     subject { Invoice::Videos.new(invoice) }
     
-    its(:traffic_upload)   { should == 111111 }
-    its(:traffic_s3)       { should == 210000001 }
-    its(:traffic_us)       { should == 420000001 }
-    its(:traffic_eu)       { should == 315000001 }
-    its(:traffic_as)       { should == 105000001 }
-    its(:traffic_jp)       { should == 63000001 }
-    its(:traffic_unknown)  { should == 21000001 }
-    its(:requests_s3)      { should == 211 }
-    its(:requests_us)      { should == 421 }
-    its(:requests_eu)      { should == 316 }
-    its(:requests_as)      { should == 106 }
-    its(:requests_jp)      { should == 64 }
-    its(:requests_unknown) { should == 22 }
-    its(:encoding_time)    { should == 101 + 100 + 301 + 303 }
-    its(:hits)             { should == 2101 }
-    its(:storage_bytehrs)  { should ==
-      (((invoice.ended_on.to_time - 10.days.ago) / 60**2).round * 111111) +
-      (((3.days.ago - 10.day.ago) / 60**2).round * 1111) +
-      (((invoice.ended_on.to_time - 3.days.ago) / 60**2).round * 1112) +
-      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 60**2).round * 222222) +
-      (((30.days.ago - invoice.started_on.to_time) / 60**2).round * 2222) +
-      (((5.days.ago - 30.days.ago) / 60**2).round * 2223) +
-      (((invoice.ended_on.to_time - 5.days.ago) / 60**2).round * 2224) +
-      (((invoice.ended_on.to_time - invoice.started_on.to_time) / 60**2).round * 2225)
-    }
-    its(:traffic_upload_amount)   { should == 1 }
-    its(:traffic_s3_amount)       { should == 2100 }
-    its(:traffic_us_amount)       { should == 4200 }
-    its(:traffic_eu_amount)       { should == 3150 }
-    its(:traffic_as_amount)       { should == 1050 }
-    its(:traffic_jp_amount)       { should == 630 }
-    its(:traffic_unknown_amount)  { should == 210 }
-    its(:traffic_amount)          { should == 11341 }
-    its(:requests_s3_amount)      { should == 0 }
-    its(:requests_us_amount)      { should == 0 }
-    its(:requests_eu_amount)      { should == 0 }
-    its(:requests_as_amount)      { should == 0 }
-    its(:requests_jp_amount)      { should == 0 }
-    its(:requests_unknown_amount) { should == 22 }
-    its(:requests_amount)         { should == 22 }
-    its(:storage_amount)          { should satisfy { |a| (185..187).include?(a) } } # depends on time
-    its(:encoding_amount)         { should == 805 }
-    its(:amount)                  { should satisfy { |a| (12353..12355).include?(a) } } # depends on time
+    its(:hits)            { should == 2101 }
+    its(:traffic_amount)  { should == (total_traffic.to_f / 1.gigabyte) * 50 }
+    its(:requests_amount) { should == (total_requests.to_f / 10000) * 1 }
+    its(:storage_amount)  { should == (total_storage.to_f / 1.gigabyte) * 30 }
+    its(:encoding_amount) { should == total_encoding_time.to_f * 1 }
+    its(:amount)          { should == (((total_traffic.to_f / 1.gigabyte) * 50) + ((total_requests.to_f / 10000) * 1) + ((total_storage.to_f / 1.gigabyte) * 30) + (total_encoding_time * 1)).round }
     
     it "should return video array" do
       videos_ids = subject.collect { |video| video[:id] }
