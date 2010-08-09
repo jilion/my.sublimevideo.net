0fcd3955b25c2d759a58ac96236004011bff72f3
Rémy Coutable <rymai@rymai.com>
2010-08-09 15:00:11 +0200

    Use secure sessions, expire session after 1 hour

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MySublimeVideo::Application.config.session_store :cookie_store, :key => '_my.sublimevideo.net_session'
+MySublimeVideo::Application.config.session_store :cookie_store, :key => '_my.sublimevideo.net_session', :expire_after => 3600
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
