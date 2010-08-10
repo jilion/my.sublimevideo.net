fb7c3ac7d577185ba196fc742588446bd5deaec5
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-08-10 08:10:13 +0200

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ MySublimeVideo::Application.routes.draw do
     
     scope :controller => 'users/registrations', :as => :user_registration do
       get    :edit,    :path => '/account/edit'
-      put    :update,  :path => '/credentials'
+      put    :update,  :path => '/account/credentials'
       delete :destroy, :path => '/account', :as => ''
     end
     
@@ -32,7 +32,7 @@ MySublimeVideo::Application.routes.draw do
     %w[log_out sign_out signout exit].each { |action| match action => redirect('/logout'),   :via => :get }
   end
   
-  resource :users, :only => :update, :path => '/info'
+  resource :users, :only => :update, :path => '/account/info'
   
   resources :sites do
     member do
@@ -68,8 +68,8 @@ MySublimeVideo::Application.routes.draw do
     scope :controller => 'admin/admins/invitations', :as => :admin_invitation do
       get  :new,    :path => '/admin/admins/invitation/new'
       post :create, :path => '/admin/admins/invitation', :as => ''
-      get :edit,    :path => '/admin/invitation/accept', :as => 'accept'
-      put :update,  :path => '/admin/invitation'#, :as => ''
+      get  :edit,   :path => '/admin/invitation/accept', :as => 'accept'
+      put  :update, :path => '/admin/invitation'#, :as => ''
     end
     
     scope :controller => 'admin/admins/registrations', :as => :admin_registration do
