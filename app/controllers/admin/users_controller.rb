5c5a29219b672e090a55f68d7f1bf7b4e85e3453
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-09-15 12:58:40 +0200

    Fixed admin user sorts

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,12 +4,12 @@ class Admin::UsersController < Admin::AdminController
   has_scope :enthusiast, :type => :boolean
   has_scope :beta, :type => :boolean
   has_scope :with_activity, :type => :boolean
+  has_scope :use_personal, :type => :boolean
+  has_scope :use_company, :type => :boolean
+  has_scope :use_clients, :type => :boolean
   # sort
   has_scope :by_name_or_email
   has_scope :by_beta
-  has_scope :by_use_personal
-  has_scope :by_use_company
-  has_scope :by_use_clients
   has_scope :by_player_hits
   has_scope :by_traffic
   has_scope :by_date
