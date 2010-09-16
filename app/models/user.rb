58c2150b132f989a712a886d120dcef9958dd78b
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-09-16 15:24:01 +0200

    Fixed http://jilion.hoptoadapp.com/errors/2337684

--- app/models/user.rb
+++ app/models/user.rb
@@ -148,7 +148,7 @@ class User < ActiveRecord::Base
   
   # allow suspended user to login (devise)
   def active?
-    %w[active suspended].include?(state)
+    %w[active suspended].include?(state) && invitation_token.nil?
   end
   
   def welcome?
