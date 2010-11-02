a9d3a2197525f2f9b2f81f4c7505c3a618c65add
Rémy Coutable <rymai@rymai.com>
2010-11-02 16:48:32 +0100

    Fix OneTime::User.delete_invited_not_yet_registered_users

--- lib/one_time/user.rb
+++ lib/one_time/user.rb
@@ -4,7 +4,7 @@ module OneTime
     class << self
       # Method used in the 'one_time:delete_not_registered_invited_users' rake task
       def delete_invited_not_yet_registered_users
-        User.invited.delete_all
+        ::User.invited.delete_all
       end
     end
     
