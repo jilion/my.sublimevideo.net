40122a850161874226fb8fc0746643052bea82a6
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-11-10 16:40:09 +0100

    Commented addons_controller

--- app/controllers/sites/addons_controller.rb
+++ app/controllers/sites/addons_controller.rb
@@ -1,28 +1,18 @@
 class Sites::AddonsController < ApplicationController
-  respond_to :js
-  
-  # GET /sites/1/addons/edit
-  def edit
-    @site = current_user.sites.find(params[:site_id])
-    respond_with(@site) do |format|
-      format.html { redirect_to sites_path }
-      format.js
-    end
-  end
   
   # PUT /sites/1/addons
-  def update
-    @site = current_user.sites.find(params[:site_id])
-    respond_with(@site) do |format|
-      if @site.update_attributes(params[:site])
-        @site.delay.activate # re-generate license file
-        format.html { redirect_to sites_path }
-        format.js
-      else
-        format.html { redirect_to sites_path }
-        format.js   { render :edit }
-      end
-    end
-  end
+  # def update
+  #   @site = current_user.sites.find(params[:site_id])
+  #   respond_with(@site) do |format|
+  #     if @site.update_attributes(params[:site])
+  #       @site.delay.activate # re-generate license file
+  #       format.html { redirect_to sites_path }
+  #       format.js
+  #     else
+  #       format.html { redirect_to sites_path }
+  #       format.js   { render :edit }
+  #     end
+  #   end
+  # end
   
 end
