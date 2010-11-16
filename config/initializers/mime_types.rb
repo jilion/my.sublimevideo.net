d5b8f8acbf375b3d7c59435eded76e1fb0143659
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-11-16 12:14:57 +0100

    Changed :js content-type from text/javascript to application/x-javascript

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
+Mime::Type.register "application/x-javascript", :js
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
