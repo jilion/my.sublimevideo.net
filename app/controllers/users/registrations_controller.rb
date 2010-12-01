b458957b12d0f79d7551008282d5d2fb153222e4
Rémy Coutable <rymai@rymai.com>
2010-12-01 19:45:26 +0100

    Don't allow access to registrations controller while suspended

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   include RedirectionFilters
   
+  before_filter :redirect_suspended_user
   before_filter :only => [:update, :destroy] do |controller|
     redirect_wrong_password(resource)
   end
