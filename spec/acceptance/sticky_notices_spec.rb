158a8433ba9e797904a6317371c5c0a800e89df9
Rémy Coutable <rymai@rymai.com>
2010-12-06 18:04:34 +0100

    Added sticky notices

---
+++ spec/acceptance/sticky_notices_spec.rb
@@ -0,0 +1,84 @@
+require 'spec_helper'
+
+feature "Sticky notices" do
+  feature "no notice" do
+    background do
+      sign_in_as :user
+    end
+    
+    scenario "no notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should_not have_content("Your credit card will expire at the end of the month")
+      page.should_not have_content("Your credit card is expired")
+      page.should_not have_content("Change your credit card")
+    end
+  end
+  
+  feature "credit card will expire this month" do
+    background do
+      sign_in_as :user, :cc_expire_on => Time.now.utc
+    end
+    
+    scenario "show a notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should have_content("Your credit card will expire at the end of the month")
+      page.should have_content("Change your credit card")
+    end
+  end
+  
+  feature "credit card is expired" do
+    background do
+      sign_in_as :user, :cc_expire_on => 2.month.ago
+    end
+    
+    scenario "show a notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should have_content("Your credit card is expired")
+    end
+  end
+  
+  feature "user will be suspended" do
+    background do
+      sign_in_as :user, :suspending_delayed_job_id => 1
+    end
+    
+    scenario "show a notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should have_content("You will be suspended on")
+    end
+  end
+  
+  feature "user will be suspended and credit card will expire this month" do
+    background do
+      sign_in_as :user, :suspending_delayed_job_id => 1, :cc_expire_on => Time.now.utc
+    end
+    
+    scenario "show a notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should have_content("You will be suspended on")
+    end
+  end
+  
+  feature "user will be suspended and credit card is expired" do
+    background do
+      sign_in_as :user, :suspending_delayed_job_id => 1, :cc_expire_on => 2.month.ago
+    end
+    
+    scenario "show a notice" do
+      visit '/sites'
+      
+      current_url.should =~ %r(http://[^/]+/sites)
+      page.should have_content("You will be suspended on")
+    end
+  end
+end
