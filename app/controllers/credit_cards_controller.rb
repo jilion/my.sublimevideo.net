ea641c6a7e1e71f15959fdf6437d9ad78fb6604c
Rémy Coutable <rymai@rymai.com>
2010-12-13 17:25:47 +0100

    Always allow credit card edition

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,7 +1,5 @@
 class CreditCardsController < ApplicationController
   
-  before_filter :credit_card_needed
-  
   # GET /card/edit
   def edit
     @user = User.find(current_user.id)
@@ -12,12 +10,6 @@ class CreditCardsController < ApplicationController
     @user = User.find(current_user.id)
     @user.update_attributes(params[:user])
     respond_with(@user, :location => edit_user_registration_path)
-  end
-  
-private
-  
-  def credit_card_needed
-    redirect_to edit_user_registration_url unless current_user.credit_card?
   end
   
 end
--- spec/controllers/credit_cards_controller_spec.rb
+++ spec/controllers/credit_cards_controller_spec.rb
@@ -5,43 +5,25 @@ describe CreditCardsController do
   context "with a logged in user" do
     before(:each) { sign_in :user, authenticated_user }
     
-    context "with no credit card" do
-      before(:each) { @current_user.stub(:credit_card?).and_return(false) }
-      
-      it "should redirect to /account/edit on GET :edit" do
-        get :edit
-        response.should redirect_to(edit_user_registration_path)
-      end
-      
-      it "should redirect to /account/edit on PUT :update" do
-        put :update, :user => {}
-        response.should redirect_to(edit_user_registration_path)
-      end
+    it "should respond with success on GET :edit" do
+      get :edit
+      response.should be_success
     end
     
-    context "with a credit card" do
-      before(:each) { @current_user.stub(:credit_card?).and_return(true) }
+    it "should redirect to /account/edit on PUT :update that succeed" do
+      @current_user.should_receive(:update_attributes).with({}) { true }
       
-      it "should respond with success on GET :edit" do
-        get :edit
-        response.should be_success
-      end
+      put :update, :user => {}
+      response.should redirect_to(edit_user_registration_path)
+    end
+    
+    it "should render :edit on PUT :update that fail" do
+      @current_user.should_receive(:update_attributes).with({}) { false }
+      @current_user.should_receive(:errors).any_number_of_times.and_return(["error"])
       
-      it "should redirect to /account/edit on PUT :update that succeed" do
-        @current_user.should_receive(:update_attributes).with({}) { true }
-        
-        put :update, :user => {}
-        response.should redirect_to(edit_user_registration_path)
-      end
-      
-      it "should render :edit on PUT :update that fail" do
-        @current_user.should_receive(:update_attributes).with({}) { false }
-        @current_user.should_receive(:errors).any_number_of_times.and_return(["error"])
-        
-        put :update, :user => {}
-        response.should be_success
-        response.should render_template(:edit)
-      end
+      put :update, :user => {}
+      response.should be_success
+      response.should render_template(:edit)
     end
   end
   
