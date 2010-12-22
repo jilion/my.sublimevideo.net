60bc70fed4bc8dd3ed244fbdcabd3f531ac09ebb
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-12-22 15:42:08 +0100

    Fixed logs output

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,9 @@ MySublimeVideo::Application.configure do
   # See everything in the log (default is :info)
   # config.log_level = :debug
 
+  # Heroku logs config
+  config.action_controller.logger = Logger.new(STDOUT)
+
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
 
