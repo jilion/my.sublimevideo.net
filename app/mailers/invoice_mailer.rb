44b2755b50235606149db0908291066b38b0e514
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2010-12-23 10:00:13 +0100

    Tried to support non-ascii fullname inside mail :to

--- app/mailers/invoice_mailer.rb
+++ app/mailers/invoice_mailer.rb
@@ -6,12 +6,12 @@ class InvoiceMailer < SublimeVideoMailer
     pdf = PDFKit.new(InvoicesController.new.render_to_string(:action => 'show', :locals => { :invoice => @invoice }))
     # pdf.stylesheets << "#{Rails.root}/public/stylesheets/invoice.css"
     attachments["invoice_#{@invoice.ended_at.month}_#{@invoice.ended_at.year}.pdf"] = pdf.to_pdf
-    mail(:to => "#{@invoice.user.full_name} <#{@invoice.user.email}>", :subject => "#{l(@invoice.ended_at, :format => :month_fullyear)} invoice is ready to be charged.")
+    mail(:to => "\"#{@invoice.user.full_name}\" <#{@invoice.user.email}>", :subject => "#{l(@invoice.ended_at, :format => :month_fullyear)} invoice is ready to be charged.")
   end
   
   def charging_failed(invoice)
     @invoice = invoice
-    mail(:to => "#{@invoice.user.full_name} <#{@invoice.user.email}>", :subject => "#{l(@invoice.ended_at, :format => :month_fullyear)} invoice charging has failed.")
+    mail(:to => "\"#{@invoice.user.full_name}\" <#{@invoice.user.email}>", :subject => "#{l(@invoice.ended_at, :format => :month_fullyear)} invoice charging has failed.")
   end
   
 end
--- app/mailers/usage_alert_mailer.rb
+++ app/mailers/usage_alert_mailer.rb
@@ -2,7 +2,7 @@ class UsageAlertMailer < SublimeVideoMailer
   
   def limit_reached(site)
     @site = site
-    mail(:to => "#{@site.user.full_name} <#{@site.user.email}>", :subject => "You have reached usage limit for your site #{@site.hostname}")
+    mail(:to => "\"#{@site.user.full_name}\" <#{@site.user.email}>", :subject => "You have reached usage limit for your site #{@site.hostname}")
   end
   
 end
