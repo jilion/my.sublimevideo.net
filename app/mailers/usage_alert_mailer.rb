32eb60dca05364ea776ecc3d948c79ba4a9b8c24
Rémy Coutable <rymai@rymai.com>
2011-01-06 12:31:40 +0100

    Implemented next_plan_recommended mail

--- app/mailers/usage_alert_mailer.rb
+++ app/mailers/usage_alert_mailer.rb
@@ -1,8 +1,13 @@
 class UsageAlertMailer < SublimeVideoMailer
-  
-  def limit_reached(site)
+
+  def plan_player_hits_reached(site)
     @site = site
     mail(:to => "\"#{@site.user.full_name}\" <#{@site.user.email}>", :subject => "You have reached usage limit for your site #{@site.hostname}")
   end
-  
+
+  def next_plan_recommended(site)
+    @site = site
+    mail(:to => "\"#{@site.user.full_name}\" <#{@site.user.email}>", :subject => "You should upgrade your site #{@site.hostname} to the next plan")
+  end
+
 end
--- spec/mailers/usage_alert_mailer_spec.rb
+++ spec/mailers/usage_alert_mailer_spec.rb
@@ -3,24 +3,39 @@ require 'spec_helper'
 describe UsageAlertMailer do
   before(:all) do
     @site = Factory(:site)
+    Factory(:plan, :price => @site.plan.price + 100)
   end
   subject { @site }
-  
-  it_should_behave_like "common mailer checks", %w[limit_reached], :params => [Factory(:site)]
-  
-  describe "#limit_reached" do
+
+  it_should_behave_like "common mailer checks", %w[plan_player_hits_reached next_plan_recommended], :params => [Factory(:site)]
+
+  describe "#plan_player_hits_reached" do
     before(:each) do
-      UsageAlertMailer.limit_reached(subject).deliver
+      UsageAlertMailer.plan_player_hits_reached(subject).deliver
       @last_delivery = ActionMailer::Base.deliveries.last
     end
-    
+
     it "should set subject" do
       @last_delivery.subject.should == "You have reached usage limit for your site #{subject.hostname}"
     end
-    
+
     it "should set a body that contain the link to edit the credit card" do
       @last_delivery.body.encoded.should include "https://#{ActionMailer::Base.default_url_options[:host]}/sites/#{subject.to_param}/edit"
     end
   end
-  
+
+  describe "#next_plan_recommended" do
+    before(:each) do
+      UsageAlertMailer.next_plan_recommended(subject).deliver
+      @last_delivery = ActionMailer::Base.deliveries.last
+    end
+
+    it "should set subject" do
+      @last_delivery.subject.should == "You should upgrade your site #{subject.hostname} to the next plan"
+    end
+
+    it "should set a body that contain the link to edit the credit card" do
+      @last_delivery.body.encoded.should include "https://#{ActionMailer::Base.default_url_options[:host]}/sites/#{subject.to_param}/edit"
+    end
+  end
 end
