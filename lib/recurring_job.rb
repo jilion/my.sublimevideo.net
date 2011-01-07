301af54d0673e9827e29fc5124a155cf0e8c8f5a
Rémy Coutable <rymai@rymai.com>
2011-01-07 08:44:05 +0100

    Fixed failing spec

--- lib/recurring_job.rb
+++ lib/recurring_job.rb
@@ -1,27 +1,27 @@
 module RecurringJob
-  
+
   logs_tasks = [
     '%Log::Voxcast%fetch_download_and_create_new_logs%',
     '%Log::Amazon::S3::Player%fetch_and_create_new_logs%',
     '%Log::Amazon::S3::Loaders%fetch_and_create_new_logs%',
     '%Log::Amazon::S3::Licenses%fetch_and_create_new_logs%'
   ]
-  
+
   NAMES = [
     '%User::CreditCard%send_credit_card_expiration%',
-    '%Site::UsageAlert%delay_send_usage_alerts%',
+    '%Site::UsageAlert%send_usage_alerts%',
     '%Invoice%complete_invoices_for_billable_users%'
   ] + logs_tasks
-  
+
   class << self
-    
+
     def launch_all
       Log.delay_fetch_and_create_new_logs
       User::CreditCard.delay_send_credit_card_expiration
       Site::UsageAlert.delay_send_usage_alerts
       Invoice.delay_complete_invoices_for_billable_users(*TimeUtil.current_month)
     end
-    
+
     def supervise
       # check if recurring jobs are all delayed twice
       unless all_delayed?
@@ -31,16 +31,16 @@ module RecurringJob
         end
       end
     end
-    
+
     def test_exception
       raise "THIS A DELAYED JOB EXCEPTION TEST"
     end
-    
+
   private
-    
+
     def all_delayed?
       NAMES.all? { |name| Delayed::Job.already_delayed?(name) }
     end
-    
+
   end
 end
--- spec/lib/recurring_job_spec.rb
+++ spec/lib/recurring_job_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe RecurringJob do
-  
+
   describe "launch_all method" do
-    
+
     RecurringJob::NAMES.each do |name|
       it "should launch #{name} recurring job" do
         Delayed::Job.already_delayed?(name).should be_false
@@ -11,24 +11,24 @@ describe RecurringJob do
         Delayed::Job.already_delayed?(name).should be_true
       end
     end
-    
+
   end
-  
+
   describe "supervise method" do
-    
+
     it "should do nothing all recurring jobs are delayed" do
       subject.launch_all
       Notify.should_not_receive(:send)
       subject.supervise
     end
-    
+
     it "should notify if all recurring jobs aren't delayed" do
       subject.launch_all
       Delayed::Job.last.delete
       Notify.should_receive(:send)
       subject.supervise
     end
-    
+
   end
-  
+
 end
