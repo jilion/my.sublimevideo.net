7d5ce6655128c57fdb001bbd09fa66b3b81adb24
Rémy Coutable <rymai@rymai.com>
2011-01-07 19:06:56 +0100

    Fixed failing spec

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,7 +19,7 @@ class Admin::UsersController < Admin::AdminController
 
   # GET /admin/users
   def index
-    @users = User.includes(:sites).where(:sites => { :state.ne => :archived })
+    @users = User.includes(:sites)
     @users = apply_scopes(@users).by_date
     respond_with(@users)
   end
--- spec/acceptance/admin/users_spec.rb
+++ spec/acceptance/admin/users_spec.rb
@@ -5,19 +5,20 @@ feature "Users pagination:" do
     sign_in_as :admin
     User.stub!(:per_page).and_return(1)
   end
-  
+
   scenario "pagination links displayed only if count of users > User.per_page" do
     Factory(:user)
-    Admin.all.size.should == 1
+    User.count.should == 1
     visit "/admin/users"
     page.should have_no_css('div.pagination')
     page.should have_no_css('span.previous_page')
     page.should have_no_css('em.current_page')
     page.should have_no_css('a.next_page')
-    
+
     Factory(:user)
+    User.count.should == 2
     visit "/admin/users"
-    save_and_open_page
+
     page.should have_css('div.pagination')
     page.should have_css('span.previous_page')
     page.should have_css('em.current_page')
