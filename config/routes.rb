774aa6129a07cc9e6dd3deccb4dbfe7b9af564eb
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-01-18 09:29:38 +0100

    Added referrer.contextual_hits with referrers controller for redirection

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 MySublimeVideo::Application.routes.draw do
-  
-  get "logs/show"
+
 
   resource :protection, :only => [:show, :create]
-  
+
   devise_for :users,
   :path => '',
   :path_names => { :sign_in => 'login', :sign_out => 'logout' },
@@ -15,48 +14,49 @@ MySublimeVideo::Application.routes.draw do
     :path => '/invitation/accept', :path_names => { :edit => '' } do
       get :edit, :as => :accept
     end
-    
+
     resource :user_registration, :only => [], :controller => 'users/registrations', :path => '' do
       get    :new,     :path => '/register', :as => 'new'
       post   :create,  :path => '/register'
-      
+
       get    :edit,    :path => '/account/edit', :as => 'edit'
       put    :update,  :path => '/account/credentials'
       delete :destroy, :path => '/account'
     end
-    
+
     %w[sign_up signup].each                { |action| match action => redirect('/register'), :via => :get }
     %w[log_in sign_in signin].each         { |action| match action => redirect('/login'),    :via => :get }
     %w[log_out sign_out signout exit].each { |action| match action => redirect('/logout'),   :via => :get }
   end
-  
+
   resource :users, :only => :update, :path => '/account/info'
   resources :sites do
     get :state, :on => :member
   end
   resources :invoices, :only => [:index, :show]
   resource :card, :controller => 'credit_cards', :as => :credit_card, :only => [:edit, :update]
-  
+
   # match ':page', :to => 'pages#show', :via => :get, :as => :page, :page => /terms|privacy|suspended/
   match ':page', :to => 'pages#show', :via => :get, :as => :page, :page => /terms|privacy/
-  
+  match 'r/:type/:token', :to => 'referrers#redirect', :via => :get, :type => /c/, :token => /[a-z0-9]{8}/
+
   resource :ticket, :only => [:new, :create], :path => '/feedback', :path_names => { :new =>  ''}
-  
+
   root :to => redirect("/sites")
-  
+
   # =========
   # = Admin =
   # =========
-  
+
   match 'admin', :to => redirect('/admin/users'), :as => 'admin'
-  
+
   devise_scope :user do
     resource :user_invitation, :only => [], :controller => 'admin/users/invitations', :path => "" do
       get  :new,    :path => '/admin/users/invitation/new', :as => 'new'
       post :create, :path => '/admin/users/invitation'
     end
   end
-  
+
   devise_for :admins,
   :path => 'admin',
   :module => 'admin/admins',
@@ -68,17 +68,17 @@ MySublimeVideo::Application.routes.draw do
       get  :edit,   :path => '/admin/invitation/accept', :as => 'accept'
       put  :update, :path => '/admin/admins/invitation'
     end
-    
+
     resource :admin_registration, :only => [], :controller => 'admin/admins/registrations', :path => "" do
       get    :edit,    :path => '/admin/account/edit', :as => 'edit'
       put    :update,  :path => '/admin/account'
       delete :destroy, :path => '/admin/account'
     end
-    
+
     %w[log_in sign_in signin].each         { |action| match "admin/#{action}" => redirect('/admin/login'),  :via => :get }
     %w[log_out sign_out signout exit].each { |action| match "admin/#{action}" => redirect('/admin/logout'), :via => :get }
   end
-  
+
   namespace "admin" do
     resources :users, :only => [:index, :show]
     resources :admins, :only => [:index, :destroy]
@@ -93,15 +93,15 @@ MySublimeVideo::Application.routes.draw do
     resources :referrers, :only => :index
     resources :stats, :only => :index
   end
-  
+
   # =======
   # = API =
   # =======
-  
+
   namespace "api" do
     resources :invitations, :only => [:create]
   end
-  
+
 end
 #== Route Map
 # Generated on 29 Sep 2010 15:45
@@ -161,13 +161,13 @@ end
 # edit_admin_registration GET    /admin/account/edit(.:format)          {:action=>"edit", :controller=>"admin/admins/registrations"}
 #      admin_registration PUT    /admin/account(.:format)               {:action=>"update", :controller=>"admin/admins/registrations"}
 #      admin_registration DELETE /admin/account(.:format)               {:action=>"destroy", :controller=>"admin/admins/registrations"}
-#            admin_log_in GET    /admin/log_in(.:format)                
-#           admin_sign_in GET    /admin/sign_in(.:format)               
-#            admin_signin GET    /admin/signin(.:format)                
-#           admin_log_out GET    /admin/log_out(.:format)               
-#          admin_sign_out GET    /admin/sign_out(.:format)              
-#           admin_signout GET    /admin/signout(.:format)               
-#              admin_exit GET    /admin/exit(.:format)                  
+#            admin_log_in GET    /admin/log_in(.:format)
+#           admin_sign_in GET    /admin/sign_in(.:format)
+#            admin_signin GET    /admin/signin(.:format)
+#           admin_log_out GET    /admin/log_out(.:format)
+#          admin_sign_out GET    /admin/sign_out(.:format)
+#           admin_signout GET    /admin/signout(.:format)
+#              admin_exit GET    /admin/exit(.:format)
 #       new_admin_session GET    /admin/login(.:format)                 {:action=>"new", :controller=>"admin/admins/sessions"}
 #           admin_session POST   /admin/login(.:format)                 {:action=>"create", :controller=>"admin/admins/sessions"}
 #   destroy_admin_session GET    /admin/logout(.:format)                {:action=>"destroy", :controller=>"admin/admins/sessions"}
--- spec/models/referrer_spec.rb
+++ spec/models/referrer_spec.rb
@@ -2,20 +2,20 @@ require 'spec_helper'
 require 'timecop'
 
 describe Referrer do
-  
+
   describe "validates" do
     [:token, :url, :hits].each do |attr|
       it { should allow_mass_assignment_of(attr) }
     end
-    
+
     it { should validate_presence_of(:url) }
     it { should validate_presence_of(:token) }
-    
+
     it { should allow_value("http://rymai.com").for(:url) }
     it { should_not allow_value("-").for(:url) }
   end
-  
-  context "with a log & site already in db" do
+
+  describe ".create_or_update_from_trackers!" do
     before(:all) do
       logs_file = File.new(Rails.root.join('spec/fixtures/logs/voxcast/cdn.sublimevideo.net.log.1284549900-1284549960.gz'))
       @trackers = LogAnalyzer.parse(logs_file, 'LogsFileFormat::VoxcastReferrers')
@@ -23,43 +23,55 @@ describe Referrer do
         Factory(:site).update_attribute(:token, token)
       end
     end
-    
-    describe "create_or_update_from_trackers!" do
-      before(:each) { Referrer.create_or_update_from_trackers!(@trackers) }
-      
-      let(:site) { Site.find_by_token('k8qaaj1l') }
-      
-      it { Referrer.count.should == 5 }
-      
-      describe "second referrer" do
-        subject { Referrer.all.first }
-        
-        it "should have valid attributes" do
-          subject.url.should == 'http://www.killy.net/'
-          subject.token.should == site.token
-          subject.site_id.should == site.id
-          subject.hits.should == 3
-          subject.created_at.should be_present
-          subject.updated_at.should be_present
-        end
-        
-        it "should update hits if same referrer reparsed" do
-          Referrer.create_or_update_from_trackers!(@trackers)
-          
-          subject.reload.hits.should == 6
-          Referrer.count.should == 5
-        end
-        
-        it "should update updated_at on hits incrementation" do
-          old_update_at = subject.updated_at
-          Timecop.travel(Time.now + 1.minute)
-          
-          Referrer.create_or_update_from_trackers!(@trackers)
-          
-          subject.reload.updated_at.should_not <= old_update_at
-        end
+    before(:each) { Referrer.create_or_update_from_trackers!(@trackers) }
+
+    let(:site) { Site.find_by_token('k8qaaj1l') }
+
+    it { Referrer.count.should == 5 }
+
+    describe "second referrer" do
+      subject { Referrer.all.first }
+
+      it "should have valid attributes" do
+        subject.url.should == 'http://www.killy.net/'
+        subject.token.should == site.token
+        subject.site_id.should == site.id
+        subject.hits.should == 3
+        subject.created_at.should be_present
+        subject.updated_at.should be_present
+      end
+
+      it "should update hits if same referrer reparsed" do
+        Referrer.create_or_update_from_trackers!(@trackers)
+
+        subject.reload.hits.should == 6
+        Referrer.count.should == 5
+      end
+
+      it "should update updated_at on hits incrementation" do
+        old_update_at = subject.updated_at
+        Timecop.travel(Time.now + 1.minute)
+
+        Referrer.create_or_update_from_trackers!(@trackers)
+
+        subject.reload.updated_at.should_not <= old_update_at
       end
     end
-    
+  end
+
+  describe ".create_or_update_from_type!", :focus => true do
+    let(:site) { Factory(:site) }
+
+    it "should create referrer and set contextual_hits to 1 if url/token doesn't exist" do
+      expect { Referrer.create_or_update_from_type!(site.token, 'http://www.bob.com', 'c') }.should change(Referrer, :count).by(1)
+      Referrer.last.contextual_hits.should == 1
+    end
+
+    it "should increment contextual_hits if referrer url/token already exsits" do
+      Referrer.create_or_update_from_type!(site.token, 'http://www.bob.com', 'c')
+      Referrer.last.contextual_hits.should == 1
+      expect { Referrer.create_or_update_from_type!(site.token, 'http://www.bob.com', 'c') }.should change(Referrer, :count).by(0)
+      Referrer.last.contextual_hits.should == 2
+    end
   end
 end
