1bf67d5e73c53fc42d02677fe6d9bd0f27562a64
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-01-26 14:39:30 +0100

    Used site.version_at(invoice.ended_at) for invoice_item.site

--- spec/helpers/sites_helper_spec.rb
+++ spec/helpers/sites_helper_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe SitesHelper do
-  
-  it "is should generate sublimevideo script_tag" do
-    site =  Factory(:site)
-    helper.sublimevideo_script_tag_for(site).should == "<script type=\"text/javascript\" src=\"http://cdn.sublimevideo.net/js/#{site.token}.js\"></script>"
+
+  describe "#sublimevideo_script_tag_for" do
+    it "is should generate sublimevideo script_tag" do
+      site =  Factory(:site)
+      helper.sublimevideo_script_tag_for(site).should == "<script type=\"text/javascript\" src=\"http://cdn.sublimevideo.net/js/#{site.token}.js\"></script>"
+    end
   end
-  
+
 end
