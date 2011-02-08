b214a5afee0ef8c94e36afe2971397f29a928fd3
Rémy Coutable <rymai@rymai.com>
2011-02-08 12:02:38 +0100

    Added moving average to the stats graph ! Also added "real" plans average and limit, and choose the right plan according to the total usage on the selected period. Re-added "Total Main" to the charts.

--- app/helpers/admin/stats_helper.rb
+++ app/helpers/admin/stats_helper.rb
@@ -1,2 +1,11 @@
 module Admin::StatsHelper
+  
+  def moving_average(array, range)
+    Array.new.tap do |arr|
+      (0..(array.size - range)).each do |index|
+        arr << array[index, range].mean
+      end
+    end
+  end
+
 end
