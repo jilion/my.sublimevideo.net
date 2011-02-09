db09ba15e9d04d5d7508f3ff631034e88732c75f
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2011-02-09 14:07:51 +0100

    Fixed notify spec

--- spec/lib/notify_spec.rb
+++ spec/lib/notify_spec.rb
@@ -1,31 +1,31 @@
 require 'spec_helper'
 
 describe Notify do
-  
+
   describe "send method" do
     before(:each) do
       HoptoadNotifier.stub(:notify)
       Notify.stub(:prowl)
     end
-    
+
     it "should notify via hoptoad" do
       message = 'Yo!'
       HoptoadNotifier.should_receive(:notify).with(:error_message => message)
       Notify.send(message)
     end
-    
+
     it "should notify via hoptoad with exception" do
       message = 'Yo!'
-      HoptoadNotifier.should_receive(:notify).with("exception", :error_message => message)
+      HoptoadNotifier.should_receive(:notify).with(:error_message => "Yo! // exception: exception")
       Notify.send(message, :exception => "exception")
     end
-    
+
     it "should notify via prowl" do
       message = 'Yo!'
       Notify.should_receive(:prowl).with(message)
       Notify.send(message)
     end
-    
+
   end
-  
+
 end
