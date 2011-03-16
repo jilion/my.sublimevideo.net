f89d5c63bd7f202176c68b13c2a16660b240873a
Rémy Coutable <remy@jilion.com>
2011-03-16 12:32:53 +0100

    Finished story: "Prevent user from entering a credit card alone (should be entered in a sites/new or plans/edit" Add remote_ip to the Ogone call as well as user's email, zip_code and country.

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,7 @@
 class CreditCardsController < ApplicationController
+  before_filter do |controller|
+    redirect_to([:edit, :user_registration]) unless current_user.cc?
+  end
 
   # GET /card/edit
   def edit
@@ -9,7 +12,13 @@ class CreditCardsController < ApplicationController
   def update
     @user = User.find(current_user.id)
     @user.attributes = params[:user]
-    check_3d_secure = @user.valid? && @user.check_credit_card(accept_url: ok_transaction_url, decline_url: ko_transaction_url, exception_url: ko_transaction_url)
+    options = {
+      accept_url: edit_user_registration_url,
+      decline_url: edit_user_registration_url,
+      exception_url: edit_user_registration_url,
+      ip: request.try(:remote_ip)
+    }
+    check_3d_secure = @user.valid? && @user.check_credit_card(options)
 
     respond_with(@user) do |format|
       format.html do
