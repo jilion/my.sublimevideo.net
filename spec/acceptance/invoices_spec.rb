ab52f969e8830cd432f178c49c566c4367043aa0
Rémy Coutable <remy@jilion.com>
2011-03-23 01:24:56 +0100

    Don't display "Next invoice" for site in dev plans
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- spec/acceptance/invoices_spec.rb
+++ spec/acceptance/invoices_spec.rb
@@ -7,7 +7,7 @@ feature "Invoice actions:" do
   end
 
   scenario "views site invoices (with 0 past invoices)" do
-    site = Factory(:site, plan_id: @dev_plan.id, user: @current_user, hostname: 'google.com')
+    site = Factory(:site_with_invoice, plan_id: @dev_plan.id, user: @current_user, hostname: 'google.com')
 
     visit "/sites"
     click_link "Edit google.com"
@@ -15,13 +15,13 @@ feature "Invoice actions:" do
 
     current_url.should =~ %r(http://[^/]+/sites/#{site.token}/invoices)
     page.should have_content('google.com')
-    
+
     page.should have_content('Past invoices')
     page.should have_content('None.')
   end
 
   scenario "views site invoices (with 0 past invoices and 1 next invoice)" do
-    site = Factory(:site, plan_id: @dev_plan.id, next_plan_id: @paid_plan.id, user: @current_user, hostname: 'google.com')
+    site = Factory(:site_with_invoice, plan_id: @dev_plan.id, user: @current_user, hostname: 'google.com')
 
     visit "/sites"
     click_link "Edit google.com"
@@ -29,15 +29,14 @@ feature "Invoice actions:" do
 
     current_url.should =~ %r(http://[^/]+/sites/#{site.token}/invoices)
     page.should have_content('google.com')
-    
-    page.should have_content('Next invoices')
-    page.should have_content("$#{display_amount(@paid_plan.price)} on #{I18n.l(site.next_cycle_ended_at.tomorrow, :format => :date)}")
-    
+
+    page.should_not have_content('Next invoice')
+
     page.should have_content('Past invoices')
     page.should have_content('None.')
   end
 
-  scenario "views site invoices (with 1 invoice)" do
+  scenario "views site invoices (with 1 invoice and 1 next invoice)" do
     site = Factory(:site_with_invoice, plan_id: @paid_plan.id, user: @current_user, hostname: 'google.com')
 
     visit "/sites"
@@ -46,7 +45,11 @@ feature "Invoice actions:" do
 
     current_url.should =~ %r(http://[^/]+/sites/#{site.token}/invoices)
     page.should have_content('google.com')
-    
+
+    page.should have_content('Next invoice')
+    page.should have_content("$#{Invoice.build_next(site: site).amount / 100.0}")
+    page.should have_content("on #{I18n.l(site.plan_cycle_ended_at.tomorrow, :format => :d_b_Y)}")
+
     page.should have_content('Past invoices')
     page.should have_content("Charged on #{I18n.l(site.last_invoice.paid_at, :format => :minutes_timezone)}")
   end
