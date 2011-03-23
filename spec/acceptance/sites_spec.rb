e5529c59e88c99016e221269237ece5c96c4d844
Rémy Coutable <remy@jilion.com>
2011-03-23 22:33:19 +0100

    Pending acceptance spec for Site
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- spec/acceptance/sites_spec.rb
+++ spec/acceptance/sites_spec.rb
@@ -1,110 +1,206 @@
 require 'spec_helper'
 
-feature "Sites actions:" do
+feature "Sites" do
 
-  background do
-    sign_in_as :user
+  context "with a user with no credit card registered" do
+    background do
+      sign_in_as :user, :without_cc => true
+    end
+
+    feature "new" do
+      scenario "in dev plan" do
+        visit "/sites"
+        click_link "Add a site"
+
+        fill_in "Domain", :with => "google.com"
+        choose "plan_dev"
+        click_button "Create"
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('google.com')
+        page.should have_content('Sandbox')
+
+        @worker.work_off
+        site = @current_user.sites.last
+        site.hostname.should == "google.com"
+        site.loader.read.should include(site.token)
+        site.license.read.should include(site.license_json)
+      end
+
+      pending "in custom plan"
+
+      pending "in paid plan" do
+        visit "/sites"
+        click_link "Add a site"
+
+        fill_in "Domain", :with => "rymai.com"
+        choose "plan_comet_month"
+        VCR.use_cassette('ogone/visa_payment_10') { click_button "Create" }
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content("You need a valid credit card to choose this plan")
+      end
+
+      scenario "invalid in paid plan" do
+        visit "/sites"
+        click_link "Add a site"
+
+        fill_in "Domain", :with => ""
+        choose "plan_comet_month"
+        click_button "Create"
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('Please set at least one domain')
+      end
+    end
+
+    feature "edit" do
+      scenario "edit a site" do
+        site = Factory(:site, user: @current_user, plan_id: @dev_plan.id, hostname: 'rymai.com')
+
+        visit "/sites"
+        page.should have_content('rymai.com')
+        click_link "Edit rymai.com"
+        fill_in "Development domains", :with => "rymai.local"
+        click_button "Update settings"
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('rymai.com')
+
+        @current_user.sites.last.dev_hostnames.should == "rymai.local"
+      end
+    end
   end
 
-  # TODO Remy
-  pending "add a new site" do
-    # visit "/sites"
-    click_link "Add a site"
+  context "with a user with a credit card registered" do
+    background do
+      sign_in_as :user, :without_cc => false
+    end
 
-    fill_in "Domain", :with => "google.com"
-    choose "plan_dev"
-    click_button "Create"
+    feature "new" do
+      scenario "in dev plan" do
+        visit "/sites"
+        click_link "Add a site"
 
-    current_url.should =~ %r(http://[^/]+/sites)
-    page.should have_content('google.com')
+        fill_in "Domain", :with => "google.com"
+        choose "plan_dev"
+        click_button "Create"
 
-    @worker.work_off
-    site = @current_user.sites.last
-    site.hostname.should == "google.com"
-    site.loader.read.should include(site.token)
-    site.license.read.should include(site.license_json)
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('google.com')
+        page.should have_content('Sandbox')
+
+        @worker.work_off
+        site = @current_user.sites.last
+        site.hostname.should == "google.com"
+        site.loader.read.should include(site.token)
+        site.license.read.should include(site.license_json)
+      end
+
+      pending "in custom plan"
+
+      scenario "in paid plan" do
+        visit "/sites"
+        click_link "Add a site"
+
+        fill_in "Domain", :with => "google.com"
+        choose "plan_comet_month"
+        VCR.use_cassette('ogone/visa_payment_10') { click_button "Create" }
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('google.com')
+        page.should have_content('Comet')
+
+        @worker.work_off
+        site = @current_user.sites.last
+        site.hostname.should == "google.com"
+        site.loader.read.should include(site.token)
+        site.license.read.should include(site.license_json)
+      end
+
+      scenario "invalid in paid plan" do
+        visit "/sites"
+        click_link "Add a site"
+
+        fill_in "Domain", :with => ""
+        choose "plan_comet_month"
+        click_button "Create"
+
+        current_url.should =~ %r(http://[^/]+/sites)
+        page.should have_content('Please set at least one domain')
+      end
+    end
+
+    scenario "user suspended" do
+      @current_user.suspend
+      visit "/sites"
+
+      current_url.should =~ %r(http://[^/]+/suspended)
+    end
   end
 
-  pending "add a new site with credit card data"
+  context "no matter if the user has a credit card or not" do
+    background do
+      sign_in_as :user
+    end
 
-  scenario "edit a site" do
-    site = Factory(:site, :user => @current_user, :hostname => 'google.com')
+    feature "archive" do
+      scenario "a dev site" do
+        site = Factory(:site, :user => @current_user, :hostname => 'google.com')
 
-    visit "/sites"
-    page.should have_content('google.com')
-    click_link "Edit google.com"
-    fill_in "Development domains", :with => "google.local"
-    click_button "Update settings"
+        visit "/sites"
+        page.should have_content('google.com')
+        @current_user.sites.last.hostname.should == "google.com"
+        VoxcastCDN.stub_chain(:delay, :purge).twice
 
-    fill_in "Password", :with => "123456"
-    click_button "Done"
+        click_link "Edit google.com"
+        click_button "Delete my site"
 
-    current_url.should =~ %r(http://[^/]+/sites)
-    page.should have_content('google.com')
+        fill_in "Password", :with => "123456"
+        click_button "Done"
 
-    site = @current_user.sites.last
-    site.dev_hostnames.should == "google.local"
-  end
+        page.should_not have_content('google.com')
+        @current_user.sites.not_archived.should be_empty
+      end
+    end
 
-  scenario "archive a site" do
-    site = Factory(:site, :user => @current_user, :hostname => 'google.com')
+    feature "index" do
+      scenario "sort buttons displayed only if count of sites > 1" do
+        Factory(:site, :user => @current_user, :hostname => 'google.com')
+        visit "/sites"
 
-    visit "/sites"
-    page.should have_content('google.com')
-    @current_user.sites.last.hostname.should == "google.com"
-    VoxcastCDN.stub_chain(:delay, :purge).twice
+        page.should have_content('google.com')
+        page.should have_no_css('div.sorting')
+        page.should have_no_css('a.sort')
 
-    click_link "Edit google.com"
-    click_button "Delete my site"
+        Factory(:site, :user => @current_user, :hostname => 'google2.com')
+        visit "/sites"
 
-    fill_in "Password", :with => "123456"
-    click_button "Done"
+        page.should have_content('google.com')
+        page.should have_content('google2.com')
+        page.should have_css('div.sorting')
+        page.should have_css('a.sort.date')
+        page.should have_css('a.sort.hostname')
+      end
 
-    page.should_not have_content('google.com')
-    @current_user.sites.not_archived.should be_empty
-  end
+      scenario "pagination links displayed only if count of sites > Site.per_page" do
+        Responders::PaginatedResponder.stub(:per_page).and_return(1)
+        Factory(:site, :user => @current_user, :hostname => 'google.com')
+        visit "/sites"
 
-  scenario "sort buttons displayed only if count of sites > 1" do
-    Factory(:site, :user => @current_user, :hostname => 'google.com')
-    visit "/sites"
+        page.should have_no_content('Next')
+        page.should have_no_css('nav.pagination')
+        page.should have_no_css('span.next')
 
-    page.should have_content('google.com')
-    page.should have_no_css('div.sorting')
-    page.should have_no_css('a.sort')
+        Factory(:site, :user => @current_user, :hostname => 'google2.com')
+        visit "/sites"
 
-    Factory(:site, :user => @current_user, :hostname => 'google2.com')
-    visit "/sites"
-
-    page.should have_content('google.com')
-    page.should have_content('google2.com')
-    page.should have_css('div.sorting')
-    page.should have_css('a.sort.date')
-    page.should have_css('a.sort.hostname')
-  end
-
-  scenario "pagination links displayed only if count of sites > Site.per_page" do
-    Responders::PaginatedResponder.stub(:per_page).and_return(1)
-    Factory(:site, :user => @current_user, :hostname => 'google.com')
-    visit "/sites"
-
-    page.should have_no_content('Next')
-    page.should have_no_css('nav.pagination')
-    page.should have_no_css('span.next')
-
-    Factory(:site, :user => @current_user, :hostname => 'google2.com')
-    visit "/sites"
-
-    page.should have_css('nav.pagination')
-    page.should have_css('span.prev')
-    page.should have_css('em.current')
-    page.should have_css('a.next')
-  end
-
-  scenario "user suspended" do
-    @current_user.suspend
-    visit "/sites"
-
-    current_url.should =~ %r(http://[^/]+/suspended)
+        page.should have_css('nav.pagination')
+        page.should have_css('span.prev')
+        page.should have_css('em.current')
+        page.should have_css('a.next')
+      end
+    end
   end
 
 end
