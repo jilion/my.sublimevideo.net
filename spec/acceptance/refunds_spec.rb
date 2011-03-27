9cb10cc7895c071b94c20c755c2596d458b3b515
Rémy Coutable <remy@jilion.com>
2011-03-27 22:55:31 +0200

    Fixed refund specs
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- spec/acceptance/refunds_spec.rb
+++ spec/acceptance/refunds_spec.rb
@@ -21,7 +21,7 @@ feature "Refunds" do
       page.should have_content "Refund"
 
       select "rymai.com", :from => "site_id"
-      click_button "Refund this site"
+      click_button "Request a refund"
 
       @site.reload.should be_archived
       @site.should be_refunded
@@ -39,7 +39,7 @@ feature "Refunds" do
       @site.update_attribute(:hostname, 'rymai') # make it invalid
 
       select "rymai.com", :from => "site_id"
-      click_button "Refund this site"
+      click_button "Request a refund"
 
       @site.reload.should_not be_archived
       @site.should_not be_refunded
