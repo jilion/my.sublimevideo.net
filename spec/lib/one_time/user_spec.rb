3d00f1098f255023d938c360e74cec8067767af3
Rémy Coutable <remy@jilion.com>
2011-03-29 12:19:01 +0200

    Fixed task that archive all invited and not registered users
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- spec/lib/one_time/user_spec.rb
+++ spec/lib/one_time/user_spec.rb
@@ -5,21 +5,28 @@ describe OneTime::User do
 
   context "with 1 invited and 1 beta user" do
     before(:all) do
-      @invited_user = Factory(:user, :invitation_token => '123', :invitation_sent_at => Time.now)
-      @beta_user    = Factory(:user, :invitation_token => nil)
+      @invited_user = Factory(:user, invitation_token: '123', :invitation_sent_at => Time.now)
+      @beta_user    = Factory(:user, invitation_token: nil, created_at: Time.utc(2011,1,1))
+      @normal_user  = Factory(:user, invitation_token: nil)
     end
 
     describe ".archive_invited_not_yet_registered_users" do
       it "should exist 1 registered user and 1 invited user" do
         @invited_user.should be_invited
         @invited_user.should be_persisted
+        @invited_user.should_not be_beta
 
         @beta_user.should_not be_invited
         @beta_user.should be_persisted
+        @beta_user.should be_beta
 
-        User.all.should == [@invited_user, @beta_user]
-        User.invited.all.should == [@invited_user]
-        User.beta.all.should == [@beta_user]
+        @normal_user.should_not be_invited
+        @normal_user.should be_persisted
+        @normal_user.should_not be_beta
+
+        User.all.should =~ [@invited_user, @beta_user, @normal_user]
+        User.invited.all.should =~ [@invited_user]
+        User.beta.all.should =~ [@beta_user]
       end
 
       context "actually test the method" do
@@ -29,10 +36,10 @@ describe OneTime::User do
 
         it "should only archive invited and not yet registered users" do
           @invited_user.reload
-          User.all.should == [@beta_user, @invited_user]
-          User.with_state(:archived).all.should == [@invited_user]
-          User.beta.all.should == [@beta_user]
-          User.invited.all.should == [@invited_user]
+          User.all.should =~ [@beta_user, @invited_user, @normal_user]
+          User.with_state(:archived).all.should =~ [@invited_user]
+          User.beta.all.should =~ [@beta_user]
+          User.invited.all.should =~ [@invited_user]
         end
       end
     end
