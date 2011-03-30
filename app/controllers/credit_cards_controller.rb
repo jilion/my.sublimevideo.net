e2095c96177510892d6f1f76f3878b4871bee262
Rémy Coutable <remy@jilion.com>
2011-03-30 15:45:09 +0200

    Typo !!!!

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,6 +1,6 @@
 class CreditCardsController < ApplicationController
   before_filter do |controller|
-    redirect_to([:edit, :user_registration]) if !current_user.cc? && !current_usr.pending_cc?
+    redirect_to([:edit, :user_registration]) if !current_user.cc? && !current_user.pending_cc?
   end
 
   # GET /card/edit
