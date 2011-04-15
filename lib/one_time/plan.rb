d0457d3af5f96cb221c130b353016c6f5b5c518c
Rémy Coutable <remy@jilion.com>
2011-04-15 23:47:48 +0200

    Don't fucking rollback (and block) beta sites that have a payment ongoing (they have a pending_plan_id present). Removed legacy code.
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- lib/one_time/plan.rb
+++ lib/one_time/plan.rb
@@ -2,22 +2,6 @@ module OneTime
   module Plan
 
     class << self
-      def create_plans
-        plans_attributes = [
-          { name: "dev",        cycle: "none",  player_hits: 0,          price: 0 },
-          { name: "sponsored",  cycle: "none",  player_hits: 0,          price: 0 },
-          { name: "beta",       cycle: "none",  player_hits: 0,          price: 0 },
-          { name: "comet",      cycle: "month", player_hits: 3_000,      price: 990 },
-          { name: "planet",     cycle: "month", player_hits: 50_000,     price: 1990 },
-          { name: "star",       cycle: "month", player_hits: 200_000,    price: 4990 },
-          { name: "galaxy",     cycle: "month", player_hits: 1_000_000,  price: 9990 },
-          { name: "comet",      cycle: "year",  player_hits: 3_000,      price: 9900 },
-          { name: "planet",     cycle: "year",  player_hits: 50_000,     price: 19900 },
-          { name: "star",       cycle: "year",  player_hits: 200_000,    price: 49900 },
-          { name: "galaxy",     cycle: "year",  player_hits: 1_000_000,  price: 99900 }
-        ]
-        plans_attributes.each { |attributes| ::Plan.create(attributes) }
-      end
     end
 
   end
--- spec/lib/one_time/site_spec.rb
+++ spec/lib/one_time/site_spec.rb
@@ -3,109 +3,44 @@ require 'spec_helper'
 
 describe OneTime::Site do
 
-  describe ".set_beta_plan" do
+  describe ".rollback_beta_sites_to_dev", :focus => true do
     before(:all) do
-      @active_invalid   = Factory.build(:new_site, plan: nil, state: 'active', hostname: 'jilion.local').tap { |s| s.save(validate: false) }
-      @archived_invalid = Factory.build(:new_site, plan: nil, state: 'archived', hostname: 'jilion.local').tap { |s| s.save(validate: false) }
-      @invalid1         = Factory.build(:new_site, plan: nil, state: 'active', hostname: nil).tap { |s| s.save(validate: false) }
-      @invalid2         = Factory.build(:new_site, plan: nil, state: 'active', hostname: '').tap { |s| s.save(validate: false) }
-    end
+      @site1 = Factory(:site, plan_id: @beta_plan.id)
+      @site2 = Factory(:site, plan_id: @beta_plan.id)
+      @site2.update_attribute(:pending_plan_id, @paid_plan.id)
+      @site3 = Factory(:site, state: 'archived', plan_id: @beta_plan.id)
 
-    context "actually test the method for all sites" do
-      before(:all) { described_class.set_beta_plan }
+      Site.update_loader_and_license(@site1.id, { loader: false, license: true })
+      Site.update_loader_and_license(@site2.id, { loader: false, license: true })
+      Site.update_loader_and_license(@site3.id, { loader: false, license: true })
 
-      it "should set all sites plan to beta" do
-        @active_invalid.reload.should be_in_beta_plan
-        @archived_invalid.reload.should be_in_beta_plan
-        @invalid1.reload.should be_in_dev_plan
-        @invalid2.reload.should be_in_dev_plan
-      end
-    end
-  end
+      @old_license1 = @site1.reload.license.read
+      @old_license2 = @site2.reload.license.read
+      @old_license3 = @site3.reload.license.read
+      puts "@site1.license.read : #{@site1.license.read}"
+      puts "@site2.license.read : #{@site2.license.read}"
+      puts "@site3.license.read : #{@site3.license.read}\n"
 
-  describe ".update_hostnames" do
-    context "on all sites" do
-      before(:all) do
-        @not_public_hostname      = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.local').tap { |s| s.save(validate: false) }
-        @not_local_dev_hostname1  = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.com', dev_hostnames: 'localhost, jilion.net').tap { |s| s.save(validate: false) }
-        @not_local_dev_hostname2  = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.com', dev_hostnames: 'jilion.net, jilion.org').tap { |s| s.save(validate: false) }
-        @duplicated_dev_hostname1 = Factory.build(:new_site, plan: @beta_plan, hostname: '127.0.0.1', dev_hostnames: 'localhost, 127.0.0.1').tap { |s| s.save(validate: false) }
-        @duplicated_dev_hostname2 = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.com', dev_hostnames: 'localhost, 127.0.0.1, 127.0.0.1, localhost').tap { |s| s.save(validate: false) }
-        @mixed_invalid_site       = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.local', dev_hostnames: 'localhost, jilion.local, 127.0.0.1, jilion.net, jilion.com').tap { |s| s.save(validate: false) }
-        @mixed_invalid_site2      = Factory.build(:new_site, plan: @beta_plan, hostname: 'jilion.local', dev_hostnames: 'localhost, jilion.local, 127.0.0.1, jilion.net').tap { |s| s.save(validate: false) }
-      end
-
-      it "all sites created should be invalid" do
-        [@not_public_hostname, @not_local_dev_hostname1, @not_local_dev_hostname2, @duplicated_dev_hostname1, @duplicated_dev_hostname2, @mixed_invalid_site, @mixed_invalid_site2].each do |invalid_site|
-          invalid_site.should_not be_valid
-        end
-      end
-
-      context "actually test the method" do
-        before(:all) { described_class.update_hostnames }
-
-        it "should not modify site when hostname is invalid" do
-          @not_public_hostname.reload.hostname.should == ""
-          @not_public_hostname.dev_hostnames.should   == '127.0.0.1, jilion.local, localhost'
-          @not_public_hostname.extra_hostnames.should == ""
-        end
-
-        it "should move dev hostnames that belong to extra hostnames" do
-          @not_local_dev_hostname1.reload.hostname.should == 'jilion.com'
-          @not_local_dev_hostname1.dev_hostnames.should   == 'localhost'
-          @not_local_dev_hostname1.extra_hostnames.should == 'jilion.net'
-        end
-
-        it "should move dev hostnames that belong to extra hostnames (bis)" do
-          @not_local_dev_hostname2.reload.hostname.should == 'jilion.com'
-          @not_local_dev_hostname2.dev_hostnames.should   == '127.0.0.1, localhost'
-          @not_local_dev_hostname2.extra_hostnames.should == 'jilion.net, jilion.org'
-        end
-
-        it "should remove duplicate dev domain" do
-          @duplicated_dev_hostname1.reload.hostname.should == ""
-          @duplicated_dev_hostname1.dev_hostnames.should   == '127.0.0.1, localhost'
-          @duplicated_dev_hostname1.extra_hostnames.should == ""
-        end
-
-        it "should remove duplicate dev domain (bis)" do
-          @duplicated_dev_hostname2.reload.hostname.should == 'jilion.com'
-          @duplicated_dev_hostname2.dev_hostnames.should   == '127.0.0.1, localhost'
-          @duplicated_dev_hostname2.extra_hostnames.should == ""
-        end
-
-        it "should not remove hostname when hostname is invalid and move it to dev, move dev hostnames that belong to extra hostnames, remove duplicate dev domain, set hostname to first extra hostname" do
-          @mixed_invalid_site.reload.hostname.should == 'jilion.net'
-          @mixed_invalid_site.dev_hostnames.should   == '127.0.0.1, jilion.local, localhost'
-          @mixed_invalid_site.extra_hostnames.should == 'jilion.com'
-        end
-
-        it "should not remove hostname when hostname is invalid and move it to dev, move dev hostnames that belong to extra hostnames, remove duplicate dev domain" do
-          @mixed_invalid_site2.reload.hostname.should == 'jilion.net'
-          @mixed_invalid_site2.dev_hostnames.should   == '127.0.0.1, jilion.local, localhost'
-          @mixed_invalid_site2.extra_hostnames.should == ""
-        end
-      end
-    end
-
-  end
-
-  describe ".rollback_beta_sites_to_dev" do
-    before(:all) do
-      @site_1 = Factory(:site, plan_id: @beta_plan.id)
-      @site_11 = Factory(:site, plan_id: @beta_plan.id)
-      @site_11.update_attribute(:pending_plan_id, @paid_plan.id)
-      @site_2 = Factory(:site, state: 'archived', plan_id: @beta_plan.id)
       described_class.rollback_beta_sites_to_dev
+
+      @worker.work_off
     end
 
     it "should rollback beta site to active state with dev plan" do
-      @site_1.reload.should be_active
-      @site_1.should be_in_dev_plan
-      @site_11.reload.should be_in_dev_plan
-      @site_11.pending_plan_id.should == @paid_plan.id
-      @site_2.reload.should be_archived
-      @site_2.should be_in_dev_plan
+      @site1.reload.should be_active
+      @site1.should be_in_dev_plan
+      @site1.license.read.should_not == @old_license1
+      puts "@site1.license.read : #{@site1.license.read}"
+
+      @site2.reload.should be_in_beta_plan
+      @site2.pending_plan_id.should == @paid_plan.id
+      @site2.license.read.should == @old_license2
+      puts "@site2.license.read : #{@site2.license.read}"
+
+      @site3.reload.should be_archived
+      @site3.should be_in_dev_plan
+      @site3.license.read.should_not == @old_license3
+      puts "@site3.license.read : #{@site3.license.read}"
     end
   end
 
