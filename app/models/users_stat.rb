30b439b9921e6af112787b5a1d4ce856992b1210
Rémy Coutable <remy@jilion.com>
2011-04-21 12:18:10 +0200

    Fixed UsersStat
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- app/models/users_stat.rb
+++ app/models/users_stat.rb
@@ -5,11 +5,11 @@ class UsersStat
   field :states_count, :type => Hash
 
   index :created_at
-  
+
   # ==========
   # = Scopes =
   # ==========
-  
+
   scope :between, lambda { |start_date, end_date| where(:created_at => { "$gte" => start_date, "$lt" => end_date }) }
 
   # =================
@@ -19,15 +19,15 @@ class UsersStat
   # Recurring task
   def self.delay_create_users_stats
     unless Delayed::Job.already_delayed?('%UsersStat%create_users_stats%')
-      delay(:run_at => Time.new.utc.tomorrow.midnight).create_users_stats # every hour
+      delay(:run_at => Time.now.utc.tomorrow.midnight).create_users_stats # every day
     end
   end
 
   def self.create_users_stats
     delay_create_users_stats
     self.create(:states_count => {
-      :active_and_billable_count     => User.active_and_billable.select("DISTINCT users.id").count,
-      :active_and_not_billable_count => User.active_and_not_billable.select("DISTINCT users.id").count,
+      :active_and_billable_count     => User.active_and_billable.select("DISTINCT(users.id)").count,
+      :active_and_not_billable_count => User.active_and_not_billable.count,
       :suspended_count               => User.with_state(:suspended).count,
       :archived_count                => User.with_state(:archived).count
     })
