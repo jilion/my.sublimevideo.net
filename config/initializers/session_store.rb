9ea9ae37014cc021154f64ef4960c433be623061
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-04-21 10:33:35 +0200

    Updated cookie domain to ".sublimevideo.net"

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-MySublimeVideo::Application.config.session_store :cookie_store, :key => '_my.sublimevideo.net_session', :domain => :all
+MySublimeVideo::Application.config.session_store :cookie_store, :key => '_my.sublimevideo.net_session', :domain => ".sublimevideo.net"
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
