6230d5216eab5f41e25431570f19f317c1f72529
Rémy Coutable <remy@jilion.com>
2011-04-26 16:53:40 +0200

    Added the ability to give a timestamp to UsersStat.create_users_stats (useful in the populate)

--- spec/models/users_stat_spec.rb
+++ spec/models/users_stat_spec.rb
@@ -19,23 +19,29 @@ describe UsersStat do
   end
 
   describe ".create_users_stats" do
+    before(:all) do
+      Factory(:site, plan_id: @paid_plan.id) # active & billable
+
+      Factory(:user) # active & not billable
+      Factory(:site, plan_id: @dev_plan.id) # active & not billable
+      Factory(:site, plan_id: @paid_plan.id).update_attribute(:next_cycle_plan_id, @dev_plan.id) # active & not billable
+
+      Factory(:user, state: 'suspended') # suspended
+      Factory(:user, state: 'archived') # archived
+
+
+      # USERS CREATED 1 DAY AGO
+      Factory(:user, created_at: 1.day.ago) # active & not billable
+
+      Factory(:user, state: 'archived', created_at: 1.day.ago) # archived
+    end
+
     it "should delay itself" do
       UsersStat.should_receive(:delay_create_users_stats)
       UsersStat.create_users_stats
     end
 
     it "should create users stats for states" do
-      user1 = Factory(:user)
-      Factory(:site, user: user1, plan_id: @paid_plan.id)
-      Factory(:site, user: user1, plan_id: @paid_plan.id)
-      user2 = Factory(:user)
-      Factory(:site, user: user1, plan_id: @paid_plan.id).update_attribute(:next_cycle_plan_id, @dev_plan.id)
-      user3 = Factory(:user)
-      Factory(:site, user: user3, plan_id: @dev_plan.id)
-      user4 = Factory(:user)
-      user5 = Factory(:user, :state => 'suspended')
-      user6 = Factory(:user, :state => 'archived')
-
       UsersStat.create_users_stats
 
       UsersStat.count.should == 1
@@ -46,6 +52,21 @@ describe UsersStat do
         "suspended_count"               => 1,
         "archived_count"                => 1
       }
+    end
+
+    context "giving it a timestamp" do
+      it "should create users stats for states" do
+        UsersStat.create_users_stats(1.day.ago)
+
+        UsersStat.count.should == 1
+        users_stat = UsersStat.last
+        users_stat.states_count.should == {
+          "active_and_billable_count"     => 0,
+          "active_and_not_billable_count" => 1,
+          "suspended_count"               => 0,
+          "archived_count"                => 1
+        }
+      end
     end
   end
 
