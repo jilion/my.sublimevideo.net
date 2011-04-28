49b08bdacff21258d01885422c0886444b90bc76
Rémy Coutable <remy@jilion.com>
2011-04-28 10:01:22 +0200

    Fixed PUT /admin/invitation route
    Signed-off-by: Rémy Coutable <remy@jilion.com>

--- config/routes.rb
+++ config/routes.rb
@@ -64,7 +64,7 @@ MySublimeVideo::Application.routes.draw do
       get  :new,    :path => '/admin/admins/invitation/new', :as => 'new'
       post :create, :path => '/admin/admins/invitation'
       get  :edit,   :path => '/admin/invitation/accept', :as => 'accept'
-      put  :update, :path => '/admin/admins/invitation'
+      put  :update, :path => '/admin/invitation'
     end
 
     resource :admin_registration, :only => [], :controller => 'admin/admins/registrations', :path => "" do
