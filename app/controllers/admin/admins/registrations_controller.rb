6716dd9e646011c1c78a9c98327abe296870ada4
Rémy Coutable <remy@jilion.com>
2011-05-09 10:13:48 +0200

    Move Devise path into Module

--- app/controllers/admin/admins/registrations_controller.rb
+++ app/controllers/admin/admins/registrations_controller.rb
@@ -2,11 +2,4 @@ class Admin::Admins::RegistrationsController < Devise::RegistrationsController
   skip_before_filter :authenticate_user!
   before_filter :authenticate_admin!
   layout 'admin'
-
-protected
-
-  def after_update_path_for(resource)
-    edit_admin_registration_url
-  end
-
 end
--- lib/custom_devise_paths.rb
+++ lib/custom_devise_paths.rb
@@ -1,19 +1,20 @@
 module CustomDevisePaths
 
-  def after_sign_in_path_for(resource)
-    if resource == :user || resource.is_a?(User)
+  def after_sign_in_path_for(resource_or_scope)
+    case Devise::Mapping.find_scope!(resource_or_scope)
+    when :user
       sites_path
-    elsif resource == :admin || resource.is_a?(Admin)
+    when :admin
       admin_delayed_jobs_url
     end
   end
 
-  def after_sign_out_path_for(resource)
-    if resource == :user || resource.is_a?(User)
-      new_user_session_url
-    elsif resource == :admin || resource.is_a?(Admin)
-      new_admin_session_url
-    end
+  def after_sign_out_path_for(resource_or_scope)
+    [:new, :"#{Devise::Mapping.find_scope!(resource_or_scope)}_session"]
+  end
+
+  def after_update_path_for(resource_or_scope)
+    [:edit, :"#{Devise::Mapping.find_scope!(resource_or_scope)}_registration"]
   end
 
 end
