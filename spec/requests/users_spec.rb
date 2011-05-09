d435f2bb451c1ff16cb0b22942368dbdaa2c83e9
Rémy Coutable <remy@jilion.com>
2011-05-09 14:32:15 +0200

    Continued API

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -210,32 +210,36 @@ feature "Users" do
     User.last.full_name.should == "John Doe"
   end
 
-  describe "API token" do
-    scenario "create an API token", :focus => true do
+  describe "API token", :focus => true do
+    scenario "create an API token" do
       sign_in_as :user
       @current_user.api_token.should be_nil
       click_link('John Doe')
+      page.should have_content("No token yet")
       click_button "user_api_tokens_submit"
 
       current_url.should =~ %r(^http://[^/]+/account/edit$)
-      @current_user.api_token.should be_present
+      @current_user.reload.api_token.should be_present
       @current_user.api_token.authentication_token.should be_present
+      page.should have_content(@current_user.api_token.authentication_token)
     end
 
-    scenario "reset an API token", :focus => true do
+    scenario "reset an API token" do
       sign_in_as :user
       @current_user.api_token.should be_nil
       click_link('John Doe')
       click_button "user_api_tokens_submit"
 
-      first_api_token = @current_user.api_token.should be_present
-      first_auth_token = @current_user.api_token.authentication_token.should be_present
+      first_api_token = @current_user.reload.api_token
+      first_auth_token = @current_user.api_token.authentication_token
+      page.should have_content(@current_user.api_token.authentication_token)
       click_button "user_api_tokens_submit"
 
       current_url.should =~ %r(^http://[^/]+/account/edit$)
       @current_user.api_token.should == first_api_token
       @current_user.api_token.authentication_token.should be_present
       @current_user.api_token.authentication_token.should_not == first_auth_token
+      page.should have_content(@current_user.api_token.authentication_token)
     end
   end
 
