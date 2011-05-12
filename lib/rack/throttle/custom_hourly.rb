5502bc14b9e22c7743b10eb4ab77950ebcdc3f52
Rémy Coutable <remy@jilion.com>
2011-05-12 11:06:01 +0200

    Added rack-throttle (not ready yet)

--- lib/rack/throttle/custom_hourly.rb
+++ lib/rack/throttle/custom_hourly.rb
@@ -3,8 +3,12 @@ module Rack
     class CustomHourly < Hourly
 
       def client_identifier(request)
+        Rails.logger.debug request.headers
         if request.params["auth_token"]
           request.params["auth_token"]
+        elsif request.headers["Authorization"]
+          Rails.logger.debug 'request.headers["Authorization"]: ' + request.headers["Authorization"]
+          request.headers["Authorization"]
         else
           request.ip.to_s
         end
