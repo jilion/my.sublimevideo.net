b2f889f54bd47af0f216c343ad9f051df7a9d9c5
Rémy Coutable <remy@jilion.com>
2011-06-24 16:13:39 +0200

    Add different root url depending on if the user is logged-in or not, changed the default admin root url

--- config/routes.rb
+++ config/routes.rb
@@ -48,13 +48,19 @@ MySublimeVideo::Application.routes.draw do
   resource :ticket, :only => [:new, :create], :path => '/support', :path_names => { :new =>  '' }
   match '/feedback' => redirect('/support'), :via => :get
 
-  root :to => redirect("/sites")
+  unauthenticated do
+    root :to => redirect('/login')
+  end
+
+  authenticated :user do
+    root :to => redirect('/sites')
+  end
 
   # =========
   # = Admin =
   # =========
 
-  match 'admin', :to => redirect('/admin/djs'), :as => 'admin'
+  match 'admin', :to => redirect('/admin/sites'), :as => 'admin'
 
   devise_for :admins,
   :path => 'admin',
