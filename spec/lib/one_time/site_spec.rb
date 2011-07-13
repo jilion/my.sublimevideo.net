ac7678a52066692ecff937a7c6c6150f17022a1a
Rémy Coutable <remy@jilion.com>
2011-07-13 14:39:30 +0200

    Added a new rake task to update sites with invalid extra hostnames

--- spec/lib/one_time/site_spec.rb
+++ spec/lib/one_time/site_spec.rb
@@ -25,7 +25,7 @@ describe OneTime::Site do
     it "should rollback beta site to active state with dev plan" do
       expect { described_class.rollback_beta_sites_to_dev }.to change(Delayed::Job, :count).by(2)
       @worker.work_off
-      
+
       @site1.reload.should be_active
       @site1.should be_in_dev_plan
       @site1.license.read.should_not == @old_license1
@@ -51,12 +51,42 @@ describe OneTime::Site do
       Factory(:site, state: 'archived')
     end
 
-    it "should regenerate loader and license of all sites" do
+    it "regenerates loader and license of all sites" do
       Delayed::Job.delete_all
       count_before = Delayed::Job.where(:handler.matches => "%update_loader_and_license%").count
       lambda { described_class.regenerate_all_loaders_and_licenses }.should change(Delayed::Job, :count).by(2)
       djs = Delayed::Job.where(:handler.matches => "%update_loader_and_license%")
       djs.count.should == count_before + 2
+    end
+  end
+
+  describe ".move_local_ip_in_extra_domains_to_dev_domains" do
+    before(:all) do
+      ::Site.delete_all
+      @invalid1 = Factory(:site, dev_hostnames: "localhost")
+      @invalid1.update_attribute(:extra_hostnames, "google.com, 172.16.4.165")
+      @invalid2 = Factory(:site, dev_hostnames: "127.0.0.1")
+      @invalid2.update_attribute(:extra_hostnames, "172.16.4.165")
+
+      @valid1 = Factory(:site, dev_hostnames: "localhost", extra_hostnames: "google.com")
+      @valid2 = Factory(:site, dev_hostnames: "127.0.0.1", extra_hostnames: "google.com")
+
+      @archived = Factory(:site, state: 'archived', dev_hostnames: nil)
+      @archived.update_attribute(:extra_hostnames, "172.16.4.165")
+    end
+
+    it "moves local domains present in extra domains into dev domains" do
+      described_class.move_local_ip_in_extra_domains_to_dev_domains
+
+      @invalid1.reload.dev_hostnames.should == "172.16.4.165, localhost"
+      @invalid1.extra_hostnames.should == "google.com"
+      @invalid2.reload.dev_hostnames.should == "127.0.0.1, 172.16.4.165"
+      @invalid2.extra_hostnames.should == ""
+
+      @valid1.reload.dev_hostnames.should == "localhost"
+      @valid1.extra_hostnames.should == "google.com"
+      @valid2.reload.dev_hostnames.should == "127.0.0.1"
+      @valid2.extra_hostnames.should == "google.com"
     end
   end
 
