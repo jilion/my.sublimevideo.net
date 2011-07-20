8aa19a270971b5d50d654b70d97d75fca0ec88da
Rémy Coutable <remy@jilion.com>
2011-07-20 12:25:22 +0200

    Allow users to enter a credit card at any moment
    Change the labels accordingly (Register/Update)

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,9 +1,4 @@
 class CreditCardsController < ApplicationController
-  before_filter do |controller|
-    if !current_user.cc? && !current_user.pending_cc? && !current_user.invoices_failed? && !current_user.invoices_open?
-      redirect_to [:edit, :user_registration]
-    end
-  end
 
   # GET /card/edit
   def edit
