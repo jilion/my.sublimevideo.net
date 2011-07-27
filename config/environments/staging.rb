f67797f0da6bf0561d737455fc52710179c93dc5
Rémy Coutable <remy@jilion.com>
2011-07-27 17:28:21 +0200

    Don't require the staging code on /oauth and /api routes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,7 +1,7 @@
 MySublimeVideo::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
   config.middleware.use(Rack::SslEnforcer, :except => /^\/r\// )
-  config.middleware.use(Rack::Private, :code => ENV['STAGING_CODE'])
+  config.middleware.use(Rack::Private, :code => ENV['STAGING_CODE'], :except => [/^oauth\/.+$$/, /^api\/.+$/])
 
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
