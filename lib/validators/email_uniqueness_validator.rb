9d006c71315169c02f1940b12d0cf254174b8e3b
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-08-11 16:46:51 +0200

    Fix for squeel

--- lib/validators/email_uniqueness_validator.rb
+++ lib/validators/email_uniqueness_validator.rb
@@ -1,9 +1,9 @@
 class EmailUniquenessValidator < ActiveModel::EachValidator
-  
+
   def validate_each(record, attribute, email)
-    if email.present? && User.without_state(:archived).where(:lower.func(:email) => :lower.func(email), :id.not_eq => record.id).exists?
+    if email.present? && User.without_state(:archived).where(:email => email.downcase, :id.not_eq => record.id).exists?
       record.errors.add(attribute, :taken)
     end
   end
-  
+
 end
