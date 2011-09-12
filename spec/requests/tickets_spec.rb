fc33cb3cca76c4929a34429bfb7446f0419da0ae
Rémy Coutable <remy@jilion.com>
2011-09-12 15:28:17 +0200

    Support page update

--- spec/requests/tickets_spec.rb
+++ spec/requests/tickets_spec.rb
@@ -12,65 +12,101 @@ feature "Checks" do
 end
 
 feature "Support" do
-  background do
-    sign_in_as :user
-    visit "/support"
+  context "user has only the 'forum' support level" do
+    background do
+      sign_in_as :user
+      FactoryGirl.create(:site, user: @current_user, plan_id: @free_plan.id)
+      visit "/support"
+    end
+
+    scenario "navigation" do
+      click_link "Support"
+      current_url.should =~ %r(http://[^/]+/support)
+    end
+
+    describe "new" do
+      scenario "doesn't have access to all ticket types" do
+        page.should have_no_content('Integration question')
+      end
+
+      scenario "doesn't have access to the form" do
+        page.should have_no_content('use the form below')
+        page.should have_no_selector('form.new_ticket')
+      end
+    end
   end
 
-  scenario "navigation" do
-    click_link "Support"
-    current_url.should =~ %r(http://[^/]+/support)
-  end
-
-  describe "new" do
-    scenario "submit a valid ticket" do
-      select "Bug report", :from => "ticket_type"
-      fill_in "Subject", :with => "I have a request!"
-      fill_in "Message", :with => "I have a request this is a long text!"
-      click_button "Send"
-
-      page.should have_content I18n.t('flash.tickets.create.notice')
-
-      Delayed::Job.last.name.should == 'Class#post_ticket'
-      VCR.use_cassette("ticket/post_ticket") { @worker.work_off }
-      Delayed::Job.last.should be_nil
-      @current_user.reload.zendesk_id.should be_present
+  context "user has the 'email' support level" do
+    background do
+      sign_in_as :user
+      FactoryGirl.create(:site, user: @current_user, plan_id: @paid_plan.id)
+      visit "/support"
     end
 
-    scenario "submit a ticket with an invalid type" do
-      select "Choose a category", :from => "ticket_type"
-      fill_in "Subject", :with => "I have a request!"
-      fill_in "Message", :with => "I have a request this is a long text!"
-      click_button "Send"
-
+    scenario "navigation" do
+      click_link "Support"
       current_url.should =~ %r(http://[^/]+/support)
-      page.should have_content "You must choose a category"
-      page.should have_no_content I18n.t('flash.tickets.create.notice')
-      Delayed::Job.last.should_not == 'Class#post_ticket'
     end
 
-    scenario "submit a ticket with an invalid subject" do
-      select "Feature request / Improvement suggestion", :from => "ticket_type"
-      fill_in "Subject", :with => ""
-      fill_in "Message", :with => "I have a request this is a long text!"
-      click_button "Send"
+    describe "new", :focus => true do
+      scenario "has access to all ticket types" do
+        page.should have_content('Integration question')
+      end
 
-      current_url.should =~ %r(http://[^/]+/support)
-      page.should have_content "Subject can't be blank"
-      page.should have_no_content I18n.t('flash.tickets.create.notice')
-      Delayed::Job.last.should_not == 'Class#post_ticket'
-    end
+      scenario "has access to the form" do
+        page.should have_content('use the form below')
+        page.should have_selector('form.new_ticket')
+      end
 
-    scenario "submit a ticket with an invalid message" do
-      select "Feature request / Improvement suggestion", :from => "ticket_type"
-      fill_in "Subject", :with => "I have a request!"
-      fill_in "Message", :with => ""
-      click_button "Send"
+      scenario "submit a valid ticket" do
+        select "Bug report", :from => "ticket_type"
+        fill_in "Subject", :with => "I have a request!"
+        fill_in "Message", :with => "I have a request this is a long text!"
+        expect { click_button "Send" }.to change(Delayed::Job.where(:handler.matches => "%post_ticket%"), :count).by(1)
 
-      current_url.should =~ %r(http://[^/]+/support)
-      page.should have_content "Message can't be blank"
-      page.should have_no_content I18n.t('flash.tickets.create.notice')
-      Delayed::Job.last.should_not == 'Class#post_ticket'
+        page.should have_content I18n.t('flash.tickets.create.notice')
+
+        VCR.use_cassette("ticket/post_ticket") do
+          expect { @worker.work_off }.to change(Delayed::Job.where(:handler.matches => "%post_ticket%"), :count).by(-1)
+        end
+        @current_user.reload.zendesk_id.should be_present
+      end
+
+      scenario "submit a ticket with an invalid type" do
+        select "Choose a category", :from => "ticket_type"
+        fill_in "Subject", :with => "I have a request!"
+        fill_in "Message", :with => "I have a request this is a long text!"
+        click_button "Send"
+
+        current_url.should =~ %r(http://[^/]+/support)
+        page.should have_content "You must choose a category"
+        page.should have_no_content I18n.t('flash.tickets.create.notice')
+        Delayed::Job.last.should_not == 'Class#post_ticket'
+      end
+
+      scenario "submit a ticket with an invalid subject" do
+        select "Feature request / Improvement suggestion", :from => "ticket_type"
+        fill_in "Subject", :with => ""
+        fill_in "Message", :with => "I have a request this is a long text!"
+        click_button "Send"
+
+        current_url.should =~ %r(http://[^/]+/support)
+        page.should have_content "Subject can't be blank"
+        page.should have_no_content I18n.t('flash.tickets.create.notice')
+        Delayed::Job.last.should_not == 'Class#post_ticket'
+      end
+
+      scenario "submit a ticket with an invalid message" do
+        select "Feature request / Improvement suggestion", :from => "ticket_type"
+        fill_in "Subject", :with => "I have a request!"
+        fill_in "Message", :with => ""
+        click_button "Send"
+
+        current_url.should =~ %r(http://[^/]+/support)
+        page.should have_content "Message can't be blank"
+        page.should have_no_content I18n.t('flash.tickets.create.notice')
+        Delayed::Job.last.should_not == 'Class#post_ticket'
+      end
     end
   end
 
