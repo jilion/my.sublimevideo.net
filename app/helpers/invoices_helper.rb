97f818f6e75aaa24df04b648ac3182d9a236ee4b
Rémy Coutable <remy@jilion.com>
2011-09-22 11:41:18 +0200

    Fix wrong order for if/elsif statements for the invoice status display, also display a "refunded" stamp when relevant

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -15,10 +15,6 @@ module InvoicesHelper
   def charging_status(invoice)
     if invoice.open?
       "Not yet paid."
-    elsif invoice.paid?
-      "Paid on #{l(invoice.paid_at, :format => :minutes_timezone)}."
-    elsif invoice.refunded?
-      "Paid on #{l(invoice.paid_at, :format => :minutes_timezone)}, refunded on #{l(invoice.site.refunded_at, :format => :minutes_timezone)}."
     elsif invoice.failed?
       text = "#{content_tag(:strong, "Payment failed", :class => "failed")} on #{l(invoice.last_failed_at, :format => :minutes_timezone)}".html_safe
       unless invoice.last_transaction.error =~ /secure.ogone/ # FIXME we store the 3d secure html in this field,
@@ -27,8 +23,22 @@ module InvoicesHelper
         text += " with the following error: #{content_tag(:em, "\"#{truncate(invoice.last_transaction.error, :length => 50)}\"")}".html_safe
       end
       text + "."
+    elsif invoice.refunded?
+      "Paid on #{l(invoice.paid_at, :format => :minutes_timezone)}, refunded on #{l(invoice.site.refunded_at, :format => :minutes_timezone)}."
+    elsif invoice.paid?
+      "Paid on #{l(invoice.paid_at, :format => :minutes_timezone)}."
     elsif invoice.waiting?
       "Waiting payment confirmation."
+    end
+  end
+
+  def stamp(invoice)
+    if invoice.failed?
+      'failed'
+    elsif invoice.paid?
+      invoice.refunded? ? 'refunded' : 'paid'
+    else
+      invoice.state
     end
   end
 
