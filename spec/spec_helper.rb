20b94ba7d18bc31757161b19463921b7d7850305
Rémy Coutable <remy@jilion.com>
2011-10-04 16:39:05 +0200

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,6 +32,7 @@ Spork.prefork do
 
     config.filter_run :focus => true
     config.run_all_when_everything_filtered = true
+    config.treat_symbols_as_metadata_keys_with_true_values = true
 
     config.mock_with :rspec
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
