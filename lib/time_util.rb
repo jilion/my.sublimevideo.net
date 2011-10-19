4a63ff9712ecdfb36a3c7832d4ec60221fa0b890
Rémy Coutable <remy@jilion.com>
2011-10-19 18:46:39 +0200

    Refactor TimeUtil module

--- lib/time_util.rb
+++ lib/time_util.rb
@@ -2,16 +2,15 @@ module TimeUtil
 
   class << self
 
-    def full_month(time = Time.now.utc)
+    def current_month(time = Time.now.utc)
       [time.beginning_of_month, time.end_of_month]
     end
-    alias :current_month :full_month
 
-    def prev_full_month(time = Time.now.utc)
+    def prev_month(time = Time.now.utc)
       [time.prev_month.beginning_of_month, time.prev_month.end_of_month]
     end
 
-    def next_full_month(time = Time.now.utc)
+    def next_month(time = Time.now.utc)
       [time.next_month.beginning_of_month, time.next_month.end_of_month]
     end
 
