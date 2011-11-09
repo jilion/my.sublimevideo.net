a1e3623075b04f1faa78fecf8b7219a8305b80ba
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-09 20:18:05 +0100

    Improve video_spec

--- spec/models/stat/video_spec.rb
+++ spec/models/stat/video_spec.rb
@@ -38,30 +38,39 @@ describe Stat::Video do
 
     context "last 61 seconds" do
       before(:each) do
-        6.times do |video_i|
-          Factory.create(:video_tag, st: 'site1234', u: "video#{video_i}", n: "Video #{video_i}", no: 'a')
-          61.times do |second_i|
-            next if second_i%2 == 0
-            Factory.create(:video_stat, st: 'site1234', u: "video#{video_i}", s: (second_i).seconds.ago.utc.change(usec: 0),
-              vv: { m: video_i + second_i, e: video_i + second_i },
-              vl: { m: video_i * second_i, e: video_i * second_i }
-            )
+        @second = Time.now
+        Timecop.freeze @second do
+          6.times do |video_i|
+            Factory.create(:video_tag, st: 'site1234', u: "video#{video_i}", n: "Video #{video_i}", no: 'a')
+            61.times do |second_i|
+              next if second_i%2 == 0
+              Factory.create(:video_stat, st: 'site1234', u: "video#{video_i}", s: (second_i).seconds.ago.utc.change(usec: 0),
+                vv: { m: video_i + second_i, e: video_i + second_i },
+                vl: { m: video_i * second_i, e: video_i * second_i }
+              )
+            end
           end
         end
       end
 
       specify { Stat::Video.top_videos('site1234', period: 'seconds', count: 1)[:videos].size.should == 6 }
       specify { Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:total].should == 6 }
-      specify { Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:from].should == 60.seconds.ago.utc.change(usec: 0).to_i }
+      specify {
+        Timecop.freeze @second do
+          Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:from].should == 60.seconds.ago.utc.change(usec: 0).to_i
+        end
+      }
 
       it "replaces vv_hash by vv_array and vl_hash by vl_hash" do
-        video = Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:videos].first
-        video["vv_array"].should eql([0, 128, 0, 124, 0, 120, 0, 116, 0, 112, 0, 108, 0, 104, 0, 100, 0, 96, 0, 92, 0, 88, 0, 84, 0, 80, 0, 76, 0, 72, 0, 68, 0, 64, 0, 60, 0, 56, 0, 52, 0, 48, 0, 44, 0, 40, 0, 36, 0, 32, 0, 28, 0, 24, 0, 20, 0, 16, 0, 12, 0])
-        video["vv_array"].size.should eql(61)
-        video["vv_hash"].should be_nil
-        video["vl_array"].should eql([0, 590, 0, 570, 0, 550, 0, 530, 0, 510, 0, 490, 0, 470, 0, 450, 0, 430, 0, 410, 0, 390, 0, 370, 0, 350, 0, 330, 0, 310, 0, 290, 0, 270, 0, 250, 0, 230, 0, 210, 0, 190, 0, 170, 0, 150, 0, 130, 0, 110, 0, 90, 0, 70, 0, 50, 0, 30, 0, 10, 0])
-        video["vl_array"].size.should eql(61)
-        video["vl_hash"].should be_nil
+        Timecop.freeze @second do
+          video = Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:videos].first
+          video["vv_array"].should eql([0, 128, 0, 124, 0, 120, 0, 116, 0, 112, 0, 108, 0, 104, 0, 100, 0, 96, 0, 92, 0, 88, 0, 84, 0, 80, 0, 76, 0, 72, 0, 68, 0, 64, 0, 60, 0, 56, 0, 52, 0, 48, 0, 44, 0, 40, 0, 36, 0, 32, 0, 28, 0, 24, 0, 20, 0, 16, 0, 12, 0])
+          video["vv_array"].size.should eql(61)
+          video["vv_hash"].should be_nil
+          video["vl_array"].should eql([0, 590, 0, 570, 0, 550, 0, 530, 0, 510, 0, 490, 0, 470, 0, 450, 0, 430, 0, 410, 0, 390, 0, 370, 0, 350, 0, 330, 0, 310, 0, 290, 0, 270, 0, 250, 0, 230, 0, 210, 0, 190, 0, 170, 0, 150, 0, 130, 0, 110, 0, 90, 0, 70, 0, 50, 0, 30, 0, 10, 0])
+          video["vl_array"].size.should eql(61)
+          video["vl_hash"].should be_nil
+        end
       end
     end
 
