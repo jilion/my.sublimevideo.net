71db32bcfd2327fbb8299099581bc774c48cf340
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-10 09:45:59 +0100

    Fix video_spec

--- spec/models/stat/video_spec.rb
+++ spec/models/stat/video_spec.rb
@@ -38,7 +38,7 @@ describe Stat::Video do
 
     context "last 61 seconds" do
       before(:each) do
-        @second = Time.now
+        @second = Time.now.change(usec: 0)
         Timecop.freeze @second do
           6.times do |video_i|
             Factory.create(:video_tag, st: 'site1234', u: "video#{video_i}", n: "Video #{video_i}", no: 'a')
@@ -64,10 +64,10 @@ describe Stat::Video do
       it "replaces vv_hash by vv_array and vl_hash by vl_hash" do
         Timecop.freeze @second do
           video = Stat::Video.top_videos('site1234', period: 'seconds', count: 5)[:videos].first
-          video["vv_array"].should eql([0, 128, 0, 124, 0, 120, 0, 116, 0, 112, 0, 108, 0, 104, 0, 100, 0, 96, 0, 92, 0, 88, 0, 84, 0, 80, 0, 76, 0, 72, 0, 68, 0, 64, 0, 60, 0, 56, 0, 52, 0, 48, 0, 44, 0, 40, 0, 36, 0, 32, 0, 28, 0, 24, 0, 20, 0, 16, 0, 12, 0])
+          video["vv_array"].should eql([0, 126, 0, 122, 0, 118, 0, 114, 0, 110, 0, 106, 0, 102, 0, 98, 0, 94, 0, 90, 0, 86, 0, 82, 0, 78, 0, 74, 0, 70, 0, 66, 0, 62, 0, 58, 0, 54, 0, 50, 0, 46, 0, 42, 0, 38, 0, 34, 0, 30, 0, 26, 0, 22, 0, 18, 0, 14, 0, 10, 0])
           video["vv_array"].size.should eql(61)
           video["vv_hash"].should be_nil
-          video["vl_array"].should eql([0, 590, 0, 570, 0, 550, 0, 530, 0, 510, 0, 490, 0, 470, 0, 450, 0, 430, 0, 410, 0, 390, 0, 370, 0, 350, 0, 330, 0, 310, 0, 290, 0, 270, 0, 250, 0, 230, 0, 210, 0, 190, 0, 170, 0, 150, 0, 130, 0, 110, 0, 90, 0, 70, 0, 50, 0, 30, 0, 10, 0])
+          video["vl_array"].should eql([0, 472, 0, 456, 0, 440, 0, 424, 0, 408, 0, 392, 0, 376, 0, 360, 0, 344, 0, 328, 0, 312, 0, 296, 0, 280, 0, 264, 0, 248, 0, 232, 0, 216, 0, 200, 0, 184, 0, 168, 0, 152, 0, 136, 0, 120, 0, 104, 0, 88, 0, 72, 0, 56, 0, 40, 0, 24, 0, 8, 0])
           video["vl_array"].size.should eql(61)
           video["vl_hash"].should be_nil
         end
