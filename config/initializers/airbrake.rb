e0e05f841ee741dc696625fc16511c9a6fdaff5b
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-14 08:53:47 +0100

    Switch to Airbrake from HoptoadNotifier gem Add Airbrake js support

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
-HoptoadNotifier.configure do |config|
+Airbrake.configure do |config|
   config.api_key = 'd83280c89d59e2babd2e3ee1c546e8d4'
   config.ignore  << ActionController::UnknownHttpMethod
   config.ignore  << Errno::EPIPE
