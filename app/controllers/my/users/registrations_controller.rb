588c795177f2a1f93281d72271a9d152afd0da1e
Rémy Coutable <remy@jilion.com>
2011-11-17 20:05:28 +0100

    Ensure user is redirected to 'my' sites after sign up

--- app/controllers/my/users/registrations_controller.rb
+++ app/controllers/my/users/registrations_controller.rb
@@ -12,11 +12,11 @@ class My::Users::RegistrationsController < Devise::RegistrationsController
       if @user.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
         sign_in(resource_name, @user)
-        respond_with resource, :location => redirect_location(resource_name, @user)
+        respond_with resource, location: sites_url(subdomain: 'my')
       else
-        set_flash_message :notice, :inactive_signed_up, :reason => inactive_reason(@user) if is_navigational_format?
+        set_flash_message :notice, :inactive_signed_up, reason: inactive_reason(@user) if is_navigational_format?
         expire_session_data_after_sign_in!
-        respond_with @user, :location => after_inactive_sign_up_path_for(@user)
+        respond_with @user, location: after_inactive_sign_up_path_for(@user)
       end
     else
       clean_up_passwords(@user)
