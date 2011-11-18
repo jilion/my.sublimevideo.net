6ef5575aeb9aadd2b6c99b0de4f20a01b0be003e
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-18 08:33:04 +0100

    Cherry pick Rails components

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# Pick the frameworks you want:
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "active_resource/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # If you have a Gemfile, require the default gems, the ones in the
 # current environment and also include :assets gems if in development
