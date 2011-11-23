4332916d67da6abf3c9cd890792d7142a40dc6d2
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-23 14:45:42 +0100

    Properly setup SendGrid for Cedar Heroku Stack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,17 @@ MySublimeVideo::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'sublimevideo.net' }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "587",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
+
   # Enable threaded mode
   # config.threadsafe!
 
