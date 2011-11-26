6babd5cc37a2e9553257adefffaa1e84dd771939
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-26 01:18:23 +0100

    Force staging fog_directory

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -3,10 +3,8 @@ AssetSync.configure do |config|
   config.aws_access_key_id     = 'AKIAJVHSNREHOLGAAR4A'
   config.aws_secret_access_key = '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'
 
-  case Rails.env
-  when 'staging';    config.fog_directory = "assets.sublimevideo-staging.net"
-  when 'production'; config.fog_directory = "assets.sublimevideo.net"
-  end
+  config.fog_directory = "assets.sublimevideo-staging.net"
+  # config.fog_directory = "assets.sublimevideo.net"
 
   config.existing_remote_files = "delete"
   # config.gzip_compression = true # don't working, doesn't uploads gz files version
