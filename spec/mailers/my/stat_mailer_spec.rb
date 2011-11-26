626171e7fd81e070e4afab492cf8e342ba90392c
Rémy Coutable <remy@jilion.com>
2011-11-26 12:06:16 +0100

    In emails, replace links to docs + GS by a link to the help + improve confirmation_instructions email

--- spec/mailers/my/stat_mailer_spec.rb
+++ spec/mailers/my/stat_mailer_spec.rb
@@ -20,7 +20,7 @@ describe My::StatMailer do
       @last_delivery.body.encoded.should include "2 days"
       @last_delivery.body.encoded.should include I18n.l(@site.stats_trial_ended_at, format: :named_date)
       @last_delivery.body.encoded.should include "https://my.#{ActionMailer::Base.default_url_options[:host]}/sites/#{@site.to_param}/plan/edit"
-      @last_delivery.body.encoded.should include "https://docs.#{ActionMailer::Base.default_url_options[:host]}"
+      @last_delivery.body.encoded.should include "https://www.#{ActionMailer::Base.default_url_options[:host]}/help"
     end
   end
 
