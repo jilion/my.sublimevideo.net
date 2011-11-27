a31fb9e24415903926a4769ee6af80fb6a708ad3
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-27 13:34:23 +0100

    Skip asset compression for debug
    Need to doing that on production.rb because assets:precompile rake task
    is always done in that environment

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -16,8 +16,7 @@ MySublimeVideo::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  # config.assets.compress = true
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
