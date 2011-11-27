0134215cef54c1aa4c9def59e05e1f2c8c5be375
Rémy Coutable <remy@jilion.com>
2011-11-27 21:47:12 +0100

    Admin tweaks

--- app/helpers/docs_helper.rb
+++ app/helpers/docs_helper.rb
@@ -1,12 +1,12 @@
 module DocsHelper
-  
+
   def page_title(title)
     content_for :title, title
     content_tag :h2 do
       title
     end
   end
-  
+
   def li_with_page_link(page, options = {}, &block)
     title = page.split('#').last
     title = title.split('/').last.gsub("-"," ").humanize if title == page
@@ -14,5 +14,5 @@ module DocsHelper
       link_to(options[:title] || title, "/#{page}") + (block_given? ? capture_haml { yield } : '')
     end
   end
-  
+
 end
