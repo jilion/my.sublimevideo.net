7cbafb015539deb23e337873c2bc1d61c9473674
Rémy Coutable <remy@jilion.com>
2011-11-28 20:19:10 +0100

    Showcases => Showcase

--- spec/routings/www/pages_routing_spec.rb
+++ spec/routings/www/pages_routing_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Www::PagesController do
 
   it { get(with_subdomain('www', '/')).should route_to('www/pages#show', page: 'home') }
-  %w[features plans demo customer-showcases vision contact].each do |page|
+  %w[features plans demo customer-showcase vision contact].each do |page|
     it { get(with_subdomain('www', page)).should route_to('www/pages#show', page: page) }
   end
 
