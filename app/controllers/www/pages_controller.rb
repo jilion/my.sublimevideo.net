3c5773243deb104f37bf62105c50c74eceff9428
Rémy Coutable <remy@jilion.com>
2011-11-28 14:17:23 +0100

    Use fragment caching instead of page caching for the doc pages

--- app/controllers/www/pages_controller.rb
+++ app/controllers/www/pages_controller.rb
@@ -4,11 +4,7 @@ class Www::PagesController < ApplicationController
 
   def show
     params[:page] ||= 'home'
-    if params[:p] && user_signed_in?
-      redirect_to sites_url(subdomain: 'my')
-    else
-      render params[:page]
-    end
+    render params[:page]
   end
 
 end
