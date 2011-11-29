fa43cf3aa3227c0b18d148edf910f8ccb03a1a84
Rémy Coutable <remy@jilion.com>
2011-11-29 09:29:40 +0100

    Page cache the releases page only when requested for atom

--- app/controllers/docs/releases_controller.rb
+++ app/controllers/docs/releases_controller.rb
@@ -1,7 +1,7 @@
 class Docs::ReleasesController < ApplicationController
   respond_to :atom
 
-  before_filter :cache_page
+  before_filter :cache_page, if: proc { |c| request.format == :atom }
 
   def index
     @releases = Docs::Release.all.reverse
