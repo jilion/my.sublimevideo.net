9882f8190dbbee6decc8ca14b955ddeebefaa723
Rémy Coutable <remy@jilion.com>
2011-11-29 01:23:16 +0100

    Spec fixing

--- spec/routings/my/users/sessions_routing_spec.rb
+++ spec/routings/my/users/sessions_routing_spec.rb
@@ -4,8 +4,8 @@ describe My::Users::SessionsController do
 
   it { get(with_subdomain('www', '?p=login')).should route_to('www/pages#show', page: 'home') }
 
-  it { get('gs-login').should  route_to('my/users/sessions#new_gs') }
-  it { post('gs-login').should route_to('my/users/sessions#create_gs') }
+  it { get(with_subdomain('my', 'gs-login')).should  route_to('my/users/sessions#new_gs') }
+  it { post(with_subdomain('my', 'gs-login')).should route_to('my/users/sessions#create_gs') }
   it { post('login').should    route_to('my/users/sessions#create') }
 
 end
