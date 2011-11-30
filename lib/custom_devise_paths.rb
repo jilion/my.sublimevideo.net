e77f9cf9437691e38b4322fae02eb69a195a4d8d
Rémy Coutable <remy@jilion.com>
2011-11-30 11:57:43 +0100

    Ensure after sign-in we redirect to 'my' if there is a stored location
    For example if the WordPress plugin's authorize needs the user to authenticate first (since we redirect to 'www' for the login, we have then to redirect him to 'my' after the sign-in).

--- lib/custom_devise_paths.rb
+++ lib/custom_devise_paths.rb
@@ -1,11 +1,15 @@
 module CustomDevisePaths
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || case Devise::Mapping.find_scope!(resource_or_scope)
-    when :user
-      sites_url(subdomain: 'my')
-    when :admin
-      admin_sites_url(subdomain: 'admin')
+    if stored_path = stored_location_for(resource_or_scope)
+      "#{request.protocol}my.#{request.domain}#{stored_path}"
+    else
+      case Devise::Mapping.find_scope!(resource_or_scope)
+      when :user
+        sites_url(subdomain: 'my')
+      when :admin
+        admin_sites_url(subdomain: 'admin')
+      end
     end
   end
 
