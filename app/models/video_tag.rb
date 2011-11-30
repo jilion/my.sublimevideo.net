0693b74774d640457e67a0ecb179e872407f031f
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2011-11-30 09:09:36 +0100

    Add notify to understand Addressable error

--- app/models/video_tag.rb
+++ app/models/video_tag.rb
@@ -78,26 +78,30 @@ private
     trackers   = only_video_tags_trackers(trackers)
     video_tags = Hash.new { |h,k| h[k] = Hash.new }
     trackers.each do |request, hits|
-      params = Addressable::URI.parse(CGI.unescape(request)).query_values || {}
-      if %w[m e].include?(params['h'])
-        case params['e']
-        when 'l'
-          if all_needed_params_present?(params, %w[vu pz])
-            params['vu'].each_with_index do |vu, index|
-              video_tags[[params['t'],vu]]['z'] = params['pz'][index]
+      begin
+        params = Addressable::URI.parse(CGI.unescape(request)).query_values || {}
+        if %w[m e].include?(params['h'])
+          case params['e']
+          when 'l'
+            if all_needed_params_present?(params, %w[vu pz])
+              params['vu'].each_with_index do |vu, index|
+                video_tags[[params['t'],vu]]['z'] = params['pz'][index]
+              end
             end
-          end
-        when 's'
-          if all_needed_params_present?(params, %w[t vu vuo vn vno vs vc vcs vsq vsf vp])
-            %w[uo n no p cs].each do |key|
-              video_tags[[params['t'],params['vu']]][key] = params["v#{key}"]
+          when 's'
+            if all_needed_params_present?(params, %w[t vu vuo vn vno vs vc vcs vsq vsf vp])
+              %w[uo n no p cs].each do |key|
+                video_tags[[params['t'],params['vu']]][key] = params["v#{key}"]
+              end
+              # Video sources
+              video_tags[[params['t'],params['vu']]]['s'] ||= {}
+              video_tags[[params['t'],params['vu']]]['s'][params['vc']] = { 'u' => params['vs'], 'q' => params['vsq'], 'f' => params['vsf'] }
+              video_tags[[params['t'],params['vu']]]['s'][params['vc']]['r'] = params['vsr'] if params['vsr'].present?
             end
-            # Video sources
-            video_tags[[params['t'],params['vu']]]['s'] ||= {}
-            video_tags[[params['t'],params['vu']]]['s'][params['vc']] = { 'u' => params['vs'], 'q' => params['vsq'], 'f' => params['vsf'] }
-            video_tags[[params['t'],params['vu']]]['s'][params['vc']]['r'] = params['vsr'] if params['vsr'].present?
           end
         end
+      rescue => ex
+        Notify.send("VideoTag parsing failed (request: '#{request}'): #{ex.message}", exception: ex)
       end
     end
     video_tags
