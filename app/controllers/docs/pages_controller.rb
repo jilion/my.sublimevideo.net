eda358002d2f4dd59dab2c1951f64d2b6750ad27
Rémy Coutable <remy@jilion.com>
2011-12-05 12:19:20 +0100

    Disable action caching for the docs

--- app/controllers/docs/pages_controller.rb
+++ app/controllers/docs/pages_controller.rb
@@ -1,6 +1,6 @@
 class Docs::PagesController < ApplicationController
 
-  caches_action :show, layout: false
+  # caches_action :show, layout: false
 
   def show
     render "docs/pages/#{params[:page]}"
