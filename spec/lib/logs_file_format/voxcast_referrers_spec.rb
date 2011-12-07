f8cd188aa1cb72ad86bb01056c759c0b0dd447e0
Rémy Coutable <remy@jilion.com>
2011-12-07 17:09:23 +0100

    Count only _.gif request with e=l for the referrers

--- spec/lib/logs_file_format/voxcast_referrers_spec.rb
+++ spec/lib/logs_file_format/voxcast_referrers_spec.rb
@@ -1,24 +1,20 @@
 require 'spec_helper'
 
 describe LogsFileFormat::VoxcastReferrers do
-  
+
   describe "with cdn.sublimevideo.net.log.1284549900-1284549960.gz logs file" do
     before(:each) do
       log_file = File.new(Rails.root.join('spec/fixtures/logs/voxcast/cdn.sublimevideo.net.log.1284549900-1284549960.gz'))
       @trackers = LogAnalyzer.parse(log_file, 'LogsFileFormat::VoxcastReferrers')
     end
-    
+
     it "should parse and return loader tracker" do
       tracker = @trackers.detect { |t| t.options[:title] == :referrers }
       tracker.categories.should == {
-        ["http://www.romainkedochim.com/blog/live-music/news-sublime-video-player-in-html5-first-test-with-clive-selwyn/", "0w1o1q3c"] => 2,
-        ["http://www.killy.net/", "k8qaaj1l"] => 3,
-        ["http://sublimevideo.net/demo", "ibvjcopp"] => 2,
-        ["http://ben.mohu.local/", "cjbace0k"] => 1,
-        ["http://www.killy.net/?paged=2", "k8qaaj1l"] => 1,
-        ["http://pineapplepark.com.au/", "hp1lepyq"] => 1,
-        ["http://capped.tv/xplsv-or_so_they_say", "zf8jbler"] => 1
+        ["http://www.sublimevideo.net/demo", "ibvjcopp"] => 1,
+        ["http://sublimevideo.net/demo", "ibvjcopp"] => 2
       }
     end
   end
+
 end
