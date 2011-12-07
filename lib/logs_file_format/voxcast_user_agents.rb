ecc25b7f443f7f91f47fc7618ee7e4b5b49b03b4
Rémy Coutable <remy@jilion.com>
2011-12-07 17:08:55 +0100

    Refactor a bit LogsFileFormat::Voxcast

--- lib/logs_file_format/voxcast_user_agents.rb
+++ lib/logs_file_format/voxcast_user_agents.rb
@@ -5,8 +5,8 @@ module LogsFileFormat
     def self.report_trackers
       analyze = RequestLogAnalyzer::Aggregator::Summarizer::Definer.new
       analyze.frequency(:useragent, :title => :useragent,
-        :category => lambda { |r| [r[:useragent], player_token_from(r[:path])] },
-        :if       => lambda { |r| player_token?(r[:path]) && countable_hit?(r) }
+        :category => lambda { |r| [r[:useragent], player_token_from(r)] },
+        :if       => lambda { |r| player_token?(r) && countable_hit?(r) }
       )
       analyze.trackers
     end
