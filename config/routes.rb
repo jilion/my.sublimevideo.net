3929b02af8c3178e3b5a70e0be9ca73f9b7581f6
Octave Zangs <octavez@octavez.com>
2011-12-07 17:05:25 +0100

    Force download of the press kit in News page

--- config/routes.rb
+++ config/routes.rb
@@ -266,7 +266,7 @@ MySublimeVideo::Application.routes.draw do
       match '/enthusiasts(/:anything)' => redirect('/')
 
       get '/pr/:page' => 'press_releases#show', as: :pr
-      get '/press-kit' => redirect('http://cl.ly/433P3t1P2a1m202w2Y3D'), as: :press_kit
+      get '/press-kit' => redirect('http://cl.ly/433P3t1P2a1m202w2Y3D/content'), as: :press_kit
 
       get '/:page' => 'pages#show', as: :page
 
