8973f76747ea04a53f5cd7523b2e2b68b308001d
Rémy Coutable <remy@jilion.com>
2011-12-14 13:05:31 +0100

    Move XXXStats models under stats/

---
+++ app/models/stats/tweets_stat.rb
@@ -0,0 +1,37 @@
+module Stats
+  class TweetsStat
+
+    def self.json(options = {})
+      conditions = {}
+      conditions[:keywords] = { "$in" => Array.wrap(options[:keyword]) } if options[:keyword]
+
+      stats = Tweet.collection.map_reduce(
+        "function(){
+          emit(Date.UTC(this.tweeted_at.getFullYear(), this.tweeted_at.getMonth(), this.tweeted_at.getDate()), 1);
+        }",
+        "function(key, vals){
+          var total = 0;
+          for(var i in vals) total += vals[i];
+          return total;
+        };",
+        {
+          query: conditions,
+          raw: true,
+          out: { inline: 1 },
+          sort: [[:_id, :asc]]
+        }
+      )
+
+      results = []
+      stats['results'].inject(0) do |sum, stat|
+        sum += stat['value'].to_i
+        results << { 'id' => (stat['_id'].to_i / 1000), 'total' => sum }
+        sum
+      end
+      puts results.pop # remove the current day
+
+      results
+    end
+
+  end
+end
