46909c31bea59f91dff1af368bd55dd9361f9c6a
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-01-09 15:20:29 +0100

    Remove Rack::NoWWW from staging

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,6 @@
 MySublimeVideo::Application.configure do
   # Settings specified here will take precedence over those in config/environment.rb
+  config.middleware.insert_before Rack::Lock, Rack::NoWWW
 
   # The test environment is used exclusively to run your application's
   # test suite.  You never need to work with it otherwise.  Remember that
