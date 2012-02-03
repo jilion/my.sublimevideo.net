e3f3b1268e9981c20d081b7639e4c2d152aed09e
Rémy Coutable <remy@jilion.com>
2012-02-03 10:19:28 +0100

    Remove bang from method name since no exception is thrown when Referrer creation fails because the Referrer is not valid.

--- app/controllers/www/referrers_controller.rb
+++ app/controllers/www/referrers_controller.rb
@@ -3,10 +3,10 @@ class Www::ReferrersController < MyController
 
   # GET /r/:type/:token
   def redirect
-    Referrer.create_or_update_from_type!(params[:token], request.referer, params[:type])
+    Referrer.create_or_update_from_type(params[:token], request.referer, params[:type])
     set_referrer_token_in_cookie
   rescue => ex
-    Notify.send("Referrer (type: #{params[:type]}) creation problem from #{request.referer} for #{params[:token]}", :exception => ex)
+    Notify.send("Referrer (type: #{params[:type]}) creation problem from #{request.referer} for #{params[:token]}", exception: ex)
   ensure
     redirect_to root_url(host: request.domain)
   end
--- app/models/referrer.rb
+++ app/models/referrer.rb
@@ -31,20 +31,19 @@ class Referrer
   # = Validations =
   # ===============
 
-  validates :token,   :presence => true
-  validates :site_id, :presence => true
-  validates :url,     :presence => true, :format => { :with => /^https?\:\/\/.*/ }
+  validates :token, presence: true
+  validates :url,   presence: true, format: { with: /^https?\:\/\/.*/ }
 
   # ==========
   # = Scopes =
   # ==========
 
-  scope :by_url,             lambda { |way = 'desc'| order_by([:url, way.to_sym]) }
-  scope :by_hits,            lambda { |way = 'desc'| order_by([:hits, way.to_sym]) }
-  scope :by_badge_hits,      lambda { |way = 'desc'| order_by([:badge_hits, way.to_sym]) }
-  scope :by_contextual_hits, lambda { |way = 'desc'| order_by([:contextual_hits, way.to_sym]) }
-  scope :by_updated_at,      lambda { |way = 'desc'| order_by([:updated_at, way.to_sym]) }
-  scope :by_created_at,      lambda { |way = 'desc'| order_by([:created_at, way.to_sym]) }
+  scope :by_url,             lambda { |way='desc'| order_by([:url, way.to_sym]) }
+  scope :by_hits,            lambda { |way='desc'| order_by([:hits, way.to_sym]) }
+  scope :by_badge_hits,      lambda { |way='desc'| order_by([:badge_hits, way.to_sym]) }
+  scope :by_contextual_hits, lambda { |way='desc'| order_by([:contextual_hits, way.to_sym]) }
+  scope :by_updated_at,      lambda { |way='desc'| order_by([:updated_at, way.to_sym]) }
+  scope :by_created_at,      lambda { |way='desc'| order_by([:created_at, way.to_sym]) }
 
   # =================
   # = Class Methods =
@@ -54,20 +53,20 @@ class Referrer
     ref_hash = trackers.detect { |t| t.options[:title] == :referrers }.categories
     ref_hash.each do |url_and_token, hits|
       url, token = url_and_token[0],  url_and_token[1]
-      if referrer = Referrer.where(:url => url, :token => token).first
+      if referrer = Referrer.where(url: url, token: token).first
         referrer.hits = referrer.hits.to_i + hits
         referrer.save
       else
         create(
-          :url   => url,
-          :token => token,
-          :hits  => hits
+          url:   url,
+          token: token,
+          hits:  hits
         )
       end
     end
   end
 
-  def self.create_or_update_from_type!(token, url, type = 'c')
+  def self.create_or_update_from_type(token, url, type='c')
     if referrer = Referrer.where(url: url, token: token).first
       case type
       when 'b'
