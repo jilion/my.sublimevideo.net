e9f5166fbb3bf923530855888ee8baca6a564b20
Rémy Coutable <remy@jilion.com>
2012-02-21 14:36:52 +0100

    Ensure no exception is raised if MIME Type couldn't be guessed.

--- spec/lib/file_header_spec.rb
+++ spec/lib/file_header_spec.rb
@@ -3,17 +3,21 @@ require 'spec_helper'
 describe FileHeader do
 
   describe ".content_type" do
-    it "set content-type to text/javascript for .js files" do
+    it "returns content-type to text/javascript for .js files" do
       described_class.content_type('foo.js').should eq 'text/javascript'
     end
 
-    it "set content-type to text/javascript for .jgz files" do
+    it "returns content-type to text/javascript for .jgz files" do
       described_class.content_type('foo.jgz').should eq 'text/javascript'
     end
 
-    it "automatically set content-type for other files" do
+    it "returns automatically guessed content-type for other files" do
       described_class.content_type('foo.jpg').should eq 'image/jpeg'
       described_class.content_type('foo.txt').should eq 'text/plain'
+    end
+
+    it "returns nil for unguessable content-type" do
+      described_class.content_type('.DS_Store').should be_nil
     end
   end
 
