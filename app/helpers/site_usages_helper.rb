5419105991c3edf76d6b11b8dd798bc62fae01e1
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-04-04 13:50:30 +0200

    Ruby 1.9 hash

--- app/helpers/site_usages_helper.rb
+++ app/helpers/site_usages_helper.rb
@@ -6,17 +6,17 @@ module SiteUsagesHelper
     %w[loader_hits player_hits main_player_hits main_player_hits_cached extra_player_hits extra_player_hits_cached dev_player_hits dev_player_hits_cached invalid_player_hits invalid_player_hits_cached flash_hits requests_s3 traffic_s3 traffic_voxcast].map(&:to_sym).each do |usage_name|
       # usages_hash[:total][usage_name] = SiteUsage.any_in(:site_id => site_id).sum(usage_name).to_i
       usages_hash[:total][usage_name] = begin
-        SiteUsage.any_in(:site_id => site_id).sum(usage_name).to_i
+        SiteUsage.any_in(site_id: site_id).sum(usage_name).to_i
       rescue
         0
       end
 
       if options[:last_30_days]
-        usages_hash[:last_30_days][usage_name] = SiteUsage.between(Time.now.utc.midnight - 30.days, Time.now.utc.midnight).any_in(:site_id => site_id).sum(usage_name).to_i
+        usages_hash[:last_30_days][usage_name] = SiteUsage.between(Time.now.utc.midnight - 30.days, Time.now.utc.midnight).any_in(site_id: site_id).sum(usage_name).to_i
       end
 
       if options[:from] && options[:to]
-        usages_hash[:range][usage_name] = SiteUsage.between(options[:from], options[:to]).any_in(:site_id => site_id).sum(usage_name).to_i
+        usages_hash[:range][usage_name] = SiteUsage.between(options[:from], options[:to]).any_in(site_id: site_id).sum(usage_name).to_i
       end
     end
     usages_hash
