409bcdad5701ecc857823e9231a4f3bbff7f630a
Rémy Coutable <remy@jilion.com>
2012-04-20 17:38:31 +0200

    Remove news shit

--- app/helpers/news_helper.rb
+++
@@ -1,9 +0,0 @@
-module NewsHelper
-  
-  def cached_latest_sublimevideo_news(count)
-    Rails.cache.fetch("sublimevideo_news_from_blog_#{count}", expires_in: 1.hour) do
-      News.get_latest_sublimevideo_news(count)
-    end
-  end
-  
-end
