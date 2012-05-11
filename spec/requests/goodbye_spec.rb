8eec376238dd18e907db7612f4c43bb0b94ea402
Rémy Coutable <remy@jilion.com>
2012-05-11 17:19:23 +0200

    Add requests specs for the goodbye feedback form

--- spec/requests/goodbye_spec.rb
+++ spec/requests/goodbye_spec.rb
@@ -2,7 +2,7 @@
 require 'spec_helper'
 
 feature 'Account deletion', :focus do
-  scenario 'shows a feedback form before deleting the account' do
+  scenario 'shows a feedback form before deleting the account and succeeds with a reason and a valid current password' do
     sign_in_as :user
     click_link 'John Doe'
     click_link 'Cancel account'
@@ -13,7 +13,6 @@ feature 'Account deletion', :focus do
     fill_in 'user_current_password', with: '123456'
     click_button 'Cancel my account'
 
-    # Not working with external url
     current_url.should eq 'http://my.sublimevideo.dev/login'
     get_me_the_cookies.map { |c| c['name'] }.should_not include('l')
 
@@ -29,5 +28,34 @@ feature 'Account deletion', :focus do
     last_delivery.to.should eq [@current_user.email]
     last_delivery.subject.should eq 'Your account has been deleted'
     last_delivery.body.encoded.should include 'Your account has been deleted.'
+  end
+
+  scenario 'shows a feedback form before deleting the account but shows an error without a valid reason' do
+    sign_in_as :user
+    click_link 'John Doe'
+    click_link 'Cancel account'
+
+    current_url.should eq 'http://my.sublimevideo.dev/goodbye'
+
+    fill_in 'user_current_password', with: '123456'
+    click_button 'Cancel my account'
+
+    current_url.should eq 'http://my.sublimevideo.dev/goodbye'
+    page.should have_content 'Reason must be given'
+  end
+
+  scenario 'shows a feedback form before deleting the account but shows an error without a valid current password' do
+    sign_in_as :user
+    click_link 'John Doe'
+    click_link 'Cancel account'
+
+    current_url.should eq 'http://my.sublimevideo.dev/goodbye'
+
+    select  'Price',                 from: 'goodbye_feedback_reason'
+    fill_in 'user_current_password', with: '654321'
+    click_button 'Cancel my account'
+
+    current_url.should eq 'http://my.sublimevideo.dev/goodbye'
+    page.should have_content 'Please enter your current passowrd to confirm the cancellation'
   end
 end
