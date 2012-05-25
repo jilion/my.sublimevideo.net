e4f3bdbe4fbf7535b2ca72653daa500932586407
Rémy Coutable <remy@jilion.com>
2012-05-25 12:38:50 +0200

    Fix specs

--- spec/mailers/mail_mailer_spec.rb
+++ spec/mailers/mail_mailer_spec.rb
@@ -18,13 +18,11 @@ describe MailMailer do
     end
 
     it "should set subject to Liquidified template.subject" do
-      @last_delivery.subject.should eql Liquid::Template.parse(@template.subject).render("user" => subject)
-      @last_delivery.subject.should include "John Doe (#{subject.email}), help us shaping the right pricing"
+      @last_delivery.subject.should eq Liquid::Template.parse(@template.subject).render("user" => subject)
     end
 
     it "should set the body to Liquidified-simple_formated-auto_linked template.body" do
-      @last_delivery.body.encoded.should eql Liquid::Template.parse(@template.body).render("user" => subject)
-      @last_delivery.body.encoded.should include "Hi John Doe (#{subject.email}), please respond to the survey, by clicking on the following url: http://survey.com"
+      @last_delivery.body.encoded.should include Liquid::Template.parse(@template.body).render("user" => subject)
     end
   end
 
