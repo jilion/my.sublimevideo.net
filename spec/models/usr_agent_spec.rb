8bf0dd4744c377b84d66792c959e083a0cd0c4b9
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-08-09 11:36:01 +0200

    Fix UsrAgent spec

--- spec/models/usr_agent_spec.rb
+++ spec/models/usr_agent_spec.rb
@@ -17,15 +17,13 @@ describe UsrAgent do
       VoxcastCDN.stub(:download_log).with('cdn.sublimevideo.net.log.1284549900-1284549960.gz').and_return(log_file)
       @log = create(:log_voxcast, name: 'cdn.sublimevideo.net.log.1284549900-1284549960.gz')
       @trackers = LogAnalyzer.parse(log_file, 'LogsFileFormat::VoxcastUserAgents')
-      %w[0w1o1q3c k8qaaj1l ibvjcopp hp1lepyq].each do |token|
-        create(:site).update_attribute(:token, token)
-      end
+      create(:site).update_attribute(:token, 'ibvjcopp')
       UsrAgent.create_or_update_from_trackers!(@log, @trackers)
     end
 
-    let(:site) { Site.find_by_token('k8qaaj1l') }
+    let(:site) { Site.find_by_token('ibvjcopp') }
 
-    it { UsrAgent.count.should == 6 }
+    it { UsrAgent.count.should == 1 }
 
     describe "first usr_agent" do
       subject { UsrAgent.where(token: site.token).first }
@@ -34,16 +32,16 @@ describe UsrAgent do
         subject.token.should == site.token
         subject.site.should eql(site)
         subject.month.should == Time.utc(2010,9,15).beginning_of_month
-        subject.platforms.should == { "Windows" => { "Windows 7" => 4 } }
-        subject.browsers.should == { "Safari" =>{ "versions" => { "5::0::2" => 4 }, "platforms" => {"Windows" => 4 } } }
+        subject.platforms.should == { "Macintosh" => { "Intel Mac OS X 10::6::8" => 3 } }
+        subject.browsers.should == { "Safari" => { "platforms" => { "Macintosh" => 3 }, "versions" => { "5::0::5" => 3 } } }
       end
 
       it "should update hits if same usr_agent reparsed" do
         UsrAgent.create_or_update_from_trackers!(@log, @trackers)
 
-        subject.reload.platforms.should == { "Windows" => { "Windows 7" => 8 } }
-        subject.reload.browsers.should == { "Safari" =>{ "versions" => { "5::0::2" => 8 }, "platforms" => {"Windows" => 8 } } }
-        UsrAgent.count.should == 6
+        subject.reload.platforms.should == { "Macintosh" => { "Intel Mac OS X 10::6::8" => 6 } }
+        subject.reload.browsers.should == { "Safari" => { "platforms" => { "Macintosh" => 6 }, "versions" => { "5::0::5" => 6 } } }
+        UsrAgent.count.should == 1
       end
     end
   end
