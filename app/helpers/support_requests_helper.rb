ce34cfbf903b4efbb7a1f5f9af78fc05c4c26dbd
Rémy Coutable <remy@jilion.com>
2012-08-20 18:13:13 +0200

    Add a note regarding the presence of the support team

--- app/helpers/support_requests_helper.rb
+++ app/helpers/support_requests_helper.rb
@@ -1,5 +1,13 @@
 module SupportRequestsHelper
 
+  def support_presence
+    if (9...18).include?(Time.now.utc.in_time_zone('Bern').hour)
+      content_tag(:strong, 'present', style: 'color:green')
+    else
+      content_tag(:span, 'not present', style: 'color:red')
+    end
+  end
+
   def support_request_site_token_options
     options_for_select([[t("support_request.site_token.choose-site_token"), ''], ["-"*16, '-']] + current_user.sites.active.map { |s| [s.hostname_or_token, s.token] }, selected: params[:support_request] ? params[:support_request][:site_token] : nil, disabled: ['-'])
   end
