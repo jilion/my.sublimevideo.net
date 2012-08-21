ad43c8aac83178e13b28d964d6779f1dbc79ec82
Rémy Coutable <remy@jilion.com>
2012-08-21 12:20:56 +0200

    Don't redirect to public help

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,7 @@
 class PagesController < ApplicationController
-  skip_before_filter :authenticate_user!, if: proc { |c| %w[terms privacy help].include?(params[:page]) }
+  skip_before_filter :authenticate_user!, if: proc { |c| %w[terms privacy].include?(params[:page]) }
   before_filter :redirect_non_suspended_user!, if: proc { |c| params[:page] == 'suspended' && user_signed_in? && !current_user.suspended? }
   before_filter :prepare_support_request, if: proc { |c| params[:page] == 'help' && user_signed_in? && current_user.email_support? }
-  before_filter :redirect_to_public_help, if: proc { |c| params[:page] == 'help' && !user_signed_in? }
 
   def show
     render params[:page]
@@ -12,10 +11,6 @@ private
 
   def redirect_non_suspended_user!
     redirect_to root_path
-  end
-
-  def redirect_to_public_help
-    redirect_to "http://#{request.domain}/help"
   end
 
   def prepare_support_request
