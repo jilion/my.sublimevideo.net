263e80c6b032e9e30e879898e374825f6218bd5b
Rémy Coutable <remy@jilion.com>
2012-08-22 17:26:05 +0200

    Fix wrong folder name

--- spec/models/site_usage_modules/api_spec.rb
+++ spec/models/site_usage_modules/api_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe SiteUsage::Api do
+describe SiteUsageModules::Api do
   let(:site) { create(:site) }
   let(:site_usage) { create(:site_usage, site_id: site.id, day: 61.days.ago.midnight, main_player_hits: 1000, main_player_hits_cached: 800, extra_player_hits: 500, extra_player_hits_cached: 400) }
   let(:response) { site_usage.as_api_response(:v1_private_self) }
