869dca7a94414a0ac58aa6f857550f2e288e2c26
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-08-23 10:06:15 +0200

    Try a version of PaperTrail with Version class namespaced

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
 require "active_record/railtie"
-require 'active_record/associations'
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
