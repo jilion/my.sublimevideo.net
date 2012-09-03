cf46728d5f40b02b2a899978c50ab62af50fc1c1
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-09-03 09:16:05 +0200

    Update kaminari

--- app/helpers/paginate_helper.rb
+++ app/helpers/paginate_helper.rb
@@ -1,7 +1,7 @@
 module PaginateHelper
 
   def page_entries_info(collection)
-    if collection.num_pages > 1
+    if collection.total_pages > 1
       if collection.class == ActiveRecord::Relation
         (%{<div class="page_info"><strong>%d&nbsp;-&nbsp;%d</strong> of <strong>%d</strong></div>} % [collection.ast.offset.expr + 1, collection.ast.offset.expr + collection.length, collection.total_count]).html_safe
       else
