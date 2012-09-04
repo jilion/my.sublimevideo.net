ca001f2f446420c6dee8ffaeaf13e9a2189fd9b5
Rémy Coutable <remy@jilion.com>
2012-09-04 16:50:26 +0200

    Don't fail fast in Semaphore

--- spec/config/rspec.rb
+++ spec/config/rspec.rb
@@ -3,6 +3,6 @@ RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run_including focus: true
   config.mock_with :rspec
-  config.fail_fast = true
+  config.fail_fast = ENV['FAST_FAIL'] != 'false'
   config.order = 'random'
 end
