7d8a53acfe12ce4fac7658b76be252bef993fa9d
Rémy Coutable <remy@jilion.com>
2012-09-04 11:50:32 +0200

    Fix premailer-rails3 issue

--- config/initializers/premailer-rails3.rb
+++ config/initializers/premailer-rails3.rb
@@ -1,9 +1,10 @@
 PremailerRails.config = {
-  adapter:           :hpricot,
-  warn_level:        Premailer::Warnings::SAFE,
-  css_to_attributes: true,
-  remove_ids:        true,
-  remove_classes:    true,
-  remove_comments:   true,
-  verbose:           Rails.env.development?
+  adapter:            :hpricot,
+  generate_text_part: true,
+  warn_level:         Premailer::Warnings::SAFE,
+  css_to_attributes:  true,
+  remove_ids:         true,
+  remove_classes:     true,
+  remove_comments:    true,
+  verbose:            Rails.env.development?
 }
