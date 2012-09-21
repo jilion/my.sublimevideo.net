fa8fe429180f80991c097a9de58316328ee61763
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-09-21 12:25:24 +0200

    Update Player::Loader class
    - move old loader logic to Player::Loader
    - handle both bucket (sv & loaders) for CDN migration
    - miss new loader content logic

--- app/models/player/loader.rb
+++ app/models/player/loader.rb
@@ -5,13 +5,19 @@
 require 'tempfile'
 require_dependency 'cdn'
 
-class Player::Loader < Struct.new(:tag, :version, :file)
+class Player::Loader < Struct.new(:site, :mode, :file)
+  delegate :token, :player_mode, to: :site
 
-  TEMPLATE_PATH = Rails.root.join("app/templates/player/loader.js.erb")
-
-  def self.update!(tag, version)
-    loader = new(tag, version)
+  def self.update!(site_id, mode)
+    site = Site.find(site_id)
+    loader = new(site, mode)
     loader.upload!
+  end
+
+  def self.delete!(site_id)
+    site = Site.find(site_id)
+    loader = new(site, mode)
+    loader.delete!
   end
 
   def initialize(*args)
@@ -21,35 +27,85 @@ class Player::Loader < Struct.new(:tag, :version, :file)
 
   def upload!
     File.open(file) do |f|
-      S3.fog_connection.put_object(
-        S3.buckets['sublimevideo'],
-        filename,
-        f.read,
-        {
-          'Cache-Control' => 'max-age=300, public', # 5 minutes
-          'Content-Type'  => 'text/javascript',
-          'x-amz-acl'     => 'public-read'
-        }
-      )
+      data = f.read
+      mode_config[:destinations].each do |destination|
+        s3_put_object(
+          destination[:bucket],
+          destination[:path],
+          data
+        )
+      end
     end
-    CDN.purge(filename)
+    purge_cdn
   end
 
-  def filename
-    if tag == 'stable'
-      'loader.js'
-    else
-      "loader-#{tag}.js"
+  def delete!
+    mode_config[:destinations].each do |destination|
+      S3.fog_connection.delete_object(
+        destination[:bucket],
+        destination[:path]
+      )
     end
+    purge_cdn
+  end
+
+  def components_path
+    [] # TODO Thibaud
   end
 
 private
 
   def generate_file
-    template = ERB.new(File.new(TEMPLATE_PATH).read)
-    self.file = Tempfile.new(filename, "#{Rails.root}/tmp")
+    template_path = Rails.root.join("app/templates/player/#{mode_config[:template]}")
+    template = ERB.new(File.new(template_path).read)
+    self.file = Tempfile.new("l-#{site.token}.js", "#{Rails.root}/tmp")
     self.file.print template.result(binding)
     self.file.flush
+  end
+
+  def s3_put_object(bucket, path, data)
+    S3.fog_connection.put_object(
+      bucket,
+      path,
+      data,
+      {
+        'Cache-Control' => 'max-age=120, public', # 2 minutes
+        'Content-Type'  => 'text/javascript',
+        'x-amz-acl'     => 'public-read'
+      }
+    )
+  end
+
+  def purge_cdn
+    CDN.purge("/#{first_destination_path}")
+  end
+
+  def first_destination_path
+    mode_config[:destinations].first[:path]
+  end
+
+  def mode_config
+    # Handle old loader
+    if mode == 'stable'
+      {
+        template: "loader-old.js.erb",
+        destinations: [{
+          bucket: S3.buckets['sublimevideo'],
+          path: "js/#{site.token}.js"
+        },{
+          bucket: S3.buckets['loaders'],
+          path: "loaders/#{site.token}.js"
+        }]
+      }
+    else
+      {
+        template: "loader.js.erb",
+        destinations: [{
+          bucket: S3.buckets['sublimevideo'],
+          path: "js/#{site.token}-#{mode}.js"
+        }]
+      }
+    end
   end
 
 end
--- spec/models/player/loader_spec.rb
+++ spec/models/player/loader_spec.rb
@@ -12,56 +12,178 @@ require File.expand_path('app/models/player')
 require File.expand_path('app/models/player/loader')
 
 describe Player::Loader, :fog_mock do
-  let(:loader) { Player::Loader.new('alpha', '2.0.0') }
-  let(:bucket) { S3.buckets['sublimevideo'] }
+  let(:site) { mock("Site",
+    token: 'abcd1234',
+    player_mode: 'beta'
+  )}
 
-  describe "filename" do
-    it "is loader.js with stable tag" do
-      loader.tag = 'stable'
-      loader.filename.should eq 'loader.js'
+  context "on stable mode" do
+    let(:loader) { Player::Loader.new(site, 'stable') }
+
+    describe "initialize" do
+      it "generates properly the loader file from templates" do
+        loader.file.should be_present
+      end
     end
 
-    it "is loader-tag.js with other tag" do
-      loader.tag = 'alpha'
-      loader.filename.should eq 'loader-alpha.js'
+    describe "upload!" do
+      before { CDN.stub(:purge) }
+
+      describe "uploaded loader file on each destination" do
+        before { loader.upload! }
+
+        context "on sublimevideo bucket" do
+          let(:bucket) { S3.buckets['sublimevideo'] }
+          let(:path)   { "js/#{site.token}.js" }
+
+          it "is public" do
+            object_acl = S3.fog_connection.get_object_acl(bucket, path).body
+            object_acl['AccessControlList'].should include(
+              {"Permission"=>"READ", "Grantee"=>{"URI"=>"http://acs.amazonaws.com/groups/global/AllUsers"}}
+            )
+          end
+          it "have good content_type public" do
+            object_headers = S3.fog_connection.get_object(bucket, path).headers
+            object_headers['Content-Type'].should eq 'text/javascript'
+          end
+          it "have 5 min max-age cache control" do
+            object_headers = S3.fog_connection.get_object(bucket, path).headers
+            object_headers['Cache-Control'].should eq 'max-age=120, public'
+          end
+          it "includes good loader version" do
+            object = S3.fog_connection.get_object(bucket, path)
+            object.body.should include '/p/beta/sublime.js'
+          end
+        end
+
+        context "on loaders bucket" do
+          let(:bucket) { S3.buckets['loaders'] }
+          let(:path)   { "loaders/#{site.token}.js" }
+
+          it "is public" do
+            object_acl = S3.fog_connection.get_object_acl(bucket, path).body
+            object_acl['AccessControlList'].should include(
+              {"Permission"=>"READ", "Grantee"=>{"URI"=>"http://acs.amazonaws.com/groups/global/AllUsers"}}
+            )
+          end
+          it "have good content_type public" do
+            object_headers = S3.fog_connection.get_object(bucket, path).headers
+            object_headers['Content-Type'].should eq 'text/javascript'
+          end
+          it "have 5 min max-age cache control" do
+            object_headers = S3.fog_connection.get_object(bucket, path).headers
+            object_headers['Cache-Control'].should eq 'max-age=120, public'
+          end
+          it "includes good player mode path" do
+            object = S3.fog_connection.get_object(bucket, path)
+            object.body.should include '/p/beta/sublime.js'
+          end
+        end
+      end
+
+      it "purges loader file from CDN" do
+        CDN.should_receive(:purge).with("/js/#{site.token}.js")
+        loader.upload!
+      end
     end
+
+    describe "#delete!" do
+      before do
+        CDN.stub(:purge)
+        loader.upload!
+      end
+
+      context "on sublimevideo bucket" do
+        let(:bucket) { S3.buckets['sublimevideo'] }
+        let(:path)   { "js/#{site.token}.js" }
+
+        it "deletes settings file" do
+          loader.delete!
+          expect { S3.fog_connection.get_object(bucket, path) }.to raise_error(Excon::Errors::NotFound)
+        end
+
+        it "purges loader file from CDN" do
+          CDN.should_receive(:purge).with("/js/#{site.token}.js")
+          loader.delete!
+        end
+      end
+
+      context "on loaders bucket" do
+        let(:bucket) { S3.buckets['loaders'] }
+        let(:path)   { "loaders/#{site.token}.js" }
+
+        it "deletes settings file" do
+          loader.delete!
+          expect { S3.fog_connection.get_object(bucket, path) }.to raise_error(Excon::Errors::NotFound)
+        end
+
+        it "purges loader file from CDN" do
+          CDN.should_receive(:purge).with("/js/#{site.token}.js")
+          loader.delete!
+        end
+      end
+    end
+
   end
 
-  describe "initialize" do
-    it "generates properly the loader file from templates" do
-      loader.file.should be_present
-    end
-  end
+  context "on beta (& alpha) mode" do
+    let(:loader) { Player::Loader.new(site, 'beta') }
+    let(:bucket) { S3.buckets['sublimevideo'] }
+    let(:path)   { "js/#{site.token}-beta.js" }
 
-  describe "upload!" do
-    before { CDN.stub(:purge) }
-
-    describe "uploaded loader file" do
-      before { loader.upload! }
-
-      it "is public" do
-        object_acl = S3.fog_connection.get_object_acl(bucket, loader.filename).body
-        object_acl['AccessControlList'].should include(
-          {"Permission"=>"READ", "Grantee"=>{"URI"=>"http://acs.amazonaws.com/groups/global/AllUsers"}}
-        )
-      end
-      it "have good content_type public" do
-        object_headers = S3.fog_connection.get_object(bucket, loader.filename).headers
-        object_headers['Content-Type'].should eq 'text/javascript'
-      end
-      it "have 5 min max-age cache control" do
-        object_headers = S3.fog_connection.get_object(bucket, loader.filename).headers
-        object_headers['Cache-Control'].should eq 'max-age=300, public'
-      end
-      it "includes good loader version" do
-        object = S3.fog_connection.get_object(bucket, loader.filename)
-        object.body.should include '2.0.0'
+    describe "initialize" do
+      it "generates properly the loader file from templates" do
+        loader.file.should be_present
       end
     end
 
-    it "purges loader file from CDN" do
-      CDN.should_receive(:purge).with(loader.filename)
-      loader.upload!
+    describe "upload!" do
+      before { CDN.stub(:purge) }
+
+      describe "uploaded loader file on each destination" do
+        before { loader.upload! }
+
+        it "is public" do
+          object_acl = S3.fog_connection.get_object_acl(bucket, path).body
+          object_acl['AccessControlList'].should include(
+            {"Permission"=>"READ", "Grantee"=>{"URI"=>"http://acs.amazonaws.com/groups/global/AllUsers"}}
+          )
+        end
+        it "have good content_type public" do
+          object_headers = S3.fog_connection.get_object(bucket, path).headers
+          object_headers['Content-Type'].should eq 'text/javascript'
+        end
+        it "have 5 min max-age cache control" do
+          object_headers = S3.fog_connection.get_object(bucket, path).headers
+          object_headers['Cache-Control'].should eq 'max-age=120, public'
+        end
+        it "includes components path" do
+          object = S3.fog_connection.get_object(bucket, path)
+          object.body.should include '[]'
+        end
+      end
+
+      it "purges loader file from CDN" do
+        CDN.should_receive(:purge).with("/#{path}")
+        loader.upload!
+      end
+    end
+
+    describe "#delete!" do
+      before do
+        CDN.stub(:purge)
+        loader.upload!
+      end
+
+      it "deletes settings file" do
+        loader.delete!
+        expect { S3.fog_connection.get_object(bucket, path) }.to raise_error(Excon::Errors::NotFound)
+      end
+
+      it "purges loader file from CDN" do
+        CDN.should_receive(:purge).with("/#{path}")
+        loader.delete!
+      end
     end
   end
 
