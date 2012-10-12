aefc80c0e94e734e06e14c1bbb1565a3bf79d5b3
Rémy Coutable <remy@jilion.com>
2012-10-12 15:25:44 +0200

    Update default component token

--- config/routes.rb
+++ config/routes.rb
@@ -141,7 +141,7 @@ MySublimeVideo::Application.routes.draw do
 
       resources :releases, only: [:index, :create, :update]
 
-      get '/app' => redirect('/app/components/e'), as: 'app'
+      get '/app' => redirect('/app/components/sa'), as: 'app'
       namespace :player, path: 'app' do
         resources :components, only: [:index, :create, :show, :update, :destroy] do
           resources :versions, only: [:index, :create, :show, :destroy], controller: 'component_versions'
