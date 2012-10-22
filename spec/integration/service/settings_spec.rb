eda232594d0514dd8885c2b26ef8694608cecb09
Rémy Coutable <remy@jilion.com>
2012-10-22 15:35:12 +0200

    Fix specs

--- spec/integration/service/settings_spec.rb
+++ spec/integration/service/settings_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Service::Settings, :fog_mock do
-  before { puts App::Design.count; CDN.stub(:delay) { mock(purge: true) } }
+  before { CDN.stub(:delay) { mock(purge: true) } }
 
   context "with populates addons" do
     describe "default settings of a new site" do
@@ -148,7 +148,7 @@ describe Service::Settings, :fog_mock do
         it "has good content" do
           File.open(subject.file) do |f|
             f.read.should eq <<-CONTENT.gsub(/^ {12}/, '')
-            sublime_.jd(\"ko\",[],function(){var a;return a={kr:{\"ku\":[\"test.com\"],\"kv\":[\"127.0.0.1\",\"localhost\"],\"kz\":null,\"ia\":null,\"ib\":\"beta\"},sa:{\"kf\":{\"ko\":{\"tm\":true,\"tn\":false},\"kp\":{\"tm\":{\"ih\":[true]},\"tn\":{\"ih\":[false]}}}},ks:{\"default\":{\"kb\":{\"kn\":\"sa.sb.sc\"},\"ka\":{\"ke\":{\"ka\":{\"logo\":{\"ko\":{\"tq\":\"autohide\",\"to\":\"bottomRight\",\"ij\":\"\",\"ik\":null},\"kp\":{\"tq\":{\"ih\":[\"autohide\"]},\"to\":{\"ih\":[\"bottomLeft\",\"bottomRight\"]},\"ij\":{},\"ik\":{}},\"kn\":\"sa.sh.sp\"},\"kg\":{\"ko\":{\"tq\":\"autohide\"},\"kp\":{\"tq\":{\"ih\":[\"hidden\",\"autohide\",\"visible\"]}},\"kn\":\"sa.sh.sq\"},\"kh\":{\"ko\":{\"tr\":\"autofade\",\"tg\":\"#000\"},\"kp\":{\"tr\":{\"ih\":[\"hidden\",\"autofade\"]},\"tg\":{\"ih\":[\"#000\"]}},\"kn\":\"sa.sh.sv\"}},\"ko\":{\"tb\":false,\"onEnd\":\"nothing\",\"td\":true,\"te\":true},\"kp\":{\"tb\":{\"ih\":[true,false]},\"onEnd\":{\"ih\":[\"nothing\",\"replay\",\"stop\"]},\"td\":{\"ih\":[true,false]},\"te\":{\"ih\":[true,false]}},\"kn\":\"sa.sh.si\"},\"kd\":{\"ko\":{\"onOpen\":\"play\",\"tg\":\"#000\",\"th\":0.7,\"ti\":\"autohide\",\"tl\":\"left\"},\"kp\":{\"onOpen\":{\"ih\":[\"nothing\",\"play\"]},\"tg\":{\"ih\":[\"#000\"]},\"th\":{\"ii\":[0.05,1]},\"ti\":{\"ih\":[\"hidden\",\"autohide\",\"visible\"]},\"tl\":{\"ih\":[\"left\",\"right\"]}},\"kn\":\"sa.sl.sm\"}}}},kt:'default'},[a]})
+            sublime_.jd("ko",[],function(){var a;return a={kr:{"ku":["test.com"],"kv":["127.0.0.1","localhost"],"kz":null,"ia":null,"ib":"beta"},sa:{"kf":{"ko":{"tm":true,"tn":false},"kp":{"tm":{"ih":[true]},"tn":{"ih":[false]}}}},ks:{"default":{"kb":{"kn":"sa.sb.sc"},"ka":{"kd":{"ko":{"onOpen":"play","tg":"#000","th":0.7,"ti":"autohide","tl":"left"},"kp":{"onOpen":{"ih":["nothing","play"]},"tg":{"ih":["#000"]},"th":{"ii":[0.05,1]},"ti":{"ih":["hidden","autohide","visible"]},"tl":{"ih":["left","right"]}},"kn":"sa.sl.sm"},"ke":{"ka":{"ki":{"ko":{"tq":"autohide","to":"bottomRight","ij":"","ik":null},"kp":{"tq":{"ih":["autohide"]},"to":{"ih":["bottomLeft","bottomRight"]},"ij":{},"ik":{}},"kn":"sa.sh.sp"},"kh":{"ko":{"tr":"autofade","tg":"#000"},"kp":{"tr":{"ih":["hidden","autofade"]},"tg":{"ih":["#000"]}},"kn":"sa.sh.sv"},"kg":{"ko":{"tq":"autohide"},"kp":{"tq":{"ih":["hidden","autohide","visible"]}},"kn":"sa.sh.sq"}},"ko":{"tb":false,"onEnd":"nothing","td":true,"te":true},"kp":{"tb":{"ih":[true,false]},"onEnd":{"ih":["nothing","replay","stop"]},"td":{"ih":[true,false]},"te":{"ih":[true,false]}},"kn":"sa.sh.si"}}}},kt:'default'},[a]})
             CONTENT
           end
         end
