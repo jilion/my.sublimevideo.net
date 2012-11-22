e403aaddb67840f21221428c9815a6b75c99552c
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-11-22 16:53:48 +0100

    Configure Sidekiq
    Maybe concurrency will need to smaller
    Maybe redis connection server will need to higher

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,9 +2,14 @@ Sidekiq.configure_client do |config|
   config.poll_interval = 5
 end
 
-# http://mongoid.org/en/mongoid/docs/tips.html#sidekiq
 Sidekiq.configure_server do |config|
+  config.redis = { size: 20 } # for worker dyno
+  # http://mongoid.org/en/mongoid/docs/tips.html#sidekiq
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
   end
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { size: 2 } # for web dyno
 end
