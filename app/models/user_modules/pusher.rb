5160e0686b914ccc9ea68b521d0fb62f852404de
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-05 08:13:15 +0100

    Fix http://jilion.airbrake.io/errors/53596029

--- app/models/user_modules/pusher.rb
+++ app/models/user_modules/pusher.rb
@@ -4,7 +4,7 @@ module UserModules::Pusher
   module ClassMethods
 
     def accessible_channel?(channel_name, user = nil)
-      if matches = channel_name.match(/private-([a-z0-9]{4,8})$/)
+      if matches = channel_name.try(:match, /private-([a-z0-9]{4,8})$/)
         site_token = matches[1]
         site_token == SiteToken[:www] || (user && user.sites.where(token: site_token).exists?)
       end
