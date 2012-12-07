f16b8c7065cf1b3998f1a6c636e8b827a77f55c9
Rémy Coutable <remy@jilion.com>
2012-12-07 16:43:53 +0100

    Oops.

--- db/migrate/20121207133427_rename_public_at_to_stable_at_in_app_designs_and_addon_plans.rb
+++ db/migrate/20121207133427_rename_public_at_to_stable_at_in_app_designs_and_addon_plans.rb
@@ -1,11 +1,11 @@
 class RenamePublicAtToStableAtInAppDesignsAndAddonPlans < ActiveRecord::Migration
   def up
-    rename_column :app_designs, :stable_at, :stable_at
-    rename_column :addon_plans, :stable_at, :stable_at
+    rename_column :app_designs, :public_at, :stable_at
+    rename_column :addon_plans, :public_at, :stable_at
   end
 
   def down
-    rename_column :app_designs, :stable_at, :stable_at
-    rename_column :addon_plans, :stable_at, :stable_at
+    rename_column :app_designs, :stable_at, :public_at
+    rename_column :addon_plans, :stable_at, :public_at
   end
 end
