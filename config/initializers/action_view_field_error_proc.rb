efb60f823c0d9c719ba26415da9c0068be1de52d
Rémy Coutable <remy@jilion.com>
2012-12-11 23:30:33 +0100

    Improve inline errors display

--- config/initializers/action_view_field_error_proc.rb
+++ config/initializers/action_view_field_error_proc.rb
@@ -1,15 +1,9 @@
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-
   error_class = "errors"
-  if html_tag =~ /<(input|textarea|select)[^>]+class=/
-    class_attribute = html_tag =~ /class=['"]/
-    html_tag.insert(class_attribute + 7, "#{error_class} ")
-  elsif html_tag =~ /<(input|textarea|select)/
-    first_whitespace = html_tag =~ /\s/
-    html_tag[first_whitespace] = " class='#{error_class}' "
-  end
-
   if instance.object.present? && html_tag =~ /<(input|textarea|select)/
+    position = html_tag =~ /( class=['"]|\s)/
+    html_tag.insert(position + $1.size, $1.size > 1 ? "#{error_class} " : " class='#{error_class}' ")
+
     if errors = instance.object.errors.messages.delete(instance.method_name.to_sym)
       if errors.length > 1
         last_error = " and #{errors.pop}"
@@ -18,11 +12,15 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
       else
         inline_errors = errors.pop
       end
+
       attribute_name = instance.object.class.human_attribute_name(instance.method_name.to_sym)
-      if html_tag =~ /<(input|textarea|select)[^>]+type="(radio|checkbox)"/
-        html_tag = "<div class='inline_errors'><span>#{attribute_name} #{inline_errors}</span></div>".html_safe + html_tag
+      inline_errors = inline_errors =~ /#{attribute_name}/ ? inline_errors : "#{attribute_name} #{inline_errors}"
+      inline_errors = inline_errors.downcase.humanize
+
+      if html_tag =~ /(radio|checkbox)/
+        html_tag = "<div class='inline_errors'><span>#{inline_errors}</span></div>".html_safe + html_tag
       else
-        html_tag += "<div class='inline_errors'><span>#{attribute_name} #{inline_errors}</span></div>".html_safe
+        html_tag += "<div class='inline_errors'><span>#{inline_errors}</span></div>".html_safe
       end
     end
   end
