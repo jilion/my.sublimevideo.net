c638539d057190eeb38dfca3782b4396ab9266e4
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-13 11:27:31 +0100

    Re-truncate VideoTag uid

--- app/models/video_tag.rb
+++ app/models/video_tag.rb
@@ -36,10 +36,9 @@ class VideoTag < ActiveRecord::Base
     uid
   end
 
-  # # TODO Try to remove once passed to sidekiq
-  # def uid=(attribute)
-  #   write_attribute :uid, attribute.try(:to, 254)
-  # end
+  def uid=(attribute)
+    write_attribute :uid, attribute.try(:to, 254)
+  end
 
   def name=(attribute)
     write_attribute :name, attribute.try(:to, 254)
