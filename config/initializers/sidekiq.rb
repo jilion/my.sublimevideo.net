731f22711a436a242ea764964b861585d5dd5cbe
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-13 22:47:33 +0100

    Reduce Sidekiq concurrency to 25

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,6 @@
 require_dependency 'service/tailor_made_player_request'
 
 Sidekiq.configure_server do |config|
-  config.redis = { size: ENV['REDIS_SIZE'] ? ENV['REDIS_SIZE'].to_i : 25 } # for worker dyno
   # http://mongoid.org/en/mongoid/docs/tips.html#sidekiq
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
