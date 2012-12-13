d378a5d3c544f25a9a8aa2a467307b32c82cc707
Rémy Coutable <remy@jilion.com>
2012-12-13 20:53:40 +0100

    Automatize CDN host url depending on the env

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,5 +1,14 @@
 module UrlsHelper
 
+  def cdn_host
+    case Rails.env
+    when 'production'
+      'cdn.sublimevideo.net'
+    else
+      'cdn.sublimevideo-staging.net'
+    end
+  end
+
   def docs_page_url(path)
     page_url(path, subdomain: 'docs', protocol: 'http')
   end
