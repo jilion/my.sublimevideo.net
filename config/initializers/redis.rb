3bf5938a1f50ecdbc594cd32fbd0a5633fe932e6
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-13 22:54:20 +0100

    empty commit

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,5 @@
 if ENV['OPENREDIS_URL'].present?
   $redis = ConnectionPool::Wrapper.new(size: 5, timeout: 3) { Redis.new(url: ENV['OPENREDIS_URL']) }
 else
-  $redis = ConnectionPool::Wrapper.new(size: 1, timeout: 3) { Redis.new }
+  $redis = ConnectionPool::Wrapper.new(size: 1) { Redis.new }
 end
