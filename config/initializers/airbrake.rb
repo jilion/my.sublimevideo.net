6bd6417a90f27c38da6319921ccb03be3e746e5b
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2012-12-14 14:07:13 +0100

    Ignore Excon::Errors::SocketError wiht Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -2,4 +2,5 @@ Airbrake.configure do |config|
   config.api_key = 'd83280c89d59e2babd2e3ee1c546e8d4'
   config.ignore  << ActionController::UnknownHttpMethod
   config.ignore  << Errno::EPIPE
+  config.ignore  << Excon::Errors::SocketError
 end
