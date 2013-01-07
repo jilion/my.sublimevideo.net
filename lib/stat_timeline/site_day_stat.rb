077ff7ce080198a4f0a2ebb05bfd795e70223a05
Rémy Coutable <remy@jilion.com>
2013-01-07 18:29:18 +0100

    Improve admin's usage timelines

--- lib/stat_timeline/site_day_stat.rb
+++ lib/stat_timeline/site_day_stat.rb
@@ -12,13 +12,25 @@ module StatTimeline
       @options = options
     end
 
-    # Creates d_pv, m_pv, e_pv, em_pv, i_pv, d_vv, m_vv, e_vv, em_vv, i_vv methods
+    # Creates d_pv, m_pv, e_pv, em_pv, d_vv, m_vv, e_vv, em_vv methods
     %w[pv vv].each do |type|
-      %w[d m e em i].each do |field|
+      %w[d m e em].each do |field|
         define_method("#{field}_#{type}") do
           all.map { |s| s[type][field].to_i }
         end
       end
+    end
+
+    def normal_pv
+      all.map { |s| s['pv']['m'].to_i + s['pv']['e'].to_i }
+    end
+
+    def billable_pv
+      all.map { |s| s['pv']['m'].to_i + s['pv']['e'].to_i + s['pv']['em'].to_i }
+    end
+
+    def normal_vv
+      all.map { |s| s['vv']['m'].to_i + s['vv']['e'].to_i }
     end
 
     def billable_vv
