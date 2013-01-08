a43887011477ccda1a5de456b959bfbb7e5407ce
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-01-08 11:21:01 +0100

    admin_return_to support for admin /login

--- app/controllers/admin/admins/sessions_controller.rb
+++ app/controllers/admin/admins/sessions_controller.rb
@@ -4,4 +4,10 @@ class Admin::Admins::SessionsController < Devise::SessionsController
 
   layout 'admin'
 
+  # GET /resource/sign_in
+  def new
+    session["#{resource_name}_return_to"] = params[:"#{resource_name}_return_to"] if params[:"#{resource_name}_return_to"]
+    super
+  end
+
 end
