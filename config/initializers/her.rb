e3694d82ef6c3d86eabd3196bc2308032f4a8e54
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-01-23 09:38:38 +0100

    Add ssl_options for Her-Faraday connection

--- config/initializers/her.rb
+++ config/initializers/her.rb
@@ -2,8 +2,10 @@ require_dependency 'api/token_authentication'
 require_dependency 'api/response_parser'
 require_dependency 'api/url'
 
+ssl_options = { ca_path: '/usr/lib/ssl/certs' }
+
 $www_api = Her::API.new
-$www_api.setup url: Api::Url.new('www').url do |connection|
+$www_api.setup url: Api::Url.new('www').url, ssl: ssl_options do |connection|
   connection.use Api::TokenAuthentication, token: (ENV['API_PASSWORD'] || 'sublimevideo')
   connection.use Her::Middleware::AcceptJSON
   connection.use Api::ResponseParser
