db529f68f58572d5a63237314a470ed894dffff6
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-02-11 12:56:03 +0100

    Refactorize FastPassHelper

--- app/helpers/fast_pass_helper.rb
+++ app/helpers/fast_pass_helper.rb
@@ -3,55 +3,13 @@
 # app. Use #url to create a signed FastPass URL, and #script to generate the JS-based integration.
 #
 module FastPassHelper
-  extend ERB::Util
-
-  def self.domain
-    @domain || "getsatisfaction.com"
-  end
-
-  def self.domain=(val)
-    @domain = val
-  end
-
-  #
-  # Generates a FastPass URL with the given +email+, +name+, and +uid+ signed with the provided
-  # consumer +key+ and +secret+. The +key+ and +secret+ should match those provided in the company
-  # admin interface.
-  #
-  def self.url(key, secret, email, name, uid, secure = false, additional_fields = {})
-    consumer = OAuth::Consumer.new(key, secret)
-    uri = URI.parse(secure ? "https://#{domain}/fastpass" : "http://#{domain}/fastpass")
-    params = additional_fields.merge(email: email, name: name, uid: uid)
-
-    uri.query = params.to_query
-
-    http      = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true if uri.scheme == "https"
-    request   = Net::HTTP::Get.new(uri.request_uri)
-    request.oauth!(http, consumer, nil, scheme: 'query_string')
-
-    signature = request.oauth_helper.signature
-    #re-apply params with signature to the uri
-    query = params.merge(request.oauth_helper.oauth_parameters).merge("oauth_signature" => signature)
-    uri.query = query.to_query
-    return uri.to_s
-  end
-
-  #
-  # Generates a FastPass IMG tag. This integration method is likely to be deprecated, unless strong
-  # use cases are presented. Be warned.
-  #
-  def self.image(*args)
-    url = url(*args)
-    %Q{<img src="#{h url}" alt="" />}
-  end
-
+  # extend ERB::Util
   #
   # Generates a FastPass SCRIPT tag. The script will automatically rewrite all GetSatisfaction
   # URLs to include a 'fastpass' query parameter with a signed fastpass URL.
   #
-  def self.script(key, secret, email, name, uid, secure=false, additional_fields={})
-    url = url(key, secret, email, name, uid, secure, additional_fields)
+  def fastpass_script(key, secret, email, name, uid, secure=false, additional_fields={})
+    url = fastpass_url(key, secret, email, name, uid, secure, additional_fields)
 
     <<-EOS
     <script type="text/javascript">
@@ -67,7 +25,7 @@ module FastPassHelper
           script.src = url;
           head.appendChild(script);
         }
-        add_js("fastpass_common", document.location.protocol + "//#{domain}/javascripts/fastpass.js");
+        add_js("fastpass_common", document.location.protocol + "//#{fastpass_domain}/javascripts/fastpass.js");
 
         if(window.onload) { var old_load = window.onload; }
         window.onload = function() {
@@ -78,5 +36,35 @@ module FastPassHelper
 
     </script>
     EOS
+  end
+
+  private
+
+  def fastpass_domain
+    "getsatisfaction.com"
+  end
+
+  #
+  # Generates a FastPass URL with the given +email+, +name+, and +uid+ signed with the provided
+  # consumer +key+ and +secret+. The +key+ and +secret+ should match those provided in the company
+  # admin interface.
+  #
+  def fastpass_url(key, secret, email, name, uid, secure = false, additional_fields = {})
+    consumer = OAuth::Consumer.new(key, secret)
+    uri = URI.parse(secure ? "https://#{fastpass_domain}/fastpass" : "http://#{fastpass_domain}/fastpass")
+    params = additional_fields.merge(email: email, name: name, uid: uid)
+
+    uri.query = params.to_query
+
+    http      = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true if uri.scheme == "https"
+    request   = Net::HTTP::Get.new(uri.request_uri)
+    request.oauth!(http, consumer, nil, scheme: 'query_string')
+
+    signature = request.oauth_helper.signature
+    #re-apply params with signature to the uri
+    query = params.merge(request.oauth_helper.oauth_parameters).merge("oauth_signature" => signature)
+    uri.query = query.to_query
+    return uri.to_s
   end
 end
