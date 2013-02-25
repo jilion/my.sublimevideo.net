721c14aa9c8ee9c59392a39f30bbec4bab95b0d3
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-02-25 09:08:54 +0100

    Improve VideoTagOldDataUpdaterBridge

--- app/services/video_tag_old_data_updater_bridge.rb
+++ app/services/video_tag_old_data_updater_bridge.rb
@@ -1,16 +1,16 @@
 require 'video_tag_updater_worker'
 
 class VideoTagOldDataUpdaterBridge
-  attr_reader :site_token, :uid, :old_data
+  attr_reader :site_token, :uid, :data
 
-  def initialize(site_token, uid, old_data)
+  def initialize(site_token, uid, data)
     @site_token = site_token
     @uid        = uid
-    @old_data   = old_data
+    @data   = data
   end
 
   def update
-    if uid_from_attribute?
+    if uid_from_attribute? || first_source_present? || sources_id_present?
       VideoTagUpdaterWorker.perform_async(site_token, uid, converted_data)
     end
   end
@@ -19,18 +19,31 @@ class VideoTagOldDataUpdaterBridge
 
   def converted_data
     {}.tap do |hash|
-      hash[:t] = old_data['n'] if title_from_attribute?
+      hash[:t] = data['n'] if title_from_attribute?
+      hash[:s] = converted_sources if first_source_present?
       %w[p i io d z uo].each do |attr|
-        hash[attr.to_sym] = old_data[attr] if old_data[attr]
+        hash[attr.to_sym] = data[attr] if data[attr]
       end
     end
   end
 
+  def converted_sources
+    [data['s'][data['cs'].first]]
+  end
+
   def uid_from_attribute?
-    old_data['uo'] == 'a'
+    data['uo'] == 'a'
   end
 
   def title_from_attribute?
-    old_data['no'] == 'a'
+    data['no'] == 'a'
+  end
+
+  def first_source_present?
+    data['cs'] && data['s'][data['cs'].first]
+  end
+
+  def sources_id_present?
+    data['i'] && data['io']
   end
 end
