13084425b6f81c5321f6e444d076ef625fcdd22d
Rémy Coutable <remy@jilion.com>
2013-02-27 12:42:34 +0100

    Remove stage from support request

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -4,9 +4,11 @@
 # is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActionController::Base.wrap_parameters format: [:json]
+ActiveSupport.on_load(:action_controller) do
+  wrap_parameters format: [:json] if respond_to?(:wrap_parameters)
+end
 
-# Disable root element in JSON by default.
-if defined?(ActiveRecord)
-  ActiveRecord::Base.include_root_in_json = false
+# To enable root element in JSON for ActiveRecord objects.
+ActiveSupport.on_load(:active_record) do
+  self.include_root_in_json = false
 end
