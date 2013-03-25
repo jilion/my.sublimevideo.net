0d8493704cf676199322eabecbb1dcb152197bec
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-03-25 14:35:56 +0100

    Better database pool size configuration for Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,9 @@
 Sidekiq.configure_server do |config|
+  if database_url = ENV['DATABASE_URL']
+    ENV['DATABASE_URL'] = "#{database_url}?pool=52"
+    ActiveRecord::Base.establish_connection
+  end
+
   # http://mongoid.org/en/mongoid/docs/tips.html#sidekiq
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
