876bd8de329d1e3d0d829da0aee1d9ff727cd322
Rémy Coutable <remy@rymai.me>
2013-04-10 15:52:54 +0200

    Fix specs and add a nice `#required_params` helper!

--- app/controllers/private_api/sites_controller.rb
+++ app/controllers/private_api/sites_controller.rb
@@ -1,31 +1,47 @@
 require 'has_scope'
 
 class PrivateApi::SitesController < SublimeVideoPrivateApiController
-  has_scope :with_state, :created_on, :not_tagged_with, :by_date
-  has_scope :with_min_billable_video_views, :first_billable_plays_on_week
+  before_filter :find_site_by_token!, only: [:show, :add_tag]
+
+  has_scope :per, :with_state, :created_on, :not_tagged_with, :by_date,
+            :with_min_billable_video_views, :first_billable_plays_on_week
   has_scope :select, :without_hostnames, :without_tokens, type: :array
 
   # GET /private_api/sites
   def index
-    @sites = Site.page(params[:page])
-    @sites.includes(*params[:includes]) if params[:includes]
-
-    respond_with(apply_scopes(@sites))
+    respond_with(apply_scopes(Site.page(params[:page])))
   end
 
   # GET /private_api/sites/:id
   def show
-    @site = Site.where(token: params[:id]).first!
-
     respond_with(@site)
   end
 
   # PUT /private_api/sites/:id/add_tag
+  before_filter(only: :add_tag) { |controller| controller.required_params!(:tag) }
   def add_tag
-    @site = Site.where(token: params[:id]).first!
     @site.tag_list << params[:tag]
     @site.save!
 
     respond_with(@site)
+  end
+
+  def required_params!(*param_keys)
+    present_params_keys = params.select { |k, v| param_keys.include?(k.to_sym) && v.present? }.keys.map(&:to_sym)
+
+    unless present_params_keys.size == param_keys.size
+      body = { error: "Missing #{list_of_params_as_string(param_keys - present_params_keys)} parameters." }
+      render(json: body, status: 400) and return
+    end
+  end
+
+  private
+
+  def find_site_by_token!
+    @site = apply_scopes(Site.find_by_token(params[:id]))
+  end
+
+  def list_of_params_as_string(list)
+    list.map { |p| ":#{p}" }.join(', ')
   end
 end
--- spec/requests/private_api/sites_spec.rb
+++ spec/requests/private_api/sites_spec.rb
@@ -1,51 +1,91 @@
 require 'spec_helper'
 
-describe "Sites requests" do
-  let!(:site1) { create(:site, hostname: 'google.com') }
+describe 'Private API Sites requests' do
+  let!(:site1) { create(:site, hostname: 'google.com').tap { |s| s.tag_list << 'adult'; s.save! } }
   let!(:site2) { create(:site, hostname: 'facebook.com', first_billable_plays_at: Time.now.utc) }
-  let!(:site3) { create(:site, created_at: 2.days.ago) }
-  before { set_api_credentials }
+  let!(:site3) { create(:site, created_at: 2.days.ago, state: 'archived') }
+  before do
+    set_api_credentials
+    @env['HTTP_HOST'] = 'my.sublimevideo.dev'
+  end
 
-  describe "index" do
-    it "supports per scope" do
-      get "private_api/sites.json", { per: 2 }, @env
+  describe 'index' do
+    it 'supports :per scope' do
+      get 'private_api/sites.json', { per: 2 }, @env
       MultiJson.load(response.body).should have(2).sites
     end
 
-    it "supports select scope" do
-      get "private_api/sites.json", { select: %w[token hostname] }, @env
+    it 'supports :with_state scope' do
+      get 'private_api/sites.json', { with_state: 'archived' }, @env
+      body = MultiJson.load(response.body)
+      body.should have(1).site
+      body[0]['token'].should eq site3.token
+    end
+
+    it 'supports :created_on scope' do
+      get 'private_api/sites.json', { created_on: 2.days.ago }, @env
+      body = MultiJson.load(response.body)
+      body.should have(1).site
+      body[0]['token'].should eq site3.token
+    end
+
+    it 'supports :not_tagged_with scope' do
+      get 'private_api/sites.json', { not_tagged_with: 'adult' }, @env
+      body = MultiJson.load(response.body)
+      body.should have(2).sites
+      body[0]['token'].should eq site2.token
+      body[1]['token'].should eq site3.token
+    end
+
+    it 'supports :select scope' do
+      get 'private_api/sites.json', { select: %w[token hostname] }, @env
       video_tag = MultiJson.load(response.body).first
-      video_tag.should have_key("token")
-      video_tag.should have_key("hostname")
-      video_tag.should_not have_key("dev_hostnames")
+      video_tag.should have_key('token')
+      video_tag.should have_key('hostname')
+      video_tag.should_not have_key('dev_hostnames')
     end
 
-    it "supports without_hostnames scope" do
-      get "private_api/sites.json", { without_hostnames: ['google.com', 'facebook.com'] }, @env
+    it 'supports :without_hostnames scope' do
+      get 'private_api/sites.json', { without_hostnames: %w[google.com facebook.com] }, @env
       body = MultiJson.load(response.body)
       body.should have(1).site
       body[0]['token'].should eq site3.token
     end
 
-    it "supports created_on scope" do
-      get "private_api/sites.json", { created_on: 2.days.ago }, @env
-      body = MultiJson.load(response.body)
-      body.should have(1).site
-      body[0]['token'].should eq site3.token
-    end
-
-    it "supports first_billable_plays_on_week scope" do
-      get "private_api/sites.json", { first_billable_plays_on_week: Time.now.utc }, @env
+    it 'supports :first_billable_plays_on_week scope' do
+      get 'private_api/sites.json', { first_billable_plays_on_week: Time.now.utc }, @env
       body = MultiJson.load(response.body)
       body.should have(1).site
       body[0]['token'].should eq site2.token
     end
   end
 
-  describe "show" do
-    it "finds site per token" do
+  describe 'show' do
+    it 'finds site per token' do
       get "private_api/sites/#{site1.token}.json", {}, @env
       MultiJson.load(response.body).should_not have_key("site")
+    end
+  end
+
+  describe 'add_tag' do
+    it 'adds tag to site' do
+      put "private_api/sites/#{site2.token}/add_tag.json", { tag: 'adult' }, @env
+      site2.tag_list.should include('adult')
+      response.status.should eq 204
+    end
+
+    describe 'requires the :tag param' do
+      it 'returns a 400 if :tag is missing' do
+        put "private_api/sites/#{site1.token}/add_tag.json", {}, @env
+        response.status.should eq 400
+        MultiJson.load(response.body).should eq({ 'error' => 'Missing :tag parameters.' })
+      end
+
+      it 'returns a 400 if :tag is nil' do
+        put "private_api/sites/#{site1.token}/add_tag.json", { tag: nil }, @env
+        response.status.should eq 400
+        MultiJson.load(response.body).should eq({ 'error' => 'Missing :tag parameters.' })
+      end
     end
   end
 end
