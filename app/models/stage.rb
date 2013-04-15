c4f9ffb23a7b9fab091730f51f33d385f2045147
Rémy Coutable <remy@rymai.me>
2013-04-15 11:21:38 +0200

    Improve doc
    [ci skip]

--- app/models/stage.rb
+++ app/models/stage.rb
@@ -5,28 +5,35 @@ class Stage
     STAGES
   end
 
-  # Returns the stage that are less stable than the given stage.
-  # It includes the given stage.
+  # Returns the given stage + the stages that are less stable
+  # than the given stage.
   #
-  # stable => %w[beta alpha]
-  # beta   => %w[alpha]
-  # alpha  => []
+  # @example Given the beta stage
+  #   Stage.stages_equal_or_more_stable_than('beta') #=> %w[beta alpha]
   #
   def self.stages_equal_or_less_stable_than(stage)
     STAGES[STAGES.index(stage)..-1]
   end
 
-  # Returns the stage that are more stable than the given stage.
-  # It includes the given stage.
+  # Returns the given stage + the stages that are more stable
+  # than the given stage.
   #
-  # stable =>[]
-  # beta   => %w[stable]
-  # alpha  => %w[stable beta]
+  # @example Given the beta stage
+  #   Stage.stages_equal_or_more_stable_than('beta') #=> %w[stable beta]
   #
   def self.stages_equal_or_more_stable_than(stage)
     STAGES[0..STAGES.index(stage)]
   end
 
+  # Returns the stage for a given version.
+  #
+  # @example A stable version
+  #   Stage.version_stage('1.0') #=> 'stable'
+  # @example A beta version
+  #   Stage.version_stage('1.1-beta') #=> 'beta'
+  # @example An alpha version
+  #   Stage.version_stage('2.0-alpha') #=> 'alpha'
+  #
   def self.version_stage(version)
     case version
     when /(alpha|beta)/
