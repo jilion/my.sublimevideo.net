cfd545605cb8cca5abcec3185715800d97fa9959
Rémy Coutable <remy@rymai.me>
2013-06-03 18:03:07 +0200

    Rename indexes that included "app_design"

---
+++ db/migrate/20130603122845_rename_some_indexes.rb
@@ -0,0 +1,21 @@
+class RenameSomeIndexes < ActiveRecord::Migration
+  def up
+    rename_index :designs, 'index_app_designs_on_name', 'index_designs_on_name'
+    rename_index :designs, 'index_app_designs_on_skin_token', 'index_designs_on_skin_token'
+
+    rename_index :app_plugins, 'index_app_plugins_on_app_design_id', 'index_app_plugins_on_design_id'
+    rename_index :app_plugins, 'index_app_plugins_on_app_design_id_and_addon_id', 'index_app_plugins_on_design_id_and_addon_id'
+
+    rename_index :kits, 'index_kits_on_app_design_id', 'index_kits_on_design_id'
+  end
+
+  def down
+    rename_index :designs, 'index_designs_on_name', 'index_app_designs_on_name'
+    rename_index :designs, 'index_designs_on_skin_token', 'index_app_designs_on_skin_token'
+
+    rename_index :app_plugins, 'index_app_plugins_on_design_id', 'index_app_plugins_on_app_design_id'
+    rename_index :app_plugins, 'index_app_plugins_on_design_id_and_addon_id', 'index_app_plugins_on_app_design_id_and_addon_id'
+
+    rename_index :kits, 'index_kits_on_design_id', 'index_kits_on_app_design_id'
+  end
+end
