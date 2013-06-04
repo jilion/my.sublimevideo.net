b34f268b636107684c1ce3bfdd8228c3712e6216
Rémy Coutable <remy@rymai.me>
2013-06-04 15:16:03 +0200

    New private APIs for addons & kits

---
+++ app/controllers/private_api/kits_controller.rb
@@ -0,0 +1,24 @@
+require 'has_scope'
+
+class PrivateApi::KitsController < SublimeVideoPrivateApiController
+  before_filter :_find_site_by_token!, only: [:index]
+  before_filter :_find_kits!, only: [:index]
+
+  has_scope :per
+
+  # GET /private_api/sites/:id/kits
+  def index
+    expires_in 2.minutes
+    respond_with(@kits)
+  end
+
+  private
+
+  def _find_site_by_token!
+    @site = Site.with_state('active').find_by_token!(params[:site_id])
+  end
+
+  def _find_kits!
+    @kits = apply_scopes(@site.kits.page(params[:page]))
+  end
+end
