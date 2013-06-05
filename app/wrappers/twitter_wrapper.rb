298ab48a524949c630cb6bb927d85e149f6f1415
Rémy Coutable <remy@rymai.me>
2013-06-05 15:13:52 +0200

    Update TwitterWrapper to use env variables

--- app/wrappers/twitter_wrapper.rb
+++ app/wrappers/twitter_wrapper.rb
@@ -1,41 +1,35 @@
-require 'configurator'
+require 'twitter'
+require 'rescue_me'
+require 'notifier'
 
 module TwitterWrapper
-  include Configurator
 
-  config_file 'twitter.yml'
-  config_accessor :consumer_key, :consumer_secret, :oauth_token, :oauth_token_secret
+  def self.method_missing(method_name, *args)
+    method_name = method_name.to_sym
 
-  class << self
-
-    def method_missing(method_name, *args)
-      method_name = method_name.to_sym
-
-      if Twitter.respond_to?(method_name)
-        begin
-          with_rescue_and_retry(7) do
-            Twitter.send(method_name, *args)
-          end
-        rescue Twitter::Error::TooManyRequests => error
-          Notifier.send('Too many Twitter requests.')
+    if Twitter.respond_to?(method_name)
+      begin
+        with_rescue_and_retry(7) do
+          Twitter.send(method_name, *args)
         end
-      else
-        super
+      rescue Twitter::Error::TooManyRequests => error
+        Notifier.send('Too many Twitter requests.')
       end
+    else
+      super
     end
+  end
 
-    def respond_to?(method_name)
-      method_name = method_name.to_sym
+  def self.respond_to?(method_name)
+    method_name = method_name.to_sym
 
-      Twitter.respond_to?(method_name) || super
+    Twitter.respond_to?(method_name) || super
+  end
+
+  def self.with_rescue_and_retry(times)
+    rescue_and_retry(times, Errno::ETIMEDOUT, Errno::ECONNRESET, Twitter::Error::BadGateway, Twitter::Error::ServiceUnavailable, Twitter::Error::InternalServerError, Twitter::Error::ClientError) do
+      yield
     end
-
-    def with_rescue_and_retry(times)
-      rescue_and_retry(times, Errno::ETIMEDOUT, Errno::ECONNRESET, Twitter::Error::BadGateway, Twitter::Error::ServiceUnavailable, Twitter::Error::InternalServerError, Twitter::Error::ClientError) do
-        yield
-      end
-    end
-
   end
 
 end
