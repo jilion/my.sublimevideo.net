79a9005740cd5add5ef96f329a174faaa8e55a1f
Rémy Coutable <remy@rymai.me>
2013-07-23 15:11:58 +0200

    Reduce complexity (and unused code) of Enthusiast

--- app/models/enthusiast.rb
+++ app/models/enthusiast.rb
@@ -12,31 +12,12 @@ class Enthusiast < ActiveRecord::Base
 
   default_scope where(trashed_at: nil)
 
-  scope :starred,                -> { where { starred == true } }
-  scope :confirmed,              -> { where { confirmed_at != nil } }
-  scope :not_confirmed,          -> { where { confirmed_at == nil } }
-  scope :invited,                -> { where { invited_at != nil } }
-  scope :not_invited,            -> { where { invited_at == nil } }
-  scope :interested_in_beta,     -> { where { interested_in_beta == true } }
-  scope :not_interested_in_beta, -> { where { interested_in_beta == false } }
-  scope :not_already_resent_confirmation_instructions, -> { where { confirmation_resent_at == nil } }
-
-  scope :having_comment, ->(bool) { bool ? where { free_text << ['', nil] } : where { free_text >> ['', nil] } }
-  scope :having_site,    ->(bool) { bool ? select('DISTINCT(enthusiasts.id)').joins(:sites) : where('enthusiasts.id NOT IN (?)', having_site(true)) }
-
-  scope :by_date,               ->(way = 'desc') { order { created_at.send(way) } }
-  scope :by_email,              ->(way = 'desc') { order { email.send(way) }.order { created_at.desc } }
-  scope :by_starred,            ->(way = 'asc') { order { starred.send(way) }.order { created_at.desc } }
-  scope :by_confirmed,          ->(way = 'asc') { order { confirmed_at.send(way) }.order { created_at.desc } }
-  scope :by_interested_in_beta, ->(way = 'desc') { order { interested_in_beta.send(way) }.order { created_at.desc } }
-  scope :by_invited,            ->(way = 'desc') { order { invited_at.send(way) }.order { created_at.desc } }
+  scope :by_date,    ->(way = 'desc') { order { created_at.send(way) } }
+  scope :by_email,   ->(way = 'desc') { order { email.send(way) }.order { created_at.desc } }
+  scope :by_invited, ->(way = 'desc') { order { invited_at.send(way) }.order { created_at.desc } }
 
   def self.additional_or_conditions
     %w[email free_text].reduce([]) { |a, e| a << ("lower(#{e}) =~ " + 'lower("%#{q}%")') }
-  end
-
-  def confirmation_resent?
-    confirmation_resent_at?
   end
 
   def confirmed?
