b4310934eee4b876c1b4dd3f9f3c8a5b7d202535
Rémy Coutable <remy@rymai.me>
2013-07-30 17:24:02 +0200

    Refactor SiteStatsTimelineBuilder

--- app/services/site_stats_timeline_builder.rb
+++ app/services/site_stats_timeline_builder.rb
@@ -8,30 +8,31 @@ class SiteStatsTimelineBuilder
   %w[pv vv].each do |type|
     %w[d m e em].each do |field|
       define_method("#{field}_#{type}") do
-        all.map { |s| s[type][field].to_i }
+        _all.map { |s| s[type][field].to_i }
       end
     end
   end
 
-  def normal_pv
-    all.map { |s| s['pv']['m'].to_i + s['pv']['e'].to_i }
-  end
-
-  def billable_pv
-    all.map { |s| s['pv']['m'].to_i + s['pv']['e'].to_i + s['pv']['em'].to_i }
-  end
-
-  def normal_vv
-    all.map { |s| s['vv']['m'].to_i + s['vv']['e'].to_i }
-  end
-
-  def billable_vv
-    all.map { |s| s['vv']['m'].to_i + s['vv']['e'].to_i + s['vv']['em'].to_i }
+  %w[p v].each do |view_type|
+    %w[normal billable].each do |type|
+      define_method("#{type}_#{view_type}v") do
+        send("_#{type}_views", view_type)
+      end
+    end
   end
 
   private
 
-  def all
-    @all ||= Stat::Site::Day.last_stats(@options)
+  def _normal_views(type)
+    _all.map { |s| s["#{type}v"]['m'].to_i + s["#{type}v"]['e'].to_i }
   end
+
+  def _billable_views(type)
+    _all.map { |s| s["#{type}v"]['m'].to_i + s["#{type}v"]['e'].to_i + s["#{type}v"]['em'].to_i }
+  end
+
+  def _all
+    @_all ||= Stat::Site::Day.last_stats(@options)
+  end
+
 end
