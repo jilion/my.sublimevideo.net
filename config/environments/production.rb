39fca893f5b4c0d0477dc424104e0330def6d266
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-08-15 16:37:53 +0200

    Remove threadsafe! config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,9 +93,6 @@ MySublimeVideo::Application.configure do
     domain: ENV['SENDGRID_DOMAIN']
   }
 
-  # Enable threaded mode
-  # config.threadsafe!
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -71,9 +71,6 @@ MySublimeVideo::Application.configure do
   config.action_mailer.delivery_method = :test
   config.action_mailer.default_url_options = { host: 'my.sublimevideo-staging.net', protocol: 'https' }
 
-  # Enable threaded mode
-  # config.threadsafe!
-
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
