42b364cfb91a0d967e2bfb5062610d3474261724
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-08-20 16:40:08 +0200

    Try to fix conflict between FactoryGirl and Spring

--- spec/config/factory_girl.rb
+++ spec/config/factory_girl.rb
@@ -1,4 +1,9 @@
 RSpec.configure do |config|
   # FactoryGirl http://railscasts.com/episodes/158-factories-not-fixtures-revised
   config.include FactoryGirl::Syntax::Methods
+
+  # Spring issue https://github.com/jonleighton/spring/issues/88
+  config.before(:all) do
+    FactoryGirl.reload
+  end
 end
