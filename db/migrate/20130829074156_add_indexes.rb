9c549fee3cd4d284d9b47eb895354d81f9602c2a
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-08-29 10:04:08 +0200

    Set a name for the big index

--- db/migrate/20130829074156_add_indexes.rb
+++ db/migrate/20130829074156_add_indexes.rb
@@ -2,6 +2,6 @@ class AddIndexes < ActiveRecord::Migration
   def change
     add_index :sites, [:id, :state]
     add_index :users, [:id, :state]
-    add_index :billable_item_activities, [:site_id, :item_type, :item_id, :state, :created_at]
+    add_index :billable_item_activities, [:site_id, :item_type, :item_id, :state, :created_at], name: 'billable_item_activities_big_index'
   end
 end
