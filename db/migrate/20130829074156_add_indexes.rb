85d33d6b3fca8eb5961b61edf0734690d1ec8203
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-08-29 09:55:26 +0200

    Add indexes

---
+++ db/migrate/20130829074156_add_indexes.rb
@@ -0,0 +1,7 @@
+class AddIndexes < ActiveRecord::Migration
+  def change
+    add_index :sites, [:id, :state]
+    add_index :users, [:id, :state]
+    add_index :billable_item_activities, [:site_id, :item_type, :item_id, :state, :created_at]
+  end
+end
