f7a830a1d483d4518e649fdfa7dd53071d190a40
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-09-05 14:29:29 +0200

    Add http cache to SitesController

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -8,21 +8,25 @@ class SitesController < ApplicationController
   before_filter :_set_sites, only: [:new, :create]
   before_filter :_set_site, only: [:edit, :update, :destroy]
 
+  etag { current_user.id }
+
   has_scope :by_hostname, :by_date, :by_last_30_days_billable_video_views, :by_last_30_days_video_tags
 
   # GET /sites
   def index
     @sites = apply_scopes(@sites.includes(:invoices).by_date)
 
-    respond_with(@sites, per_page: 10) do |format|
-      format.html
-      format.json { render json: @sites.to_backbone_json }
+    if stale?(last_modified: @sites.maximum(:updated_at), etag: @sites.to_sql)
+      respond_with(@sites, per_page: 10) do |format|
+        format.html
+        format.json { render json: @sites.to_backbone_json }
+      end
     end
   end
 
   # GET /sites/:id/edit
   def edit
-    respond_with(@site)
+    respond_with(@site) if stale?(@site)
   end
 
   # PUT /sites/:id
