a110007af839ed6287247b59979a34343fb77de5
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-09-17 14:55:58 +0200

    Fix WebMock::NetConnectNotAllowedError error with CodeClimate coverage

--- spec/config/vcr.rb
+++ spec/config/vcr.rb
@@ -1,9 +1,11 @@
 require 'vcr'
 
+WebMock.disable_net_connect!(allow: "codeclimate.com")
+
 VCR.config do |config|
   config.stub_with :webmock, :typhoeus
   config.cassette_library_dir     = 'spec/fixtures/vcr_cassettes'
-  config.ignore_hosts             'sublimevideo.dev', 'example.com'
+  config.ignore_hosts             'sublimevideo.dev', 'example.com', 'codeclimate.com'
   config.ignore_localhost         = true
   config.default_cassette_options = { record: :new_episodes }
 end
