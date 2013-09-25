9c2dfea0fc9768cd03f052657a321e4fa2660f6d
Rémy Coutable <remy@rymai.me>
2013-09-25 16:13:20 +0200

    Update RSpec config

--- spec/config/rspec.rb
+++ spec/config/rspec.rb
@@ -1,8 +1,8 @@
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run_including focus: ENV['FOCUS'] != 'false'
+  config.filter_run_including focus: ENV['CI'] != 'true'
   config.mock_with :rspec
-  config.fail_fast = ENV['FAST_FAIL'] != 'false'
+  config.fail_fast = ENV['CI'] != 'true'
   config.order = ENV['ORDER'] || 'random'
 end
