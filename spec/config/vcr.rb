4db82a7ac09803859e37d082dbba21a99706754a
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-09-26 14:20:04 +0200

    Update VCR config and remove cassettes

--- spec/config/vcr.rb
+++ spec/config/vcr.rb
@@ -1,14 +1,11 @@
 require 'vcr'
 
-VCR.config do |config|
-  config.stub_with :webmock, :typhoeus
-  config.cassette_library_dir     = 'spec/fixtures/vcr_cassettes'
-  config.ignore_hosts             'sublimevideo.dev', 'example.com', 'codeclimate.com'
-  config.ignore_localhost         = true
-  config.default_cassette_options = { record: :new_episodes }
+VCR.configure do |c|
+  c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
+  c.hook_into :webmock, :typhoeus
+  c.ignore_hosts 'sublimevideo.dev', 'example.com', 'codeclimate.com'
+  c.ignore_localhost = true
+  c.default_cassette_options = { record: :new_episodes }
+  c.configure_rspec_metadata!
 end
 WebMock.disable_net_connect!(allow: "codeclimate.com")
-
-RSpec.configure do |config|
-  config.extend VCR::RSpec::Macros
-end
