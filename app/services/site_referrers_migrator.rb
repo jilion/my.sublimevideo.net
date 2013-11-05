8a81d74f2352c4b99052155fed2cd34bdf93ac74
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-11-05 21:02:21 +0100

    Improve SiteReferrersMigrator

--- app/services/site_referrers_migrator.rb
+++ app/services/site_referrers_migrator.rb
@@ -3,6 +3,15 @@ require 'site_admin_pages_migrator_worker'
 class SiteReferrersMigrator
   attr_accessor :site
 
+  def self.migrate_all
+    Site.all.each { |site| self.delay(queue: 'my-stats_migration').migrate(site.id) }
+  end
+
+  def self.migrate(site_id)
+    site = Site.find_by_id(site_id)
+    new(site).migrate
+  end
+
   def initialize(site)
     @site = site
   end
