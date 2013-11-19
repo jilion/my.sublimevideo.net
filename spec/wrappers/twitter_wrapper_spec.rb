75ad24cda3e2abc5a213314bad81270857152cc6
Rémy Coutable <remy@rymai.me>
2013-11-19 17:21:48 +0100

    Fix spec

--- spec/wrappers/twitter_wrapper_spec.rb
+++ spec/wrappers/twitter_wrapper_spec.rb
@@ -6,7 +6,7 @@ describe TwitterWrapper do
 
   describe 'method_missing' do
     it 'delegates to Twitter if possible' do
-      Twitter.should_receive(:favorites)
+      TwitterWrapper.client.should_receive(:favorites)
 
       TwitterWrapper.favorites
     end
