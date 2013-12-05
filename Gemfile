fe06e7f0ac8ff96400d7b75e108d1067ac65be1b
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2013-12-05 13:48:19 +0100

    Remove typhoeus / Update turbolinks

--- Gemfile
+++ Gemfile
@@ -165,7 +165,6 @@ group :test do
   gem 'poltergeist'
   gem 'show_me_the_cookies'
   gem 'webmock'
-  # gem 'typhoeus'
   gem 'vcr'
   gem 'codeclimate-test-reporter', require: false
 
--- spec/config/vcr.rb
+++ spec/config/vcr.rb
@@ -2,7 +2,7 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  c.hook_into :webmock#, :typhoeus
+  c.hook_into :webmock
   c.ignore_hosts 'sublimevideo.dev', 'example.com', 'codeclimate.com'
   c.ignore_localhost = true
   c.default_cassette_options = { record: :new_episodes }
