abb69ddd4543fa82caa718c1760758ec0ad97fe9
Rémy Coutable <remy@rymai.me>
2014-01-24 10:56:07 +0100

    Remove useless file

--- spec/routing/billings_routing_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe BillingsController do
-
-  it { get(with_subdomain('my', 'account/billing/edit')).should route_to('billings#edit') }
-  it { put(with_subdomain('my', 'account/billing')).should route_to('billings#update') }
-
-end
