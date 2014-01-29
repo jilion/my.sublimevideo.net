c9e13e015b93018ed7273910bd37e51327dfcf3d
Thibaud Guillaume-Gentil <thibaud@jilion.com>
2014-01-29 15:00:26 +0100

    Fix asset_host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -56,7 +56,7 @@ MySublimeVideo::Application.configure do
   config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = '//sublimevideo-staging.net'
+  config.action_controller.asset_host = '//my.sublimevideo-staging.net'
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
